fb26069e03fd274f8ce9dcb8be8da9be5644ab24
knutsvk <ksk38@cam.ac.uk>
2019-03-14 13:18:19 +0000

    fix computation of derived quantities, get rid of eb_gradu module

--- src/derive/derive_eb_mod.f90
+++ src/derive/derive_eb_mod.f90
@@ -5,7 +5,6 @@ module derive_eb_module
    use param,             only: zero, half, one, two, my_huge
    use amrex_ebcellflag_module, only: is_covered_cell, &
                                       is_single_valued_cell
-   use eb_gradu_module,  only: compute_eb_gradu
 
    implicit none
    private
@@ -18,14 +17,6 @@ contains
                                     sr, slo, shi,    &
                                     vel, vlo, vhi,    &
                                     flags,    flo,  fhi, &
-                                    afrac_x, axlo, axhi, &
-                                    afrac_y, aylo, ayhi, &
-                                    afrac_z, azlo, azhi, &
-                                    cent_x,  cxlo, cxhi, &
-                                    cent_y,  cylo, cyhi, &
-                                    cent_z,  czlo, czhi, &
-                                    vfrac,   vflo, vfhi, &
-                                    bcent,    blo,  bhi, &
                                     dx) bind(C)
 
       ! Loops bounds
@@ -35,40 +26,18 @@ contains
       integer(c_int),  intent(in   ) ::  slo(3),  shi(3)
       integer(c_int),  intent(in   ) ::  vlo(3),  vhi(3)
       integer(c_int),  intent(in   ) ::  flo(3),  fhi(3)
-      integer(c_int),  intent(in   ) :: axlo(3), axhi(3)
-      integer(c_int),  intent(in   ) :: aylo(3), ayhi(3)
-      integer(c_int),  intent(in   ) :: azlo(3), azhi(3)
-      integer(c_int),  intent(in   ) :: cxlo(3), cxhi(3)
-      integer(c_int),  intent(in   ) :: cylo(3), cyhi(3)
-      integer(c_int),  intent(in   ) :: czlo(3), czhi(3)
-      integer(c_int),  intent(in   ) :: vflo(3), vfhi(3)
-      integer(c_int),  intent(in   ) ::  blo(3),  bhi(3)
 
       ! Grid
       real(rt), intent(in   ) :: dx(3)
 
       ! Arrays
-      real(rt), intent(in   ) ::                                  &
-           &     vel(  vlo(1):  vhi(1),  vlo(2):  vhi(2),  vlo(3):  vhi(3),3), &
-           & afrac_x( axlo(1): axhi(1), axlo(2): axhi(2), axlo(3): axhi(3)  ), &
-           & afrac_y( aylo(1): ayhi(1), aylo(2): ayhi(2), aylo(3): ayhi(3)  ), &
-           & afrac_z( azlo(1): azhi(1), azlo(2): azhi(2), azlo(3): azhi(3)  ), &
-           &  cent_x( cxlo(1): cxhi(1), cxlo(2): cxhi(2), cxlo(3): cxhi(3),2), &
-           &  cent_y( cylo(1): cyhi(1), cylo(2): cyhi(2), cylo(3): cyhi(3),2), &
-           &  cent_z( czlo(1): czhi(1), czlo(2): czhi(2), czlo(3): czhi(3),2), &
-           &   vfrac( vflo(1): vfhi(1), vflo(2): vfhi(2), vflo(3): vfhi(3)  ), &
-           &   bcent(  blo(1):  bhi(1),  blo(2):  bhi(2),  blo(3):  bhi(3),3)
-
-      real(rt),  intent(inout) ::                                 &
-         sr(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3))
-
-      integer,  intent(in   ) :: &
-         flags(flo(1):fhi(1),flo(2):fhi(2),flo(3):fhi(3))
+      real(rt), intent(in   ) ::   vel(vlo(1):vhi(1), vlo(2):vhi(2), vlo(3):vhi(3), 3)
+      real(rt), intent(inout) ::    sr(slo(1):shi(1), slo(2):shi(2), slo(3):shi(3)   )
+      integer,  intent(in   ) :: flags(flo(1):fhi(1), flo(2):fhi(2), flo(3):fhi(3)   )
 
       integer(c_int) :: i, j, k
       real(rt)       :: idx, idy, idz
       real(rt)       :: ux, uy, uz, vx, vy, vz, wx, wy, wz
-      real(rt)       :: gradu(9)
       real(rt)       :: c0, c1, c2
 
       ! Coefficients for one-sided difference estimation
@@ -90,52 +59,26 @@ contains
 
                else if (is_single_valued_cell(flags(i,j,k))) then
 
-                  ! Compute the velocity gradients at the cell centroid
-                  ! 
-                  call compute_eb_gradu(gradu, dx, i, j, k, & 
-                                        vel, vlo, vhi, &
-                                        bcent, blo, bhi, &
-                                        afrac_x, axlo, axhi, & 
-                                        afrac_y, aylo, ayhi, & 
-                                        afrac_z, azlo, azhi, 1)
-                  ux = gradu(1) * idx
-                  uy = gradu(2) * idy
-                  uz = gradu(3) * idz
-                  !
-                  vx = gradu(4) * idx
-                  vy = gradu(5) * idy
-                  vz = gradu(6) * idz
-                  !
-                  wx = gradu(7) * idx
-                  wy = gradu(8) * idy
-                  wz = gradu(9) * idz
-
-                  sr(i,j,k) = &
-                     sqrt(two * ux**2 + two * vy**2 + two * wz**2 + &
-                          (uy + vx)**2 + (vz + wy)**2 + (wx + uz)**2)
-
-               else
-
-                  ! Need to check if there are cut cells in neighbours --
+                  ! Need to check if there are covered cells in neighbours --
                   ! -- if so, use one-sided difference computation (but still quadratic)
 
-                  if (is_single_valued_cell(flags(i+1,j,k))) then 
+                  if (is_covered_cell(flags(i+1,j,k))) then 
 
-                     ! Cut cell to the right, go fish left
+                     ! Covered cell to the right, go fish left
                      ux = - (c0 * vel(i,j,k,1) + c1 * vel(i-1,j,k,1) + c2 * vel(i-2,j,k,1)) * idx
                      vx = - (c0 * vel(i,j,k,2) + c1 * vel(i-1,j,k,2) + c2 * vel(i-2,j,k,2)) * idx
                      wx = - (c0 * vel(i,j,k,3) + c1 * vel(i-1,j,k,3) + c2 * vel(i-2,j,k,3)) * idx
 
-                  else if (is_single_valued_cell(flags(i-1,j,k))) then
+                  else if (is_covered_cell(flags(i-1,j,k))) then
 
-                     ! Cut cell to the left, go fish right
+                     ! Covered cell to the left, go fish right
                      ux = (c0 * vel(i,j,k,1) + c1 * vel(i+1,j,k,1) + c2 * vel(i+2,j,k,1)) * idx
                      vx = (c0 * vel(i,j,k,2) + c1 * vel(i+1,j,k,2) + c2 * vel(i+2,j,k,2)) * idx
                      wx = (c0 * vel(i,j,k,3) + c1 * vel(i+1,j,k,3) + c2 * vel(i+2,j,k,3)) * idx
 
                   else
 
-                     ! No cut cells right or left, use standard stencil
+                     ! No covered cells right or left, use standard stencil
                      ux = half * (vel(i+1,j,k,1) - vel(i-1,j,k,1)) * idx
                      vx = half * (vel(i+1,j,k,2) - vel(i-1,j,k,2)) * idx
                      wx = half * (vel(i+1,j,k,3) - vel(i-1,j,k,3)) * idx
@@ -144,13 +87,13 @@ contains
                                                                    
                   ! Do the same in y-direction 
 
-                  if (is_single_valued_cell(flags(i,j+1,k))) then
+                  if (is_covered_cell(flags(i,j+1,k))) then
 
                      uy = - (c0 * vel(i,j,k,1) + c1 * vel(i,j-1,k,1) + c2 * vel(i,j-2,k,1)) * idy
                      vy = - (c0 * vel(i,j,k,2) + c1 * vel(i,j-1,k,2) + c2 * vel(i,j-2,k,2)) * idy
                      wy = - (c0 * vel(i,j,k,3) + c1 * vel(i,j-1,k,3) + c2 * vel(i,j-2,k,3)) * idy
 
-                  else if (is_single_valued_cell(flags(i,j-1,k))) then
+                  else if (is_covered_cell(flags(i,j-1,k))) then
 
                      uy = (c0 * vel(i,j,k,1) + c1 * vel(i,j+1,k,1) + c2 * vel(i,j+2,k,1)) * idy
                      vy = (c0 * vel(i,j,k,2) + c1 * vel(i,j+1,k,2) + c2 * vel(i,j+2,k,2)) * idy
@@ -166,13 +109,13 @@ contains
 
                   ! Do the same in z-direction 
 
-                  if (is_single_valued_cell(flags(i,j,k+1))) then
+                  if (is_covered_cell(flags(i,j,k+1))) then
 
                      uz = - (c0 * vel(i,j,k,1) + c1 * vel(i,j,k-1,1) + c2 * vel(i,j,k-2,1)) * idz
                      vz = - (c0 * vel(i,j,k,2) + c1 * vel(i,j,k-1,2) + c2 * vel(i,j,k-2,2)) * idz
                      wz = - (c0 * vel(i,j,k,3) + c1 * vel(i,j,k-1,3) + c2 * vel(i,j,k-2,3)) * idz
 
-                  else if (is_single_valued_cell(flags(i,j,k-1))) then
+                  else if (is_covered_cell(flags(i,j,k-1))) then
 
                      uz = (c0 * vel(i,j,k,1) + c1 * vel(i,j,k+1,1) + c2 * vel(i,j,k+2,1)) * idz
                      vz = (c0 * vel(i,j,k,2) + c1 * vel(i,j,k+1,2) + c2 * vel(i,j,k+2,2)) * idz
@@ -190,6 +133,24 @@ contains
                   sr(i,j,k) = sqrt(two * ux**2 + two * vy**2 + two * wz**2 + & 
                                    (uy + vx)**2 + (vz + wy)**2 + (wx + uz)**2)
 
+               else
+
+                  ux = half * (vel(i+1,j,k,1) - vel(i-1,j,k,1)) * idx
+                  vx = half * (vel(i+1,j,k,2) - vel(i-1,j,k,2)) * idx
+                  wx = half * (vel(i+1,j,k,3) - vel(i-1,j,k,3)) * idx
+
+                  uy = half * (vel(i,j+1,k,1) - vel(i,j-1,k,1)) * idy
+                  vy = half * (vel(i,j+1,k,2) - vel(i,j-1,k,2)) * idy
+                  wy = half * (vel(i,j+1,k,3) - vel(i,j-1,k,3)) * idy
+                                                                   
+                  uz = half * (vel(i,j,k+1,1) - vel(i,j,k-1,1)) * idz
+                  vz = half * (vel(i,j,k+1,2) - vel(i,j,k-1,2)) * idz
+                  wz = half * (vel(i,j,k+1,3) - vel(i,j,k-1,3)) * idz
+
+                  ! Finally, compute the strain-rate magnitude
+                  sr(i,j,k) = sqrt(two * ux**2 + two * vy**2 + two * wz**2 + & 
+                                   (uy + vx)**2 + (vz + wy)**2 + (wx + uz)**2)
+
                end if
 
             end do
@@ -203,14 +164,6 @@ contains
                               vort, slo, shi,    &
                               vel, vlo, vhi,    &
                               flags,    flo,  fhi, &
-                              afrac_x, axlo, axhi, &
-                              afrac_y, aylo, ayhi, &
-                              afrac_z, azlo, azhi, &
-                              cent_x,  cxlo, cxhi, &
-                              cent_y,  cylo, cyhi, &
-                              cent_z,  czlo, czhi, &
-                              vfrac,   vflo, vfhi, &
-                              bcent,    blo,  bhi, &
                               dx) bind(C)
 
       ! Loops bounds
@@ -220,40 +173,18 @@ contains
       integer(c_int),  intent(in   ) ::  slo(3),  shi(3)
       integer(c_int),  intent(in   ) ::  vlo(3),  vhi(3)
       integer(c_int),  intent(in   ) ::  flo(3),  fhi(3)
-      integer(c_int),  intent(in   ) :: axlo(3), axhi(3)
-      integer(c_int),  intent(in   ) :: aylo(3), ayhi(3)
-      integer(c_int),  intent(in   ) :: azlo(3), azhi(3)
-      integer(c_int),  intent(in   ) :: cxlo(3), cxhi(3)
-      integer(c_int),  intent(in   ) :: cylo(3), cyhi(3)
-      integer(c_int),  intent(in   ) :: czlo(3), czhi(3)
-      integer(c_int),  intent(in   ) :: vflo(3), vfhi(3)
-      integer(c_int),  intent(in   ) ::  blo(3),  bhi(3)
 
       ! Grid
       real(rt), intent(in   ) :: dx(3)
 
       ! Arrays
-      real(rt), intent(in   ) ::                                  &
-           &     vel(  vlo(1):  vhi(1),  vlo(2):  vhi(2),  vlo(3):  vhi(3),3), &
-           & afrac_x( axlo(1): axhi(1), axlo(2): axhi(2), axlo(3): axhi(3)  ), &
-           & afrac_y( aylo(1): ayhi(1), aylo(2): ayhi(2), aylo(3): ayhi(3)  ), &
-           & afrac_z( azlo(1): azhi(1), azlo(2): azhi(2), azlo(3): azhi(3)  ), &
-           &  cent_x( cxlo(1): cxhi(1), cxlo(2): cxhi(2), cxlo(3): cxhi(3),2), &
-           &  cent_y( cylo(1): cyhi(1), cylo(2): cyhi(2), cylo(3): cyhi(3),2), &
-           &  cent_z( czlo(1): czhi(1), czlo(2): czhi(2), czlo(3): czhi(3),2), &
-           &   vfrac( vflo(1): vfhi(1), vflo(2): vfhi(2), vflo(3): vfhi(3)  ), &
-           &   bcent(  blo(1):  bhi(1),  blo(2):  bhi(2),  blo(3):  bhi(3),3)
-
-      real(rt),  intent(inout) ::                                 &
-         vort(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3))
-
-      integer,  intent(in   ) :: &
-         flags(flo(1):fhi(1),flo(2):fhi(2),flo(3):fhi(3))
+      real(rt), intent(in   ) ::   vel(vlo(1):vhi(1), vlo(2): vhi(2), vlo(3):vhi(3), 3)
+      real(rt), intent(inout) ::  vort(slo(1):shi(1), slo(2): shi(2), slo(3):shi(3)   )
+      integer,  intent(in   ) :: flags(flo(1):fhi(1), flo(2): fhi(2), flo(3):fhi(3)   )
 
       integer(c_int) :: i, j, k
       real(rt)       :: idx, idy, idz
       real(rt)       :: uy, uz, vx, vz, wx, wy
-      real(rt)       :: gradu(9)
       real(rt)       :: c0, c1, c2
 
       ! Coefficients for one-sided difference estimation
@@ -275,45 +206,24 @@ contains
 
                else if (is_single_valued_cell(flags(i,j,k))) then
 
-                  ! Compute the velocity gradients at the cell centroid
-                  ! 
-                  call compute_eb_gradu(gradu, dx, i, j, k, & 
-                                        vel, vlo, vhi, &
-                                        bcent, blo, bhi, &
-                                        afrac_x, axlo, axhi, & 
-                                        afrac_y, aylo, ayhi, & 
-                                        afrac_z, azlo, azhi, 1)
-                  uy = gradu(2) * idy
-                  uz = gradu(3) * idz
-                  !
-                  vx = gradu(4) * idx
-                  vz = gradu(6) * idz
-                  !
-                  wx = gradu(7) * idx
-                  wy = gradu(8) * idy
-
-                  vort(i,j,k) = sqrt((wy-vz)**2+(uz-wx)**2+(vx-uy)**2)
-
-               else
-
-                  ! Need to check if there are cut cells in neighbours --
+                  ! Need to check if there are covered cells in neighbours --
                   ! -- if so, use one-sided difference computation (but still quadratic)
 
-                  if ( is_single_valued_cell(flags(i+1,j,k)) ) then 
+                  if ( is_covered_cell(flags(i+1,j,k)) ) then 
 
-                     ! Cut cell to the right, go fish left
+                     ! Covered cell to the right, go fish left
                      vx = - (c0 * vel(i,j,k,2) + c1 * vel(i-1,j,k,2) + c2 * vel(i-2,j,k,2)) * idx
                      wx = - (c0 * vel(i,j,k,3) + c1 * vel(i-1,j,k,3) + c2 * vel(i-2,j,k,3)) * idx
 
-                  else if ( is_single_valued_cell(flags(i-1,j,k)) ) then 
+                  else if ( is_covered_cell(flags(i-1,j,k)) ) then 
 
-                     ! Cut cell to the left, go fish right
+                     ! Covered cell to the left, go fish right
                      vx = (c0 * vel(i,j,k,2) + c1 * vel(i+1,j,k,2) + c2 * vel(i+2,j,k,2)) * idx
                      wx = (c0 * vel(i,j,k,3) + c1 * vel(i+1,j,k,3) + c2 * vel(i+2,j,k,3)) * idx
 
                   else
 
-                     ! No cut cells right or left, use standard stencil
+                     ! No covered cells right or left, use standard stencil
                      vx = half * (vel(i+1,j,k,2) - vel(i-1,j,k,2)) * idx
                      wx = half * (vel(i+1,j,k,3) - vel(i-1,j,k,3)) * idx
 
@@ -321,12 +231,12 @@ contains
                                                                    
                   ! Do the same in y-direction 
 
-                  if ( is_single_valued_cell(flags(i,j+1,k)) ) then 
+                  if ( is_covered_cell(flags(i,j+1,k)) ) then 
 
                      uy = - (c0 * vel(i,j,k,1) + c1 * vel(i,j-1,k,1) + c2 * vel(i,j-2,k,1)) * idy
                      wy = - (c0 * vel(i,j,k,3) + c1 * vel(i,j-1,k,3) + c2 * vel(i,j-2,k,3)) * idy
 
-                  else if ( is_single_valued_cell(flags(i,j-1,k)) ) then 
+                  else if ( is_covered_cell(flags(i,j-1,k)) ) then 
 
                      uy = (c0 * vel(i,j,k,1) + c1 * vel(i,j+1,k,1) + c2 * vel(i,j+2,k,1)) * idy
                      wy = (c0 * vel(i,j,k,3) + c1 * vel(i,j+1,k,3) + c2 * vel(i,j+2,k,3)) * idy
@@ -340,24 +250,36 @@ contains
 
                   ! Do the same in z-direction 
 
-                  if ( is_single_valued_cell(flags(i,j,k+1)) ) then 
+                  if ( is_covered_cell(flags(i,j,k+1)) ) then 
 
                      uz = - (c0 * vel(i,j,k,1) + c1 * vel(i,j,k-1,1) + c2 * vel(i,j,k-2,1)) * idz
                      vz = - (c0 * vel(i,j,k,2) + c1 * vel(i,j,k-1,2) + c2 * vel(i,j,k-2,2)) * idz
 
-                  else if ( is_single_valued_cell(flags(i,j,k-1)) ) then 
+                  else if ( is_covered_cell(flags(i,j,k-1)) ) then 
 
                      uz = (c0 * vel(i,j,k,1) + c1 * vel(i,j,k+1,1) + c2 * vel(i,j,k+2,1)) * idz
                      vz = (c0 * vel(i,j,k,2) + c1 * vel(i,j,k+1,2) + c2 * vel(i,j,k+2,2)) * idz
                                                                    
                   else
 
-                     uz = (vel(i  ,j  ,k+1,1) - vel(i  ,j  ,k-1,1)) * idz * half
-                     vz = (vel(i  ,j  ,k+1,2) - vel(i  ,j  ,k-1,2)) * idz * half
+                     uz = half * (vel(i,j,k+1,1) - vel(i,j,k-1,1)) * idz
+                     vz = half * (vel(i,j,k+1,2) - vel(i,j,k-1,2)) * idz
                                                                    
                   end if
                   
                   ! Finally, compute the vorticity magnitude
+                  vort(i,j,k) = sqrt((wy-vz)**2 + (uz-wx)**2 + (vx-uy)**2)
+
+
+               else
+
+                  vx = half * (vel(i+1,j,k,2) - vel(i-1,j,k,2)) * idx
+                  wx = half * (vel(i+1,j,k,3) - vel(i-1,j,k,3)) * idx
+                  uy = half * (vel(i,j+1,k,1) - vel(i,j-1,k,1)) * idy
+                  wy = half * (vel(i,j+1,k,3) - vel(i,j-1,k,3)) * idy
+                  uz = (vel(i  ,j  ,k+1,1) - vel(i  ,j  ,k-1,1)) * idz * half
+                  vz = (vel(i  ,j  ,k+1,2) - vel(i  ,j  ,k-1,2)) * idz * half
+                  
                   vort(i,j,k) = sqrt((wy-vz)**2 + (uz-wx)**2 + (vx-uy)**2)
 
                end if
