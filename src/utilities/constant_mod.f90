bce89595f7fcccee67c72830e2319bf1d0ced00c
knutsvk <ksk38@cam.ac.uk>
2019-03-15 17:29:14 +0000

    cleanup param and constant modules

--- src/utilities/constant_mod.f90
+++ src/utilities/constant_mod.f90
@@ -1,42 +1,30 @@
-!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
-!                                                                      C
-!  Module: constant                                                    C
-!  Author: M. Syamlal                                 Date: 5-FEB-92   C
-!                                                                      C
-!  Purpose: Common block containing physical constants and constants   C
-!           used in the numerical technique                            C
-!                                                                      C
-!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
-      MODULE constant
+module constant
 
-         use amrex_fort_module, only : rt => amrex_real
-         use iso_c_binding , only: c_int, c_char
-
-! Modules
-!---------------------------------------------------------------------//
+   use amrex_fort_module, only : rt => amrex_real
+   use iso_c_binding , only: c_int, c_char
 
 ! Gravitational acceleration
-         real(rt) :: gravity(3)
+   real(rt) :: gravity(3)
 
 ! Initial density
-         real(rt) :: ro_0
+   real(rt) :: ro_0
 
 ! Dynamic coefficient of viscosity
-         real(rt) :: mu
+   real(rt) :: mu
 
 ! Flow index
-         real(rt) :: n
+   real(rt) :: n
 
 ! Yield stress
-         real(rt) :: tau_0
+   real(rt) :: tau_0
 
 ! Papanastasiou regularisation parameter
-         real(rt) :: papa_reg
+   real(rt) :: papa_reg
 
 ! Zero-strain-limit viscosity
-         real(rt) :: eta_0
+   real(rt) :: eta_0
 
 ! Fluid type
-         character(:), allocatable :: fluid_model
+   character(:), allocatable :: fluid_model
 
-      END MODULE constant
+end module constant
