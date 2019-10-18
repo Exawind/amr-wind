df9aa9eb940a9352dcf29d90f45ea7e1afe59a12
Ann Almgren <asalmgren@lbl.gov>
2019-10-18 12:55:46 -0700

    Need to keep the ghost cell filling routines for eta for the cases where we have non-constant viscosity.

--- src/rheology/rheology.cpp
+++ src/rheology/rheology.cpp
@@ -2,10 +2,11 @@
 
 #include <incflo.H>
 #include <rheology_F.H>
+#include <boundary_conditions_F.H>
 
 void incflo::ComputeViscosity()
 {
-	BL_PROFILE("incflo::ComputeViscosity");
+    BL_PROFILE("incflo::ComputeViscosity");
 
     for(int lev = 0; lev <= finest_level; lev++)
     {
@@ -14,7 +15,7 @@ void incflo::ComputeViscosity()
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
-        for(MFIter mfi(*vel[lev], true); mfi.isValid(); ++mfi)
+        for(MFIter mfi(*eta[lev], true); mfi.isValid(); ++mfi)
         {
             // Tilebox
             Box bx = mfi.tilebox();
@@ -22,18 +23,27 @@ void incflo::ComputeViscosity()
             const auto& strainrate_arr = strainrate[lev]->array(mfi);
             const auto& viscosity_arr = eta[lev]->array(mfi);
 
-            for(int i = bx.smallEnd(0); i<= bx.bigEnd(0); i++)
-            for(int j = bx.smallEnd(1); j<= bx.bigEnd(1); j++)
-#if (AMREX_SPACEDIM == 2)
-            {
-                viscosity_arr(i,j) = viscosity(strainrate_arr(i,j));
-            }
-#elif (AMREX_SPACEDIM == 3)
-            for(int k = bx.smallEnd(2); k<= bx.bigEnd(2); k++)
+            AMREX_FOR_3D(bx, i, j, k, 
             {
                 viscosity_arr(i,j,k) = viscosity(strainrate_arr(i,j,k));
-            }
-#endif
+            });
         }
+
+        eta[lev]->FillBoundary(geom[lev].periodicity());
+
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion())
+#endif
+        for(MFIter mfi(*density[lev], TilingIfNotGPU()); mfi.isValid(); ++mfi)
+        {
+            fill_bc0(BL_TO_FORTRAN_ANYD((*eta[lev])[mfi]),
+                     bc_ilo[lev]->dataPtr(), bc_ihi[lev]->dataPtr(),
+                     bc_jlo[lev]->dataPtr(), bc_jhi[lev]->dataPtr(),
+                     bc_klo[lev]->dataPtr(), bc_khi[lev]->dataPtr(),
+                     domain.loVect(), domain.hiVect(),
+                     &nghost);
+        }
+
+        eta[lev]->FillBoundary(geom[lev].periodicity());
     }
 }
