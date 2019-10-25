Exawind/amr-wind

commit 7a5a35df5a43bf7d0edc5b161c1f885cc0ccbf99

no need for e_x, e_y and e_z

src/convection/incflo_predict_vels_on_faces.cpp
@@ -86,13 +86,13 @@ incflo::incflo_predict_vels_on_faces ( int lev, Real time,
           // Tilebox
           Box  bx = mfi.tilebox();
 
-          Box ubx = mfi.tilebox(e_x);
-          Box vbx = mfi.tilebox(e_y);
-          Box wbx = mfi.tilebox(e_z);
+          Box ubx = mfi.nodaltilebox(0);
+          Box vbx = mfi.nodaltilebox(1);
+          Box wbx = mfi.nodaltilebox(2);
 
-          Box ubx_grown = mfi.growntilebox(e_x);
-          Box vbx_grown = mfi.growntilebox(e_y);
-          Box wbx_grown = mfi.growntilebox(e_z);
+          Box ubx_grown = mfi.grownnodaltilebox(0);
+          Box vbx_grown = mfi.grownnodaltilebox(1);
+          Box wbx_grown = mfi.grownnodaltilebox(2);
 
           const EBFArrayBox&  vel_fab = static_cast<EBFArrayBox const&>((*vel_in[lev])[mfi]);
           const EBCellFlagFab&  flags = vel_fab.getEBCellFlagFab();
@@ -258,9 +258,9 @@ incflo::incflo_predict_vels_on_faces ( int lev, Real time,
        {
            // Tilebox
           Box  bx = mfi.tilebox();
-          Box ubx = mfi.tilebox(e_x);
-          Box vbx = mfi.tilebox(e_y);
-          Box wbx = mfi.tilebox(e_z);
+          Box ubx = mfi.nodaltilebox(0);
+          Box vbx = mfi.nodaltilebox(1);
+          Box wbx = mfi.nodaltilebox(2);
       
           // Check efficiently if this tile contains any eb stuff
 

src/diffusion/DiffusionOp.cpp
@@ -9,11 +9,6 @@
 
 using namespace amrex;
 
-// Define unit vectors to easily convert indices
-extern const amrex::IntVect e_x;
-extern const amrex::IntVect e_y;
-extern const amrex::IntVect e_z;
-
 //
 // Constructor:
 // We set up everything which doesn't change between timesteps here

src/incflo.H
@@ -227,11 +227,6 @@ private:
 
      int ntrac = 1;
 
-     // Unit vectors in Cartesian space
-     static IntVect e_x;
-     static IntVect e_y;
-     static IntVect e_z;
-
      mutable amrex::Cuda::ManagedVector<amrex::Real> m_bc_u;
      mutable amrex::Cuda::ManagedVector<amrex::Real> m_bc_v;
      mutable amrex::Cuda::ManagedVector<amrex::Real> m_bc_w;

src/incflo.cpp
@@ -6,11 +6,6 @@
 // Need this for TagCutCells
 #include <AMReX_EBAmrUtil.H>
 
-// Define unit vectors for easily convert indices
-amrex::IntVect incflo::e_x(1,0,0);
-amrex::IntVect incflo::e_y(0,1,0);
-amrex::IntVect incflo::e_z(0,0,1);
-
 DiffusionType incflo::m_diff_type = DiffusionType::Implicit;
 
 // Constructor

src/utilities/constants.H
@@ -1,9 +1,0 @@
-#ifndef CONSTANTS_H_
-#define CONSTANTS_H_
-
-// Define unit vectors to easily convert indices
-extern const amrex::IntVect e_x;
-extern const amrex::IntVect e_y;
-extern const amrex::IntVect e_z;
-
-#endif

commit d10479120e566e09a876183ab7846485cf550cc1

fix a bug introduced in 7a5a35df5a43bf7d0edc5b1

src/convection/incflo_predict_vels_on_faces.cpp
@@ -90,9 +90,9 @@ incflo::incflo_predict_vels_on_faces ( int lev, Real time,
           Box vbx = mfi.nodaltilebox(1);
           Box wbx = mfi.nodaltilebox(2);
 
-          Box ubx_grown = mfi.grownnodaltilebox(0);
-          Box vbx_grown = mfi.grownnodaltilebox(1);
-          Box wbx_grown = mfi.grownnodaltilebox(2);
+          Box ubx_grown = mfi.growntilebox(IntVect::TheDimensionVector(0));
+          Box vbx_grown = mfi.growntilebox(IntVect::TheDimensionVector(1));
+          Box wbx_grown = mfi.growntilebox(IntVect::TheDimensionVector(2));
 
           const EBFArrayBox&  vel_fab = static_cast<EBFArrayBox const&>((*vel_in[lev])[mfi]);
           const EBCellFlagFab&  flags = vel_fab.getEBCellFlagFab();