d27f3c0ef2cb7e79babe280eb69324c6bb915bd3
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-10-25 11:31:05 -0700

    indentation in main

--- src/main.cpp
+++ src/main.cpp
@@ -23,48 +23,49 @@ int main(int argc, char* argv[])
     { /* These braces are necessary to ensure amrex::Finalize() can be called without explicitly
         deleting all the incflo member MultiFabs */
 
-    BL_PROFILE_VAR("main()", pmain)
+        BL_PROFILE_VAR("main()", pmain);
 
-    // Issue an error if input file is not given 
-    if(argc < 2) amrex::Abort("Input file must be given as command-line argument.");
+        // Issue an error if input file is not given 
+        if(argc < 2) amrex::Abort("Input file must be given as command-line argument.");
 
-    // Write out the incflo git hash (the AMReX git hash is already written)
-    const char* githash_incflo = buildInfoGetGitHash(1);
-    amrex::Print() << "incflo git hash: " << githash_incflo << "\n";
+        // Write out the incflo git hash (the AMReX git hash is already written)
+        const char* githash_incflo = buildInfoGetGitHash(1);
+        amrex::Print() << "incflo git hash: " << githash_incflo << "\n";
 
-    // Start timing the program
-    Real start_time = ParallelDescriptor::second();
+        // Start timing the program
+        Real start_time = ParallelDescriptor::second();
 
-    // Default constructor. Note inheritance: incflo : AmrCore : AmrMesh. 
-    incflo my_incflo;
+        // Default constructor. Note inheritance: incflo : AmrCore : AmrMesh. 
+        incflo my_incflo;
 
-    // Get boundary conditions from inputs file
-    my_incflo.GetInputBCs();
+        // Get boundary conditions from inputs file
+        my_incflo.GetInputBCs();
 
-    // Set global static pointer to incflo object. Used by fillpatch utility
-    set_ptr_to_incflo(my_incflo);
+        // Set global static pointer to incflo object. Used by fillpatch utility
+        set_ptr_to_incflo(my_incflo);
 
-    // Initialize data, parameters, arrays and derived internals
-    my_incflo.InitData();
+        // Initialize data, parameters, arrays and derived internals
+        my_incflo.InitData();
 
-    // my_incflo.WriteMyEBSurface();
+        // my_incflo.WriteMyEBSurface();
 
-    // Time spent on initialization
-    Real init_time = ParallelDescriptor::second() - start_time;
-    ParallelDescriptor::ReduceRealMax(init_time, ParallelDescriptor::IOProcessorNumber());
+        // Time spent on initialization
+        Real init_time = ParallelDescriptor::second() - start_time;
 
-    // Evolve system to final time 
-    my_incflo.Evolve();
+        // Evolve system to final time
+        my_incflo.Evolve();
 
-    // Time spent in total 
-    Real end_time = ParallelDescriptor::second() - start_time;
-    ParallelDescriptor::ReduceRealMax(end_time, ParallelDescriptor::IOProcessorNumber());
+        // Time spent in total
+        Real end_time = ParallelDescriptor::second() - start_time;
 
-    // Print timing results
-    amrex::Print() << "Time spent in InitData():    " << init_time << std::endl;
-    amrex::Print() << "Time spent in Evolve():      " << end_time - init_time << std::endl;
+        ParallelDescriptor::ReduceRealMax(init_time, ParallelDescriptor::IOProcessorNumber());
+        ParallelDescriptor::ReduceRealMax(end_time, ParallelDescriptor::IOProcessorNumber());
 
-    BL_PROFILE_VAR_STOP(pmain);
+        // Print timing results
+        amrex::Print() << "Time spent in InitData():    " << init_time << std::endl;
+        amrex::Print() << "Time spent in Evolve():      " << end_time - init_time << std::endl;
+
+        BL_PROFILE_VAR_STOP(pmain);
     }
-	amrex::Finalize();
+    amrex::Finalize();
 }
