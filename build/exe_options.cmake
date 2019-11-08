3a1387db1a15bbb944f32cd1e5ed3e051e8db721
Jon Rood <jon.rood@nrel.gov>
2019-11-08 13:15:06 -0700

    Modifying configure script.

--- build/exe_options.cmake
+++
@@ -1,8 +0,0 @@
-#User-specific source files
-#list(APPEND AMR_WIND_EXTRA_SOURCES ${CMAKE_SOURCE_DIR}/Build/probdata.f90)
-#list(APPEND AMR_WIND_EXTRA_SOURCES ${CMAKE_SOURCE_DIR}/Build/bc_fill_nd.F90)
-
-#Compile-time options for executable
-set(AMR_WIND_DIM 3)
-set(AMR_WIND_ENABLE_EB ON)
-set(AMR_WIND_ENABLE_MASA OFF)
