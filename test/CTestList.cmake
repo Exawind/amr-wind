4bb2101def6e822edec07b43e0a9560d4dc49037
Jon Rood <jon.rood@nrel.gov>
2019-11-12 12:34:01 -0700

    Only generate AMReX_buildInfo.cpp once.

--- test/CTestList.cmake
+++ test/CTestList.cmake
@@ -10,33 +10,33 @@ include(ProcessorCount)
 ProcessorCount(PROCESSES)
 
 # Set TOLERANCE for testing
-if(NOT ${TEST_TOLERANCE} STREQUAL "")
-  set(TOLERANCE ${TEST_TOLERANCE}) # User defined
-else(NOT ${TEST_TOLERANCE} STREQUAL "")
-  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"
-        OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
-      set(TOLERANCE "1e-3")
-    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-      set(TOLERANCE "1e-3")
-    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
-      set(TOLERANCE "1e-2")
-    else()
-      set(TOLERANCE "1e-8") # Mac default
-    endif()
-  elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-      set(TOLERANCE "1e-5")
-    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-      set(TOLERANCE "1e-15")
-    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
-      set(TOLERANCE "1e-2")
-    else()
-      set(TOLERANCE "1e-8") # Linux default
-    endif()
-  endif()
-endif()
-message(STATUS "Using test tolerance of ${TOLERANCE}")
+#if(NOT ${TEST_TOLERANCE} STREQUAL "")
+#  set(TOLERANCE ${TEST_TOLERANCE}) # User defined
+#else(NOT ${TEST_TOLERANCE} STREQUAL "")
+#  if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+#    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"
+#        OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+#      set(TOLERANCE "1e-3")
+#    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+#      set(TOLERANCE "1e-3")
+#    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+#      set(TOLERANCE "1e-2")
+#    else()
+#      set(TOLERANCE "1e-8") # Mac default
+#    endif()
+#  elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+#    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+#      set(TOLERANCE "1e-5")
+#    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+#      set(TOLERANCE "1e-15")
+#    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+#      set(TOLERANCE "1e-2")
+#    else()
+#      set(TOLERANCE "1e-8") # Linux default
+#    endif()
+#  endif()
+#endif()
+#message(STATUS "Using test tolerance of ${TOLERANCE}")
 
 #=============================================================================
 # Functions for adding tests / Categories of tests
@@ -75,8 +75,6 @@ function(add_test_r TEST_NAME NP)
     if(TEST_WITH_FEXTREMA)
       set(FEXTREMA_COMMAND "&& ${FEXTREMA} ${PLOT_TEST} > ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.ext && ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/test_files/fextrema_compare.py -f ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.ext -g ${CURRENT_TEST_SOURCE_DIR}/${TEST_NAME}.ext.gold -t ${TOLERANCE}")
     endif()
-    # Place the exe in the correct working directory
-    #set_target_properties(${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/")
     # Add test and actual test commands to CTest database
     add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${CMAKE_BINARY_DIR}/${amr_wind_exe_name} ${MPIEXEC_POSTFLAGS} ${CURRENT_TEST_BINARY_DIR}/${TEST_NAME}.i ${RUNTIME_OPTIONS} ${FEXTREMA_COMMAND} ${FCOMPARE_COMMAND}")
     # Set properties for test
@@ -90,8 +88,6 @@ function(add_test_u TEST_NAME NP)
     set(CURRENT_TEST_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/test_files/${TEST_NAME})
     # Make working directory for test
     file(MAKE_DIRECTORY ${CURRENT_TEST_BINARY_DIR})
-    # Place the exe in the correct working directory
-    #set_target_properties(${TEST_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CURRENT_TEST_BINARY_DIR}/")
     # Add test and commands to CTest database
     add_test(${TEST_NAME} sh -c "${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${NP} ${MPIEXEC_PREFLAGS} ${CURRENT_TEST_BINARY_DIR}/${amr_wind_unit_test_exe_name}")
     # Set properties for test
