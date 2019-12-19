35185c3a83639481e2377be7ff861c8b01fee663
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-12-18 21:40:59 -0800

    fix ext_dir for predict_vels_on_faces

--- src/convection/incflo_convection_K.cpp
+++ src/convection/incflo_convection_K.cpp
@@ -9,59 +9,179 @@ void incflo::predict_vels_on_faces (int lev, Box const& ubx, Box const& vbx, Box
 {
     constexpr Real small = 1.e-10;
 
-    amrex::ParallelFor(ubx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    const Box& domain_box = geom[lev].Domain();
+    const int domain_ilo = domain_box.smallEnd(0);
+    const int domain_ihi = domain_box.bigEnd(0);
+    const int domain_jlo = domain_box.smallEnd(1);
+    const int domain_jhi = domain_box.bigEnd(1);
+    const int domain_klo = domain_box.smallEnd(2);
+    const int domain_khi = domain_box.bigEnd(2);
+
+    auto const bc_ilo = m_bc_type[Orientation(Direction::x,Orientation::low)];
+    auto const bc_ihi = m_bc_type[Orientation(Direction::x,Orientation::high)];
+    auto const bc_jlo = m_bc_type[Orientation(Direction::y,Orientation::low)];
+    auto const bc_jhi = m_bc_type[Orientation(Direction::y,Orientation::high)];
+    auto const bc_klo = m_bc_type[Orientation(Direction::z,Orientation::low)];
+    auto const bc_khi = m_bc_type[Orientation(Direction::z,Orientation::high)];
+
+    bool extdir_ilo = (bc_ilo == BC::mass_inflow) or (bc_ilo == BC::no_slip_wall);
+    bool extdir_ihi = (bc_ihi == BC::mass_inflow) or (bc_ihi == BC::no_slip_wall);
+    bool extdir_jlo = (bc_jlo == BC::mass_inflow) or (bc_jlo == BC::no_slip_wall);
+    bool extdir_jhi = (bc_jhi == BC::mass_inflow) or (bc_jhi == BC::no_slip_wall);
+    bool extdir_klo = (bc_klo == BC::mass_inflow) or (bc_klo == BC::no_slip_wall);
+    bool extdir_khi = (bc_khi == BC::mass_inflow) or (bc_khi == BC::no_slip_wall);
+
+    // At an ext_dir boundary, the boundary value is on the face, not cell center.
+
+    if ((extdir_ilo and domain_ilo >= ubx.smallEnd(0)-1) or
+        (extdir_ihi and domain_ihi <= ubx.bigEnd(0)))
     {
-        Real upls = vcc(i  ,j,k,0) - 0.5 * incflo_xslope(i  ,j,k,0,vcc);
-        Real umns = vcc(i-1,j,k,0) + 0.5 * incflo_xslope(i-1,j,k,0,vcc);
-        if (umns < 0.0 and upls > 0.0) {
-            u(i,j,k) = 0.0;
-        } else {
-            Real avg = 0.5 * (upls + umns);
-            if (std::abs(avg) < small) {
+        amrex::ParallelFor(ubx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            Real upls = vcc(i,j,k,0) - 0.5 * incflo_xslope_extdir
+                (i,j,k,0,vcc, extdir_ilo, extdir_ihi, domain_ilo, domain_ihi);
+            Real umns = vcc(i-1,j,k,0) + 0.5 * incflo_xslope_extdir
+                (i-1,j,k,0,vcc, extdir_ilo, extdir_ihi, domain_ilo, domain_ihi);
+            if (umns < 0.0 and upls > 0.0) {
                 u(i,j,k) = 0.0;
-            } else if (avg > 0.0) {
-                u(i,j,k) = umns;
             } else {
-                u(i,j,k) = upls;
+                Real avg = 0.5 * (upls + umns);
+                if (std::abs(avg) < small) {
+                    u(i,j,k) = 0.0;
+                } else if (avg > 0.0) {
+                    u(i,j,k) = umns;
+                } else {
+                    u(i,j,k) = upls;
+                }
             }
-        }
-    });
 
-    amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+            if (extdir_ilo and i == domain_ilo) {
+                u(i,j,k) = vcc(i-1,j,k,0);
+            } else if (extdir_ihi and i == domain_ihi+1) {
+                u(i,j,k) = vcc(i,j,k,0);
+            }
+        });
+    }
+    else
     {
-        Real vpls = vcc(i,j  ,k,1) - 0.5 * incflo_yslope(i,j  ,k,1,vcc);
-        Real vmns = vcc(i,j-1,k,1) + 0.5 * incflo_yslope(i,j-1,k,1,vcc);
-        if (vmns < 0.0 and vpls > 0.0) {
-            v(i,j,k) = 0.0;
-        } else {
-            Real avg = 0.5 * (vpls + vmns);
-            if (std::abs(avg) < small) {
+        amrex::ParallelFor(ubx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            Real upls = vcc(i  ,j,k,0) - 0.5 * incflo_xslope(i  ,j,k,0,vcc);
+            Real umns = vcc(i-1,j,k,0) + 0.5 * incflo_xslope(i-1,j,k,0,vcc);
+            if (umns < 0.0 and upls > 0.0) {
+                u(i,j,k) = 0.0;
+            } else {
+                Real avg = 0.5 * (upls + umns);
+                if (std::abs(avg) < small) {
+                    u(i,j,k) = 0.0;
+                } else if (avg > 0.0) {
+                    u(i,j,k) = umns;
+                } else {
+                    u(i,j,k) = upls;
+                }
+            }
+        });
+    }
+
+    if ((extdir_jlo and domain_jlo >= vbx.smallEnd(1)-1) or
+        (extdir_jhi and domain_jhi <= vbx.bigEnd(1)))
+    {
+        amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            Real vpls = vcc(i,j,k,1) - 0.5 * incflo_yslope_extdir
+                (i,j,k,1,vcc, extdir_jlo, extdir_jhi, domain_jlo, domain_jhi);
+            Real vmns = vcc(i,j-1,k,1) + 0.5 * incflo_yslope_extdir
+                (i,j-1,k,1,vcc, extdir_jlo, extdir_jhi, domain_jlo, domain_jhi);
+            if (vmns < 0.0 and vpls > 0.0) {
                 v(i,j,k) = 0.0;
-            } else if (avg > 0.0) {
-                v(i,j,k) = vmns;
             } else {
-                v(i,j,k) = vpls;
+                Real avg = 0.5 * (vpls + vmns);
+                if (std::abs(avg) < small) {
+                    v(i,j,k) = 0.0;
+                } else if (avg > 0.0) {
+                    v(i,j,k) = vmns;
+                } else {
+                    v(i,j,k) = vpls;
+                }
             }
-        }
-    });
 
-    amrex::ParallelFor(wbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-    {
-        Real wpls = vcc(i,j,k  ,2) - 0.5 * incflo_zslope(i,j,k  ,2,vcc);
-        Real wmns = vcc(i,j,k-1,2) + 0.5 * incflo_zslope(i,j,k-1,2,vcc);
-        if (wmns < 0.0 and wpls > 0.0) {
-            w(i,j,k) = 0.0;
-        } else {
-            Real avg = 0.5 * (wpls + wmns);
-            if (std::abs(avg) < small) {
-                w(i,j,k) = 0.0;
-            } else if (avg > 0.0) {
-                w(i,j,k) = wmns;
-            } else {
-                w(i,j,k) = wpls;
+            if (extdir_jlo and j == domain_jlo) {
+                v(i,j,k) = vcc(i,j-1,k,1);
+            } else if (extdir_jhi and j == domain_jhi+1) {
+                v(i,j,k) = vcc(i,j,k,1);
             }
-        }
-    });
+        });
+    }
+    else
+    {
+        amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            Real vpls = vcc(i,j  ,k,1) - 0.5 * incflo_yslope(i,j  ,k,1,vcc);
+            Real vmns = vcc(i,j-1,k,1) + 0.5 * incflo_yslope(i,j-1,k,1,vcc);
+            if (vmns < 0.0 and vpls > 0.0) {
+                v(i,j,k) = 0.0;
+            } else {
+                Real avg = 0.5 * (vpls + vmns);
+                if (std::abs(avg) < small) {
+                    v(i,j,k) = 0.0;
+                } else if (avg > 0.0) {
+                    v(i,j,k) = vmns;
+                } else {
+                    v(i,j,k) = vpls;
+                }
+            }
+        });
+    }
+
+    if ((extdir_klo and domain_klo >= wbx.smallEnd(2)-1) or
+        (extdir_khi and domain_khi <= wbx.bigEnd(2)))
+    {
+        amrex::ParallelFor(wbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            Real wpls = vcc(i,j,k,2) - 0.5 * incflo_zslope_extdir
+                (i,j,k,2,vcc, extdir_klo, extdir_khi, domain_klo, domain_khi);
+            Real wmns = vcc(i,j,k-1,2) + 0.5 * incflo_zslope_extdir(
+                i,j,k-1,2,vcc, extdir_klo, extdir_khi, domain_klo, domain_khi);
+            if (wmns < 0.0 and wpls > 0.0) {
+                w(i,j,k) = 0.0;
+            } else {
+                Real avg = 0.5 * (wpls + wmns);
+                if (std::abs(avg) < small) {
+                    w(i,j,k) = 0.0;
+                } else if (avg > 0.0) {
+                    w(i,j,k) = wmns;
+                } else {
+                    w(i,j,k) = wpls;
+                }
+            }
+
+            if (extdir_klo and k == domain_klo) {
+                w(i,j,k) = vcc(i,j,k-1,2);
+            } else if (extdir_khi and k == domain_khi+1) {
+                w(i,j,k) = vcc(i,j,k,2);
+            }
+        });
+    }
+    else
+    {
+        amrex::ParallelFor(wbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            Real wpls = vcc(i,j,k  ,2) - 0.5 * incflo_zslope(i,j,k  ,2,vcc);
+            Real wmns = vcc(i,j,k-1,2) + 0.5 * incflo_zslope(i,j,k-1,2,vcc);
+            if (wmns < 0.0 and wpls > 0.0) {
+                w(i,j,k) = 0.0;
+            } else {
+                Real avg = 0.5 * (wpls + wmns);
+                if (std::abs(avg) < small) {
+                    w(i,j,k) = 0.0;
+            } else if (avg > 0.0) {
+                    w(i,j,k) = wmns;
+                } else {
+                    w(i,j,k) = wpls;
+                }
+            }
+        });
+    }
 }
 
 
@@ -77,18 +197,57 @@ void incflo::predict_vels_on_faces_eb (int lev, Box const& ccbx,
 {
     constexpr Real small = 1.e-10;
 
+    const Box& domain_box = geom[lev].Domain();
+    const int domain_ilo = domain_box.smallEnd(0);
+    const int domain_ihi = domain_box.bigEnd(0);
+    const int domain_jlo = domain_box.smallEnd(1);
+    const int domain_jhi = domain_box.bigEnd(1);
+    const int domain_klo = domain_box.smallEnd(2);
+    const int domain_khi = domain_box.bigEnd(2);
+
+    auto const bc_ilo = m_bc_type[Orientation(Direction::x,Orientation::low)];
+    auto const bc_ihi = m_bc_type[Orientation(Direction::x,Orientation::high)];
+    auto const bc_jlo = m_bc_type[Orientation(Direction::y,Orientation::low)];
+    auto const bc_jhi = m_bc_type[Orientation(Direction::y,Orientation::high)];
+    auto const bc_klo = m_bc_type[Orientation(Direction::z,Orientation::low)];
+    auto const bc_khi = m_bc_type[Orientation(Direction::z,Orientation::high)];
+
+    bool extdir_ilo = (bc_ilo == BC::mass_inflow) or (bc_ilo == BC::no_slip_wall);
+    bool extdir_ihi = (bc_ihi == BC::mass_inflow) or (bc_ihi == BC::no_slip_wall);
+    bool extdir_jlo = (bc_jlo == BC::mass_inflow) or (bc_jlo == BC::no_slip_wall);
+    bool extdir_jhi = (bc_jhi == BC::mass_inflow) or (bc_jhi == BC::no_slip_wall);
+    bool extdir_klo = (bc_klo == BC::mass_inflow) or (bc_klo == BC::no_slip_wall);
+    bool extdir_khi = (bc_khi == BC::mass_inflow) or (bc_khi == BC::no_slip_wall);
+
+    // At an ext_dir boundary, the boundary value is on the face, not cell center.
+
     FArrayBox tmp(amrex::grow(ccbx,1),2);
     Elixir eli = tmp.elixir();
 
     Array4<Real> const& upls = tmp.array(0);
     Array4<Real> const& umns = tmp.array(1);
 
-    amrex::ParallelFor(amrex::grow(amrex::grow(ubx,1,1),2,1),
-    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    if ((extdir_ilo and domain_ilo >= ubx.smallEnd(0)-1) or
+        (extdir_ihi and domain_ihi <= ubx.bigEnd(0)))
     {
-        upls(i,j,k) = vcc(i  ,j,k,0) - 0.5 * incflo_xslope(i  ,j,k,0,vcc);
-        umns(i,j,k) = vcc(i-1,j,k,0) + 0.5 * incflo_xslope(i-1,j,k,0,vcc);
-    });
+        amrex::ParallelFor(amrex::grow(amrex::grow(ubx,1,1),2,1),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            upls(i,j,k) = vcc(i,j,k,0) - 0.5 * incflo_xslope_extdir_eb
+                (i,j,k,0,vcc,flag, extdir_ilo, extdir_ihi, domain_ilo, domain_ihi);
+            umns(i,j,k) = vcc(i-1,j,k,0) + 0.5 * incflo_xslope_extdir_eb
+                (i-1,j,k,0,vcc,flag, extdir_ilo, extdir_ihi, domain_ilo, domain_ihi);
+        });
+    }
+    else
+    {
+        amrex::ParallelFor(amrex::grow(amrex::grow(ubx,1,1),2,1),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            upls(i,j,k) = vcc(i  ,j,k,0) - 0.5 * incflo_xslope_eb(i  ,j,k,0,vcc,flag);
+            umns(i,j,k) = vcc(i-1,j,k,0) + 0.5 * incflo_xslope_eb(i-1,j,k,0,vcc,flag);
+        });
+    }
 
     amrex::ParallelFor(ubx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
@@ -126,6 +285,12 @@ void incflo::predict_vels_on_faces_eb (int lev, Box const& ccbx,
                     u(i,j,k) = upls_on_centroid;
                 }
             }
+
+            if (extdir_ilo and i == domain_ilo) {
+                u(i,j,k) = vcc(i-1,j,k,0);
+            } else if (extdir_ihi and i == domain_ihi+1) {
+                u(i,j,k) = vcc(i,j,k,0);
+            }
         } else {
             u(i,j,k) = 0.0;
         }
@@ -134,12 +299,27 @@ void incflo::predict_vels_on_faces_eb (int lev, Box const& ccbx,
     Array4<Real> const& vpls = upls;
     Array4<Real> const& vmns = umns;
 
-    amrex::ParallelFor(amrex::grow(amrex::grow(vbx,0,1),2,1),
-    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    if ((extdir_jlo and domain_jlo >= vbx.smallEnd(1)-1) or
+        (extdir_jhi and domain_jhi <= vbx.bigEnd(1)))
     {
-        vpls(i,j,k) = vcc(i,j  ,k,1) - 0.5 * incflo_yslope(i,j  ,k,1,vcc);
-        vmns(i,j,k) = vcc(i,j-1,k,1) + 0.5 * incflo_yslope(i,j-1,k,1,vcc);
-    });
+        amrex::ParallelFor(amrex::grow(amrex::grow(vbx,0,1),2,1),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            vpls(i,j,k) = vcc(i,j,k,1) - 0.5 * incflo_yslope_extdir_eb
+                (i,j,k,1,vcc,flag, extdir_jlo, extdir_jhi, domain_jlo, domain_jhi);
+            vmns(i,j,k) = vcc(i,j-1,k,1) + 0.5 * incflo_yslope_extdir_eb
+                (i,j-1,k,1,vcc,flag, extdir_jlo, extdir_jhi, domain_jlo, domain_jhi);
+        });
+    }
+    else
+    {
+        amrex::ParallelFor(amrex::grow(amrex::grow(vbx,0,1),2,1),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            vpls(i,j,k) = vcc(i,j  ,k,1) - 0.5 * incflo_yslope_eb(i,j  ,k,1,vcc,flag);
+            vmns(i,j,k) = vcc(i,j-1,k,1) + 0.5 * incflo_yslope_eb(i,j-1,k,1,vcc,flag);
+        });
+    }
 
     amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
@@ -177,6 +357,12 @@ void incflo::predict_vels_on_faces_eb (int lev, Box const& ccbx,
                     v(i,j,k) = vpls_on_centroid;
                 }
             }
+
+            if (extdir_jlo and j == domain_jlo) {
+                v(i,j,k) = vcc(i,j-1,k,1);
+            } else if (extdir_jhi and j == domain_jhi+1) {
+                v(i,j,k) = vcc(i,j,k,1);
+            }
         } else {
             v(i,j,k) = 0.0;
         }
@@ -185,12 +371,27 @@ void incflo::predict_vels_on_faces_eb (int lev, Box const& ccbx,
     Array4<Real> const& wpls = upls;
     Array4<Real> const& wmns = umns;
 
-    amrex::ParallelFor(amrex::grow(amrex::grow(vbx,0,1),1,1),
-    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+    if ((extdir_klo and domain_klo >= wbx.smallEnd(2)-1) or
+        (extdir_khi and domain_khi <= wbx.bigEnd(2)))
     {
-        wpls(i,j,k) = vcc(i,j,k  ,2) - 0.5 * incflo_zslope(i,j,k  ,2,vcc);
-        wmns(i,j,k) = vcc(i,j,k-1,2) + 0.5 * incflo_zslope(i,j,k-1,2,vcc);
-    });
+        amrex::ParallelFor(amrex::grow(amrex::grow(vbx,0,1),1,1),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            wpls(i,j,k) = vcc(i,j,k,2) - 0.5 * incflo_zslope_extdir_eb
+                (i,j,k,2,vcc,flag, extdir_klo, extdir_khi, domain_klo, domain_khi);
+            wmns(i,j,k) = vcc(i,j,k-1,2) + 0.5 * incflo_zslope_extdir_eb
+                (i,j,k-1,2,vcc,flag, extdir_klo, extdir_khi, domain_klo, domain_khi);
+        });
+    }
+    else
+    {
+        amrex::ParallelFor(amrex::grow(amrex::grow(vbx,0,1),1,1),
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            wpls(i,j,k) = vcc(i,j,k  ,2) - 0.5 * incflo_zslope_eb(i,j,k  ,2,vcc,flag);
+            wmns(i,j,k) = vcc(i,j,k-1,2) + 0.5 * incflo_zslope_eb(i,j,k-1,2,vcc,flag);
+        });
+    }
 
     amrex::ParallelFor(wbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
@@ -227,6 +428,12 @@ void incflo::predict_vels_on_faces_eb (int lev, Box const& ccbx,
                 } else {
                     w(i,j,k) = wpls_on_centroid;
                 }
+            }
+
+            if (extdir_klo and k == domain_klo) {
+                w(i,j,k) = vcc(i,j,k-1,2);
+            } else if (extdir_khi and k == domain_khi+1) {
+                w(i,j,k) = vcc(i,j,k,2);
             }
         } else {
             w(i,j,k) = 0.0;
