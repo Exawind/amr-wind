cbcd6fadfc6a88530e9968d21317360e320b11d8
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-01-17 20:01:12 -0800

    compute div tau

--- src/diffusion/DiffusionTensorOp.cpp
+++ src/diffusion/DiffusionTensorOp.cpp
@@ -2,6 +2,10 @@
 #include <DiffusionTensorOp.H>
 #include <AMReX_ParmParse.H>
 
+#ifdef AMREX_USE_EB
+#include <AMReX_EB_utils.H>
+#endif
+
 using namespace amrex;
 
 DiffusionTensorOp::DiffusionTensorOp (incflo* a_incflo)
@@ -11,8 +15,10 @@ DiffusionTensorOp::DiffusionTensorOp (incflo* a_incflo)
 
     int finest_level = m_incflo->finestLevel();
 
-    LPInfo info;
-    info.setMaxCoarseningLevel(m_mg_max_coarsening_level);
+    LPInfo info_solve;
+    info_solve.setMaxCoarseningLevel(m_mg_max_coarsening_level);
+    LPInfo info_apply;
+    info_apply.setMaxCoarseningLevel(0);
 #ifdef AMREX_USE_EB
     if (!m_incflo->EBFactory(0).isAllRegular())
     {
@@ -20,24 +26,42 @@ DiffusionTensorOp::DiffusionTensorOp (incflo* a_incflo)
         for (int lev = 0; lev <= finest_level; ++lev) {
             ebfact.push_back(&(m_incflo->EBFactory(lev)));
         }
-        m_eb_op.reset(new MLEBTensorOp(m_incflo->Geom(0,finest_level),
-                                       m_incflo->boxArray(0,finest_level),
-                                       m_incflo->DistributionMap(0,finest_level),
-                                       info, ebfact));
-        m_eb_op->setMaxOrder(m_mg_maxorder);
-        m_eb_op->setDomainBC(m_incflo->get_diffuse_tensor_bc(Orientation::low),
-                             m_incflo->get_diffuse_tensor_bc(Orientation::high));
+        m_eb_solve_op.reset(new MLEBTensorOp(m_incflo->Geom(0,finest_level),
+                                             m_incflo->boxArray(0,finest_level),
+                                             m_incflo->DistributionMap(0,finest_level),
+                                             info_solve, ebfact));
+        m_eb_solve_op->setMaxOrder(m_mg_maxorder);
+        m_eb_solve_op->setDomainBC(m_incflo->get_diffuse_tensor_bc(Orientation::low),
+                                   m_incflo->get_diffuse_tensor_bc(Orientation::high));
+        if (m_incflo->need_divtau()) {
+            m_eb_apply_op.reset(new MLEBTensorOp(m_incflo->Geom(0,finest_level),
+                                                 m_incflo->boxArray(0,finest_level),
+                                                 m_incflo->DistributionMap(0,finest_level),
+                                                 info_apply, ebfact));
+            m_eb_apply_op->setMaxOrder(m_mg_maxorder);
+            m_eb_apply_op->setDomainBC(m_incflo->get_diffuse_tensor_bc(Orientation::low),
+                                       m_incflo->get_diffuse_tensor_bc(Orientation::high));
+        }
     }
     else
 #endif
     {
-        m_reg_op.reset(new MLTensorOp(m_incflo->Geom(0,finest_level),
-                                      m_incflo->boxArray(0,finest_level),
-                                      m_incflo->DistributionMap(0,finest_level),
-                                      info));
-        m_reg_op->setMaxOrder(m_mg_maxorder);
-        m_reg_op->setDomainBC(m_incflo->get_diffuse_tensor_bc(Orientation::low),
-                              m_incflo->get_diffuse_tensor_bc(Orientation::high));
+        m_reg_solve_op.reset(new MLTensorOp(m_incflo->Geom(0,finest_level),
+                                            m_incflo->boxArray(0,finest_level),
+                                            m_incflo->DistributionMap(0,finest_level),
+                                            info_solve));
+        m_reg_solve_op->setMaxOrder(m_mg_maxorder);
+        m_reg_solve_op->setDomainBC(m_incflo->get_diffuse_tensor_bc(Orientation::low),
+                                    m_incflo->get_diffuse_tensor_bc(Orientation::high));
+        if (m_incflo->need_divtau()) {
+            m_reg_apply_op.reset(new MLTensorOp(m_incflo->Geom(0,finest_level),
+                                                m_incflo->boxArray(0,finest_level),
+                                                m_incflo->DistributionMap(0,finest_level),
+                                                info_apply));
+            m_reg_apply_op->setMaxOrder(m_mg_maxorder);
+            m_reg_apply_op->setDomainBC(m_incflo->get_diffuse_tensor_bc(Orientation::low),
+                                        m_incflo->get_diffuse_tensor_bc(Orientation::high));
+        }
     }
 }
 
@@ -81,22 +105,22 @@ DiffusionTensorOp::diffuse_velocity (Vector<MultiFab*> const& velocity,
     const int finest_level = m_incflo->finestLevel();
 
 #ifdef AMREX_USE_EB
-    if (m_eb_op)
+    if (m_eb_solve_op)
     {
-        m_eb_op->setScalars(1.0, dt);
+        m_eb_solve_op->setScalars(1.0, dt);
         for (int lev = 0; lev <= finest_level; ++lev) {
-            m_eb_op->setACoeffs(lev, *density[lev]);
-            m_eb_op->setShearViscosity(lev, m_incflo->m_mu);
-            m_eb_op->setEBShearViscosity(lev, m_incflo->m_mu);
+            m_eb_solve_op->setACoeffs(lev, *density[lev]);
+            m_eb_solve_op->setShearViscosity(lev, m_incflo->m_mu);
+            m_eb_solve_op->setEBShearViscosity(lev, m_incflo->m_mu);
         }
     }
     else
 #endif
     {
-        m_reg_op->setScalars(1.0, dt);
+        m_reg_solve_op->setScalars(1.0, dt);
         for (int lev = 0; lev <= finest_level; ++lev) {
-            m_reg_op->setACoeffs(lev, *density[lev]);
-            m_reg_op->setShearViscosity(lev, m_incflo->m_mu);
+            m_reg_solve_op->setACoeffs(lev, *density[lev]);
+            m_reg_solve_op->setShearViscosity(lev, m_incflo->m_mu);
         }
     }
 
@@ -120,19 +144,20 @@ DiffusionTensorOp::diffuse_velocity (Vector<MultiFab*> const& velocity,
         }
 
 #ifdef AMREX_USE_EB
-        if (m_eb_op) {
-            m_eb_op->setLevelBC(lev, velocity[lev]);
+        if (m_eb_solve_op) {
+            m_eb_solve_op->setLevelBC(lev, velocity[lev]);
         } else
 #endif
         {
-            m_reg_op->setLevelBC(lev, velocity[lev]);
+            m_reg_solve_op->setLevelBC(lev, velocity[lev]);
         }
     }
 
 #ifdef AMREX_USE_EB
-    MLMG mlmg(m_eb_op ? static_cast<MLLinOp&>(*m_eb_op) : static_cast<MLLinOp&>(*m_reg_op));
+    MLMG mlmg(m_eb_solve_op ? static_cast<MLLinOp&>(*m_eb_solve_op)
+              :               static_cast<MLLinOp&>(*m_reg_solve_op));
 #else
-    MLMG mlmg(*m_reg_op);
+    MLMG mlmg(*m_reg_solve_op);
 #endif
 
     // The default bottom solver is BiCG
@@ -154,4 +179,89 @@ DiffusionTensorOp::diffuse_velocity (Vector<MultiFab*> const& velocity,
     mlmg.setCGVerbose(m_mg_cg_verbose);
 
     mlmg.solve(velocity, GetVecOfConstPtrs(rhs), m_mg_rtol, m_mg_atol);
+}
+
+void DiffusionTensorOp::compute_divtau (Vector<MultiFab*> const& a_divtau,
+                                        Vector<MultiFab const*> const& a_velocity,
+                                        Vector<MultiFab const*> const& a_density,
+                                        Real t)
+{
+    BL_PROFILE("DiffusionTensorOp::compute_divtau");
+
+    int finest_level = m_incflo->finestLevel();
+
+    Vector<MultiFab> velocity(finest_level+1);
+    for (int lev = 0; lev <= finest_level; ++lev) {
+        velocity[lev].define(a_velocity[lev]->boxArray(),
+                             a_velocity[lev]->DistributionMap(),
+                             AMREX_SPACEDIM, 1, MFInfo(),
+                             a_velocity[lev]->Factory());
+        MultiFab::Copy(velocity[lev], *a_velocity[lev], 0, 0, AMREX_SPACEDIM, 1);
+    }
+
+#ifdef AMREX_USE_EB
+    if (m_eb_apply_op)
+    {
+        Vector<MultiFab> divtau_tmp(finest_level+1);
+        for (int lev = 0; lev <= finest_level; ++lev) {
+            divtau_tmp[lev].define(a_divtau[lev]->boxArray(),
+                                   a_divtau[lev]->DistributionMap(),
+                                   AMREX_SPACEDIM, 2, MFInfo(),
+                                   a_divtau[lev]->Factory());
+            divtau_tmp[lev].setVal(0.0);
+        }
+
+        // We want to return div (mu grad)) phi
+        m_eb_apply_op->setScalars(0.0, -1.0);
+        for (int lev = 0; lev <= finest_level; ++lev) {
+            m_eb_apply_op->setACoeffs(lev, *a_density[lev]);
+            m_eb_apply_op->setShearViscosity(lev, m_incflo->m_mu);
+            m_eb_apply_op->setEBShearViscosity(lev, m_incflo->m_mu);
+            m_eb_apply_op->setLevelBC(lev, &velocity[lev]);
+        }
+
+        MLMG mlmg(*m_eb_apply_op);
+        mlmg.apply(GetVecOfPtrs(divtau_tmp), GetVecOfPtrs(velocity));
+
+        for(int lev = 0; lev <= finest_level; lev++)
+        {
+            // xxxxx TODO
+            amrex::single_level_redistribute(lev, divtau_tmp[lev],
+                                             *a_divtau[lev], 0, 3,
+                                             m_incflo->Geom());
+        }
+    }
+    else
+#endif
+    {
+        // We want to return div (mu grad)) phi
+        m_reg_apply_op->setScalars(0.0, -1.0);
+        for (int lev = 0; lev <= finest_level; ++lev) {
+            m_reg_apply_op->setACoeffs(lev, *a_density[lev]);
+            m_reg_apply_op->setShearViscosity(lev, m_incflo->m_mu);
+            m_reg_apply_op->setLevelBC(lev, &velocity[lev]);
+        }
+
+        MLMG mlmg(*m_reg_apply_op);
+        mlmg.apply(a_divtau, GetVecOfPtrs(velocity));
+    }
+
+#ifdef _OPENMP
+#pragma omp parallel if (Gpu::notInLaunchRegion());
+#endif
+    for (int lev = 0; lev <= finest_level; ++lev) {
+        for (MFIter mfi(*a_divtau[lev],TilingIfNotGPU()); mfi.isValid(); ++mfi) {
+            Box const& bx = mfi.tilebox();
+            Array4<Real> const& divtau_arr = a_divtau[lev]->array(mfi);
+            Array4<Real const> const& rho_arr = a_density[lev]->const_array(mfi);
+            amrex::ParallelFor(bx,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+            {
+                Real rhoinv = 1.0/rho_arr(i,j,k);
+                divtau_arr(i,j,k,0) *= rhoinv;
+                divtau_arr(i,j,k,1) *= rhoinv;
+                divtau_arr(i,j,k,2) *= rhoinv;
+            });
+        }
+    }
 }
