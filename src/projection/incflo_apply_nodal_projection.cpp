2d1c40a6dba419c1c683ca062f6140380bf2f4a5
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-01-28 12:47:14 -0800

    no longer need to average down velocity after projection because of changes in amrex nodal projector

--- src/projection/incflo_apply_nodal_projection.cpp
+++ src/projection/incflo_apply_nodal_projection.cpp
@@ -202,13 +202,9 @@ void incflo::ApplyProjection (Real time, Real scaling_factor, bool incremental)
 
     for (int lev = finest_level-1; lev >= 0; --lev) {
 #ifdef AMREX_USE_EB
-        amrex::EB_average_down(m_leveldata[lev+1]->velocity, m_leveldata[lev]->velocity,
-                               0, AMREX_SPACEDIM, refRatio(lev));
         amrex::EB_average_down(m_leveldata[lev+1]->gp, m_leveldata[lev]->gp,
                                0, AMREX_SPACEDIM, refRatio(lev));
 #else
-        amrex::average_down(m_leveldata[lev+1]->velocity, m_leveldata[lev]->velocity,
-                            0, AMREX_SPACEDIM, refRatio(lev));
         amrex::average_down(m_leveldata[lev+1]->gp, m_leveldata[lev]->gp,
                             0, AMREX_SPACEDIM, refRatio(lev));
 #endif
