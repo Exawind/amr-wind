d2de086c1b63f131aa4d48b01008b7547462a19c
Shreyas Ananthan <shreyas@umd.edu>
2020-03-25 09:24:17 -0600

    Introduce unit tests for ABL source terms

---
+++ unit_tests/wind_energy/test_abl_src.cpp
@@ -0,0 +1,165 @@
+#include "abl_test_utils.H"
+#include "trig_ops.H"
+
+#include "AMReX_Gpu.H"
+#include "AMReX_Random.H"
+#include "ABLForcing.H"
+#include "CoriolisForcing.H"
+
+namespace amr_wind_tests {
+
+TEST_F(ABLTest, abl_forcing)
+{
+    constexpr amrex::Real tol = 1.0e-12;
+
+    pp_utils::default_time_inputs();
+    utils::populate_abl_params();
+
+    amrex::Box bx{{0, 0, 0}, {2, 2, 2}};
+    amrex::FArrayBox vel_src(bx, AMREX_SPACEDIM);
+
+    amr_wind::SimTime time;
+    time.parse_parameters();
+
+    amr_wind::ABLForcing abl_forcing(time);
+
+    // During initialization ensure that the source terms are zero
+    vel_src.setVal(0.0);
+    abl_forcing(bx, vel_src.array());
+    for (int i=0; i < AMREX_SPACEDIM; ++i) {
+        const auto min_src = vel_src.min(i);
+        const auto max_src = vel_src.max(i);
+        // Ensure that the source term is zero for initialization
+        EXPECT_NEAR(min_src, 0.0, tol);
+        // Ensure that the source term is constant throughout the domain
+        EXPECT_NEAR(min_src, max_src, tol);
+    }
+
+    // Mimic source term at later timesteps
+    {
+        time.new_timestep();
+        time.set_current_cfl(2.0);
+        EXPECT_NEAR(time.deltaT(), 0.1, tol);
+
+        vel_src.setVal(0.0);
+        abl_forcing.set_mean_velocities(10.0, 5.0);
+        abl_forcing(bx, vel_src.array());
+
+        // Targets are U = (20.0, 10.0, 0.0) set in initial conditions
+        // Means (set above) V = (10.0, 5.0, 0.0)
+        // deltaT (set above) dt = 0.1
+        const amrex::Array<amrex::Real, AMREX_SPACEDIM> golds{{100.0, 50.0, 0.0}};
+        for (int i=0; i < AMREX_SPACEDIM; ++i) {
+            const auto min_src = vel_src.min(i);
+            const auto max_src = vel_src.max(i);
+            EXPECT_NEAR(min_src, golds[i], tol);
+            // Ensure that the source term is constant throughout the domain
+            EXPECT_NEAR(min_src, max_src, tol);
+        }
+    }
+}
+
+TEST_F(ABLTest, coriolis_const_vel)
+{
+    constexpr amrex::Real tol = 1.0e-12;
+    constexpr amrex::Real corfac = 2.0 * amr_wind::utils::two_pi() / 86400.0;
+    // Latitude is set to 45 degrees in the input file so sinphi = cosphi
+    const amrex::Real latfac = std::sin(amr_wind::utils::radians(45.0));
+    // Initialize a random value for the velocity component
+    const amrex::Real vel_comp = 10.0 + 5.0 * (amrex::Random() - 0.5);
+
+    // Initialize parameters
+    utils::populate_abl_params();
+
+    // Create velocity and source terms fields
+    amrex::Box bx{{0, 0, 0}, {2, 2, 2}};
+    amrex::FArrayBox velocity(bx, AMREX_SPACEDIM);
+    amrex::FArrayBox vel_src(bx, AMREX_SPACEDIM);
+
+    amr_wind::CoriolisForcing coriolis;
+
+
+    // Velocity in x-direction test
+    {
+        amrex::Real golds[AMREX_SPACEDIM] = {0.0, -corfac * latfac * vel_comp,
+                                             corfac * latfac * vel_comp};
+        // Reset fields
+        velocity.setVal(0.0);
+        vel_src.setVal(0.0);
+        // set x component
+        velocity.setVal(vel_comp, 0);
+
+        coriolis(bx, velocity.array(), vel_src.array());
+
+        for (int i=0; i < AMREX_SPACEDIM; ++i) {
+            const auto min_src = vel_src.min(i);
+            const auto max_src = vel_src.max(i);
+            EXPECT_NEAR(min_src, golds[i], tol);
+            // Ensure that the source term is constant throughout the domain
+            EXPECT_NEAR(min_src, max_src, tol);
+        }
+    }
+
+    // Velocity in y direction test
+    {
+        amrex::Real golds[AMREX_SPACEDIM] = {corfac * latfac * vel_comp, 0.0, 0.0};
+        // Reset fields
+        velocity.setVal(0.0);
+        vel_src.setVal(0.0);
+        // Set y component
+        velocity.setVal(vel_comp, 1);
+
+        coriolis(bx, velocity.array(), vel_src.array());
+
+        for (int i=0; i < AMREX_SPACEDIM; ++i) {
+            const auto min_src = vel_src.min(i);
+            const auto max_src = vel_src.max(i);
+            EXPECT_NEAR(min_src, golds[i], tol);
+            // Ensure that the source term is constant throughout the domain
+            EXPECT_NEAR(min_src, max_src, tol);
+        }
+    }
+}
+
+TEST_F(ABLTest, coriolis_height_variation)
+{
+    constexpr int kdim = 16;
+    constexpr amrex::Real tol = 1.0e-12;
+    constexpr amrex::Real corfac = 2.0 * amr_wind::utils::two_pi() / 86400.0;
+    // Latitude is set to 45 degrees in the input file so sinphi = cosphi
+    const amrex::Real latfac = std::sin(amr_wind::utils::radians(45.0));
+
+    // Initialize parameters
+    utils::populate_abl_params();
+
+    // Create velocity and source terms fields
+    amrex::Box bx{{0, 0, 0}, {2, 2, kdim}};
+    amrex::FArrayBox velocity(bx, AMREX_SPACEDIM);
+    amrex::FArrayBox vel_src(bx, AMREX_SPACEDIM);
+
+    amr_wind::CoriolisForcing coriolis;
+
+    velocity.setVal(0.0);
+    vel_src.setVal(0.0);
+    {
+        // Set y velocity as a function of height with (dx = 1.0)
+        auto vel = velocity.array();
+        amrex::ParallelFor(bx, [vel] AMREX_GPU_DEVICE(int i, int j, int k) {
+            vel(i, j, k, 1) = static_cast<amrex::Real>(k);
+        });
+    }
+
+    coriolis(bx, velocity.array(), vel_src.array());
+
+    EXPECT_NEAR(vel_src.min(0), 0.0, tol);
+    EXPECT_NEAR(vel_src.max(0), corfac * latfac * kdim, tol);
+
+    for (int i=1; i < AMREX_SPACEDIM; ++i) {
+        const auto min_src = vel_src.min(i);
+        const auto max_src = vel_src.max(i);
+        EXPECT_NEAR(min_src, 0.0, tol);
+        EXPECT_NEAR(min_src, max_src, tol);
+    }
+}
+
+} // namespace amr_wind_tests
