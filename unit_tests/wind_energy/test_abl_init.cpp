90e75cb40cc756430a9dc5054013c0c62cfb77af
Shreyas Ananthan <shreyas@umd.edu>
2020-03-26 08:56:49 -0600

    Add unit tests for ABL forcing and Coriolis source terms (#29)
    * Introduce unit tests for ABL source terms
    
    * Fixes for GPU/CUDA build

--- unit_tests/wind_energy/test_abl_init.cpp
+++ unit_tests/wind_energy/test_abl_init.cpp
@@ -1,4 +1,4 @@
-#include "aw_test_utils/MeshTest.H"
+#include "abl_test_utils.H"
 #include "aw_test_utils/iter_tools.H"
 #include "aw_test_utils/test_utils.H"
 #include "ABLFieldInit.H"
@@ -6,65 +6,12 @@
 namespace amr_wind_tests {
 namespace {
 
-// Introduce namespace for the text fixture
-class ABLTest : public MeshTest
-{};
-
-void populate_abl_params()
-{
-    amrex::ParmParse pp("abl");
-
-    // Initial conditions (Temperature)
-    amrex::Vector<amrex::Real> theights{{0.0, 650.0, 750.0, 1000.0}};
-    amrex::Vector<amrex::Real> tvalues{{300.0, 300.0, 308.0, 308.75}};
-    pp.add("ntemperature", theights.size());
-    pp.addarr("temperature_heights", theights);
-    pp.addarr("temperature_values", tvalues);
-    pp.add("perturb_ref_height", 50.0);
-
-    // Boussinesq Buoyancy
-    const amrex::Real tecoeff = 1.0 / 300.0;
-    pp.add("reference_temperature", 300.0);
-    pp.add("thermal_expansion_coeff", tecoeff);
-
-    // ABL Forcing
-    pp.add("abl_forcing_height", 90.0);
-
-    // Coriolis term
-    pp.add("latitude", 45.0);
-
-    pp.add("kappa", 0.41);
-    pp.add("surface_roughness_z0", 0.1);
-
-    // Needed for initial conditions
-    {
-        amrex::ParmParse pp("incflo");
-        pp.add("ro_0", 1.0);   // Density
-        pp.add("ic_u", 20.0);  // Ux
-        pp.add("ic_v", 10.0);   // Uy
-        pp.add("ic_w", 0.0);   // Uw
-    }
-
-    // Adjust computational domain to be more like ABL mesh in the z direction
-    {
-        amrex::ParmParse pp("amr");
-        amrex::Vector<int> ncell{{8, 8, 64}};
-        pp.addarr("n_cell", ncell);
-    }
-    {
-        amrex::ParmParse pp("geometry");
-        amrex::Vector<amrex::Real> probhi {{120.0, 120.0, 1000.0}};
-
-        pp.addarr("prob_hi", probhi);
-    }
-}
-
 } // namespace
 
-TEST_F(ABLTest, abl_initialization)
+TEST_F(ABLMeshTest, abl_initialization)
 {
     populate_parameters();
-    populate_abl_params();
+    utils::populate_abl_params();
 
     // For the first test disable velocity perturbations
     {
