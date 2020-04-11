2e16e6fcf89a7c22291fe88b2c9190b985551a5d
Shreyas Ananthan <shreyas@umd.edu>
2020-04-11 14:13:24 -0600

    Introduce Factory registration and allow PDEs to be registered

--- unit_tests/equation_systems/test_pde.cpp
+++ unit_tests/equation_systems/test_pde.cpp
@@ -6,6 +6,15 @@
 #include "PDE.H"
 #include "icns/icns_ops.H"
 
+namespace amr_wind {
+namespace pde {
+
+template class PDESystem<ICNS, fvm::Godunov>;
+template class PDESystem<Temperature, fvm::Godunov>;
+
+}
+}
+
 namespace amr_wind_tests {
 
 class PDETest : public MeshTest
@@ -35,6 +44,29 @@ TEST_F(PDETest, test_pde_impl)
 
     lowmach.compute_source_term(amr_wind::FieldState::Old);
     theta.compute_source_term(amr_wind::FieldState::Old);
+
+}
+
+TEST_F(PDETest, test_pde_create)
+{
+    initialize_mesh();
+    amr_wind::SimTime time;
+
+    auto lowmach = amr_wind::pde::PDEBase::create(
+        "ICNS-Godunov", time, mesh().field_repo(), 35);
+    auto theta = amr_wind::pde::PDEBase::create(
+        "Temperature-Godunov", time, mesh().field_repo(), 35);
+
+    amrex::Print()
+        << "Num registered fields: "
+        << mesh().field_repo().num_fields() << std::endl;
+
+    for (auto& ff: mesh().field_repo().fields()) {
+        amrex::Print() << " - " << ff->name() << std::endl;
+    }
+
+    lowmach->compute_source_term(amr_wind::FieldState::Old);
+    theta->compute_source_term(amr_wind::FieldState::Old);
 }
 
 }
