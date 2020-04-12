c25325ce9f46e0aa667d7650b1dd69a079baffb1
Shreyas Ananthan <shreyas@umd.edu>
2020-04-12 11:13:05 -0600

    Add checks in PDE unit tests

--- unit_tests/equation_systems/test_pde.cpp
+++ unit_tests/equation_systems/test_pde.cpp
@@ -6,48 +6,12 @@
 #include "PDE.H"
 #include "icns/icns_ops.H"
 
-namespace amr_wind {
-namespace pde {
-
-template class PDESystem<ICNS, fvm::Godunov>;
-template class PDESystem<Temperature, fvm::Godunov>;
-
-}
-}
-
 namespace amr_wind_tests {
 
 class PDETest : public MeshTest
 {};
 
-TEST_F(PDETest, test_pde_impl)
-{
-    initialize_mesh();
-
-    amr_wind::SimTime time;
-
-    using ICNSGodunov =
-        amr_wind::pde::PDESystem<amr_wind::pde::ICNS, amr_wind::fvm::Godunov>;
-    using TemperatureGodunov =
-        amr_wind::pde::PDESystem<amr_wind::pde::Temperature, amr_wind::fvm::Godunov>;
-
-    ICNSGodunov lowmach(time, mesh().field_repo(), 35);
-    TemperatureGodunov theta(time, mesh().field_repo(), 35);
-
-    amrex::Print()
-        << "Num registered fields: "
-        << mesh().field_repo().num_fields() << std::endl;
-
-    for (auto& ff: mesh().field_repo().fields()) {
-        amrex::Print() << " - " << ff->name() << std::endl;
-    }
-
-    lowmach.compute_source_term(amr_wind::FieldState::Old);
-    theta.compute_source_term(amr_wind::FieldState::Old);
-
-}
-
-TEST_F(PDETest, test_pde_create)
+TEST_F(PDETest, test_pde_create_godunov)
 {
     initialize_mesh();
     amr_wind::SimTime time;
@@ -57,16 +21,20 @@ TEST_F(PDETest, test_pde_create)
     auto theta = amr_wind::pde::PDEBase::create(
         "Temperature-Godunov", time, mesh().field_repo(), 35);
 
-    amrex::Print()
-        << "Num registered fields: "
-        << mesh().field_repo().num_fields() << std::endl;
+    EXPECT_EQ(mesh().field_repo().num_fields(), 19);
+}
 
-    for (auto& ff: mesh().field_repo().fields()) {
-        amrex::Print() << " - " << ff->name() << std::endl;
-    }
+TEST_F(PDETest, test_pde_create_mol)
+{
+    initialize_mesh();
+    amr_wind::SimTime time;
 
-    lowmach->compute_source_term(amr_wind::FieldState::Old);
-    theta->compute_source_term(amr_wind::FieldState::Old);
+    auto lowmach = amr_wind::pde::PDEBase::create(
+        "ICNS-MOL", time, mesh().field_repo(), 35);
+    auto theta = amr_wind::pde::PDEBase::create(
+        "Temperature-MOL", time, mesh().field_repo(), 35);
+
+    EXPECT_EQ(mesh().field_repo().num_fields(), 23);
 }
 
 }
