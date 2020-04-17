bf41a885f3fcddefbe04a0ddecdc0c6e68f416a6
Shreyas Ananthan <shreyas@umd.edu>
2020-04-17 14:06:52 -0600

    Refactor to remove duplication of mac project

---
+++ src/equation_systems/icns/icns_ops.cpp
@@ -0,0 +1,77 @@
+#include "icns_ops.H"
+#include "incflo_MAC_bcs.H"
+#include "AMReX_MacProjector.H"
+#include "mac_projection.H"
+#include "MLMGOptions.H"
+
+namespace amr_wind {
+namespace pde {
+
+void advection_mac_project(FieldRepo& repo, const FieldState fstate)
+{
+    auto& geom = repo.mesh().Geom();
+    auto& u_mac = repo.get_field("u_mac");
+    auto& v_mac = repo.get_field("v_mac");
+    auto& w_mac = repo.get_field("w_mac");
+    auto& density = repo.get_field("density", fstate);
+
+    // This will hold (1/rho) on faces
+    auto rho_xf = repo.create_scratch_field(1, 0, amr_wind::FieldLoc::XFACE);
+    auto rho_yf = repo.create_scratch_field(1, 0, amr_wind::FieldLoc::YFACE);
+    auto rho_zf = repo.create_scratch_field(1, 0, amr_wind::FieldLoc::ZFACE);
+
+    amrex::Vector<amrex::Array<amrex::MultiFab*, ICNS::ndim>> rho_face(
+        repo.num_active_levels());
+    amrex::Vector<amrex::Array<amrex::MultiFab*, ICNS::ndim>> mac_vec(
+        repo.num_active_levels());
+
+    // fixme todo clean this up, this was done to replace
+    // GetVecOfArrOfConstPtrs() below
+    amrex::Vector<amrex::Array<amrex::MultiFab const*, ICNS::ndim>>
+        rho_face_const;
+    rho_face_const.reserve(repo.num_active_levels());
+
+    for (int lev = 0; lev < repo.num_active_levels(); ++lev) {
+        rho_face[lev][0] = &(*rho_xf)(lev);
+        rho_face[lev][1] = &(*rho_yf)(lev);
+        rho_face[lev][2] = &(*rho_zf)(lev);
+
+        amrex::average_cellcenter_to_face(
+            rho_face[lev], density(lev), geom[lev]);
+        for (int idim = 0; idim < ICNS::ndim; ++idim) {
+            rho_face[lev][idim]->invert(1.0, 0);
+        }
+
+        rho_face_const.push_back(GetArrOfConstPtrs(rho_face[lev]));
+
+        mac_vec[lev][0] = &u_mac(lev);
+        mac_vec[lev][1] = &v_mac(lev);
+        mac_vec[lev][2] = &w_mac(lev);
+    }
+
+    amr_wind::MLMGOptions options("mac_proj");
+
+    // If we want to set max_coarsening_level we have to send it in to the
+    // constructor
+    amrex::LPInfo lp_info;
+    lp_info.setMaxCoarseningLevel(options.max_coarsen_level);
+
+    // Perform MAC projection
+    amrex::MacProjector macproj(
+        mac_vec, rho_face_const,
+        repo.mesh().Geom(0, repo.num_active_levels() - 1), lp_info);
+
+    // get the bc types from pressure field
+    auto& bctype = repo.get_field("p").bc_type();
+
+    macproj.setDomainBC(
+        mac::get_projection_bc(
+            amrex::Orientation::low, bctype, repo.mesh().Geom()),
+        mac::get_projection_bc(
+            amrex::Orientation::high, bctype, repo.mesh().Geom()));
+
+    macproj.project(options.rel_tol, options.abs_tol, amrex::MLMG::Location::FaceCentroid);
+}
+
+} // namespace pde
+} // namespace amr_wind
