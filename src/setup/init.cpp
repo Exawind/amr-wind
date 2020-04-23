12d7d1c3deae39511ffe097392049ae8a9f6583f
Shreyas Ananthan <shreyas@umd.edu>
2020-04-23 14:34:51 -0600

    Setup SrcTermOp to read and initialize sources

--- src/setup/init.cpp
+++ src/setup/init.cpp
@@ -77,11 +77,11 @@ void incflo::ReadParameters ()
 
     // FIXME: clean up WIP logic
     if (m_probtype == 35) {
-        m_physics.emplace_back(new amr_wind::ABL(m_time, this));
+        m_physics.emplace_back(new amr_wind::ABLOld(m_time, this));
     }
     
     if (m_probtype == 11) {
-        m_physics.emplace_back(new amr_wind::BoussinesqBubble(this));
+        m_physics.emplace_back(new amr_wind::BoussinesqBubbleOld(this));
     }
 
     {
