019a122506623dd047eb2fed9e03f99d8a1dc167
Shreyas Ananthan <shreyas@umd.edu>
2020-05-26 12:29:39 -0600

    Overset: Prepare FieldRepo for overset - iblank field handling (#133)
    * Prepare FieldRepo for handling integer field
    
    * Introduce integer fields in FieldRepo
    
    * Add setVal convenience function

--- unit_tests/core/test_field.cpp
+++ unit_tests/core/test_field.cpp
@@ -298,4 +298,27 @@ TEST_F(FieldRepoTest, scratch_fields)
     }
 }
 
+TEST_F(FieldRepoTest, int_fields)
+{
+    initialize_mesh();
+
+    auto& frepo = mesh().field_repo();
+    auto& ibcell = frepo.declare_int_field("iblank_cell", 1, 0, 1);
+    auto& ibnode = frepo.declare_int_field(
+        "iblank_node", 1, 0, 1, amr_wind::FieldLoc::NODE);
+
+    const int nlevels = frepo.num_active_levels();
+    for (int lev=0; lev < nlevels; ++lev) {
+        ibcell(lev).setVal(1);
+        ibnode(lev).setVal(0);
+    }
+
+    for (int lev=0; lev < nlevels; ++lev) {
+        EXPECT_EQ(ibcell(lev).max(0), 1);
+        EXPECT_EQ(ibcell(lev).min(0), 1);
+        EXPECT_EQ(ibnode(lev).max(0), 0);
+        EXPECT_EQ(ibnode(lev).min(0), 0);
+    }
+}
+
 }
