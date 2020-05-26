19582a4980b0faff36b58026acf4a1042a0ef308
Shreyas Ananthan <shreyas@umd.edu>
2020-05-26 09:25:19 -0600

    Add setVal convenience function

--- src/core/IntField.cpp
+++ src/core/IntField.cpp
@@ -53,4 +53,36 @@ amrex::Vector<const amrex::iMultiFab*> IntField::vec_const_ptrs() const noexcept
     return ret;
 }
 
+void IntField::setVal(int value) noexcept
+{
+    BL_PROFILE("amr-wind::IntField::setVal 1")
+    for (int lev=0; lev < m_repo.num_active_levels(); ++lev) {
+        operator()(lev).setVal(value);
+    }
+}
+
+void IntField::setVal(int value, int start_comp, int num_comp, int nghost) noexcept
+{
+    BL_PROFILE("amr-wind::IntField::setVal 2")
+    for (int lev=0; lev < m_repo.num_active_levels(); ++lev) {
+        operator()(lev).setVal(value, start_comp, num_comp, nghost);
+    }
+}
+
+void IntField::setVal(const amrex::Vector<int>& values, int nghost) noexcept
+{
+    BL_PROFILE("amr-wind::IntField::setVal 3")
+    AMREX_ASSERT(num_comp() == static_cast<int>(values.size()));
+
+    // Update 1 component at a time
+    const int ncomp = 1;
+    for (int lev=0; lev < m_repo.num_active_levels(); ++lev) {
+        auto& mf = operator()(lev);
+        for (int ic=0; ic < num_comp(); ++ic) {
+            int value = values[ic];
+            mf.setVal(value, ic, ncomp, nghost);
+        }
+    }
+}
+
 } // namespace amr_wind
