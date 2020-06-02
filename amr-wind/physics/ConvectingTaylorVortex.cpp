fb0c423f81834422314b3bcbd14f01748042add0
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2020-06-02 15:26:23 -0600

    Adding CTV test case

---
+++ amr-wind/physics/ConvectingTaylorVortex.cpp
@@ -0,0 +1,164 @@
+#include "amr-wind/physics/ConvectingTaylorVortex.H"
+#include "amr-wind/CFDSim.H"
+#include "AMReX_iMultiFab.H"
+#include "AMReX_MultiFabUtil.H"
+#include "AMReX_ParmParse.H"
+#include "amr-wind/utilities/trig_ops.H"
+
+namespace amr_wind {
+namespace ctv {
+
+namespace {
+amrex::Real u_exact(
+    const amrex::Real u0,
+    const amrex::Real v0,
+    const amrex::Real omega,
+    const amrex::Real x,
+    const amrex::Real y,
+    const amrex::Real t)
+{
+    return u0 - std::cos(utils::pi() * (x - u0 * t)) *
+                    std::sin(utils::pi() * (y - v0 * t)) *
+                    std::exp(-2.0 * omega * t);
+}
+
+amrex::Real v_exact(
+    const amrex::Real u0,
+    const amrex::Real v0,
+    const amrex::Real omega,
+    const amrex::Real x,
+    const amrex::Real y,
+    const amrex::Real t)
+{
+    return v0 + std::sin(utils::pi() * (x - u0 * t)) *
+                    std::cos(utils::pi() * (y - v0 * t)) *
+                    std::exp(-2.0 * omega * t);
+}
+
+} // namespace
+
+ConvectingTaylorVortex::ConvectingTaylorVortex(const CFDSim& sim)
+    : m_time(sim.time())
+    , m_repo(sim.repo())
+    , m_mesh(sim.mesh())
+    , m_velocity(sim.repo().get_field("velocity"))
+    , m_density(sim.repo().get_field("density"))
+{
+    amrex::ParmParse pp("CTV");
+    pp.query("density", m_rho);
+    pp.query("u0", m_u0);
+    pp.query("v0", m_v0);
+    {
+        amrex::Real nu;
+        amrex::ParmParse pp("transport");
+        pp.query("viscosity", nu);
+        m_omega = utils::pi() * nu;
+    }
+    if (amrex::ParallelDescriptor::IOProcessor()) {
+        std::ofstream f;
+        f.open(m_output_fname.c_str());
+        f << std::setw(m_w) << "time" << std::setw(m_w) << "L2_u"
+          << std::setw(m_w) << "L2_v" << std::endl;
+        f.close();
+    }
+}
+
+/** Initialize the velocity and density fields at the beginning of the
+ *  simulation.
+ */
+void ConvectingTaylorVortex::initialize_fields(
+    int level, const amrex::Geometry& geom)
+{
+    using namespace utils;
+
+    auto& velocity = m_velocity(level);
+    auto& density = m_density(level);
+
+    density.setVal(m_rho);
+
+    for (amrex::MFIter mfi(velocity); mfi.isValid(); ++mfi) {
+        const auto& vbx = mfi.validbox();
+
+        const auto& dx = geom.CellSizeArray();
+        const auto& problo = geom.ProbLoArray();
+        auto vel = velocity.array(mfi);
+
+        amrex::ParallelFor(
+            vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+                const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
+                vel(i, j, k, 0) = u_exact(m_u0, m_v0, m_omega, x, y, 0.0);
+                vel(i, j, k, 1) = v_exact(m_u0, m_v0, m_omega, x, y, 0.0);
+                vel(i, j, k, 2) = 0.0;
+            });
+    }
+}
+
+amrex::Real ConvectingTaylorVortex::compute_error(
+    const int comp, const Field& field, amr_wind::ctv::FuncDef f)
+{
+    amrex::Real error = 0.0;
+    const amrex::Real time = m_time.current_time();
+
+    const int nlevels = m_repo.num_active_levels();
+    for (int lev = 0; lev < nlevels; ++lev) {
+
+        amrex::iMultiFab level_mask;
+        if (lev < nlevels - 1) {
+            level_mask = makeFineMask(
+                m_mesh.boxArray(lev), m_mesh.DistributionMap(lev),
+                m_mesh.boxArray(lev + 1), amrex::IntVect(2), 1, 0);
+        } else {
+            level_mask.define(
+                m_mesh.boxArray(lev), m_mesh.DistributionMap(lev), 1, 0,
+                amrex::MFInfo());
+            level_mask.setVal(1);
+        }
+
+        const auto& dx = m_mesh.Geom(lev).CellSizeArray();
+        const auto& problo = m_mesh.Geom(lev).ProbLoArray();
+        const amrex::Real cell_vol = dx[0] * dx[1] * dx[2];
+
+        const auto& fld = field(lev);
+        for (amrex::MFIter mfi(fld); mfi.isValid(); ++mfi) {
+            const auto& vbx = mfi.validbox();
+            const auto& field_arr = fld.array(mfi);
+            const auto& mask_arr = level_mask.array(mfi);
+
+            amrex::Real err_fab = 0.0;
+            amrex::LoopOnCpu(vbx, [=, &err_fab](int i, int j, int k) noexcept {
+                const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+                const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
+                const amrex::Real u = field_arr(i, j, k, comp);
+                const amrex::Real u_exact = f(m_u0, m_v0, m_omega, x, y, time);
+                err_fab += cell_vol * mask_arr(i, j, k) * (u - u_exact) *
+                           (u - u_exact);
+            });
+            error += err_fab;
+        }
+    }
+    amrex::ParallelDescriptor::ReduceRealSum(error);
+
+    const amrex::Real total_vol = m_mesh.Geom(0).ProbDomain().volume();
+    return std::sqrt(error / total_vol);
+}
+
+void ConvectingTaylorVortex::output_error()
+{
+    const amrex::Real u_err = compute_error(0, m_velocity, u_exact);
+    const amrex::Real v_err = compute_error(1, m_velocity, v_exact);
+
+    if (amrex::ParallelDescriptor::IOProcessor()) {
+        std::ofstream f;
+        f.open(m_output_fname.c_str(), std::ios_base::app);
+        f << std::setprecision(12) << std::setw(m_w) << m_time.new_time()
+          << std::setw(m_w) << u_err << std::setw(m_w) << v_err << std::endl;
+        f.close();
+    }
+}
+
+void ConvectingTaylorVortex::post_init_actions() { output_error(); }
+
+void ConvectingTaylorVortex::post_advance_work() { output_error(); }
+} // namespace ctv
+} // namespace amr_wind
---
+++ test/test_files/ctv_godunov_plm/ctv_godunov_plm.i
@@ -0,0 +1,47 @@
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#            SIMULATION STOP            #
+#.......................................#
+time.stop_time               =   0.2   # Max (simulated) time to evolve
+time.max_step                =   200   # Max number of time steps
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#         TIME STEP COMPUTATION         #
+#.......................................#
+time.fixed_dt         =   -0.005        # Use this constant dt if > 0
+time.cfl              =   0.5        # CFL factor
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#            INPUT AND OUTPUT           #
+#.......................................#
+time.plot_interval  =  20   # Steps between plot files
+time.checkpoint_interval =   -1  # Steps between checkpoint files
+io.KE_int = 1        # calculate kinetic energy 
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#               PHYSICS                 #
+#.......................................#
+incflo.gravity          = 0.  0.  0.  # Gravitational force (3D)
+incflo.density             = 1.          # Reference density 
+incflo.use_godunov      = 1
+incflo.use_ppm = 0
+transport.viscosity = 0.001
+transport.laminar_prandtl = 1.0
+turbulence.model = Laminar
+
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#        ADAPTIVE MESH REFINEMENT       #
+#.......................................#
+amr.n_cell              =   32 32 32   # Grid cells at coarsest AMRlevel
+amr.max_level           =   0           # Max AMR level in hierarchy 
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#              GEOMETRY                 #
+#.......................................#
+geometry.prob_lo        =   0.  0.  0.  # Lo corner coordinates
+geometry.prob_hi        =   2.  2.  2.  # Hi corner coordinates
+geometry.is_periodic    =   1   1   1   # Periodicity x y z (0/1)
+
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨#
+#          INITIAL CONDITIONS           #
+#.......................................#
+incflo.physics = ConvectingTaylorVortex
