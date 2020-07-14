fc1bf9752111b68f8c0feac140241f595c71145b
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2020-07-13 22:45:38 -0600

    Change dissipation term coefficient at the ground

--- amr-wind/equation_systems/tke/source_terms/KsgsM84Src.cpp
+++ amr-wind/equation_systems/tke/source_terms/KsgsM84Src.cpp
@@ -1,3 +1,5 @@
+#include <AMReX_Orientation.H>
+
 #include "amr-wind/equation_systems/tke/source_terms/KsgsM84Src.H"
 #include "amr-wind/CFDSim.H"
 #include "amr-wind/turbulence/TurbulenceModel.H"
@@ -15,7 +17,8 @@ KsgsM84Src::KsgsM84Src(const CFDSim& sim)
     AMREX_ALWAYS_ASSERT(sim.turbulence_model().model_name() == "OneEqKsgsM84");
     auto coeffs = sim.turbulence_model().model_coeffs();
     m_Ceps = coeffs["Ceps"];
-    
+    m_CepsGround =  (3.9/0.93) * m_Ceps;
+
 }
 
 KsgsM84Src::~KsgsM84Src() = default;
@@ -32,12 +35,30 @@ void KsgsM84Src::operator()(
     const auto& buoy_prod_arr = (this->m_buoy_prod)(lev).array(mfi);
     const auto& tke_arr = (this->m_tke)(lev).array(mfi);
     const amrex::Real Ceps = this->m_Ceps;
+    const amrex::Real CepsGround = this->m_CepsGround;
 
     amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
       src_term(i, j, k) = shear_prod_arr(i,j,k)
           + buoy_prod_arr(i,j,k)
           - Ceps * std::sqrt(tke_arr(i,j,k)) * tke_arr(i,j,k) / tlscale_arr(i,j,k);
     });
+
+    auto& bctype = (this->m_tke).bc_type();
+    for (int dir=0; dir < AMREX_SPACEDIM; ++dir) {
+      amrex::Orientation olo(dir, amrex::Orientation::low);
+      if (bctype[olo] == BC::wall_model) {
+        amrex::Box blo = amrex::adjCellLo(bx,dir,0) & bx;
+        if (blo.ok()) {
+          amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+            src_term(i, j, k) = shear_prod_arr(i,j,k)
+                + buoy_prod_arr(i,j,k)
+                - CepsGround * std::sqrt(tke_arr(i,j,k)) * tke_arr(i,j,k) / tlscale_arr(i,j,k);
+          });
+        }
+      }
+    }
+    
+    
     
 }
 
