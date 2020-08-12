1999ce96d5fbb492c668fc6c261fff34f078f922
michael brazell <michael.brazell@nrel.gov>
2020-08-12 11:39:20 -0700

    fixed stencils and added to unit test

--- unit_tests/aw_test_utils/MeshTest.cpp
+++ unit_tests/aw_test_utils/MeshTest.cpp
@@ -22,16 +22,19 @@ void MeshTest::initialize_mesh()
 void MeshTest::reset_prob_domain()
 {
     amrex::Vector<amrex::Real> problo(3), probhi(3);
+    amrex::Vector<int> periodic(3);
 
     amrex::ParmParse pp("geometry");
     pp.getarr("prob_lo",problo,0,AMREX_SPACEDIM);
     pp.getarr("prob_hi",probhi,0,AMREX_SPACEDIM);
+    pp.getarr("is_periodic",periodic,0,AMREX_SPACEDIM);
 
     amrex::RealBox rb(problo.data(), probhi.data());
     amrex::Geometry* gg = amrex::AMReX::top()->getDefaultGeometry();
 
     if (gg != nullptr) {
         gg->ResetDefaultProbDomain(rb);
+        gg->ResetDefaultPeriodicity({periodic[0],periodic[1],periodic[2]});
     }
 }
 
