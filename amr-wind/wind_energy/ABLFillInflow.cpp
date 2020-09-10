62a75f5801c0ba3cdc07691818d08ef0dac73d1f
Shreyas Ananthan <shreyas@umd.edu>
2020-09-10 15:20:36 -0600

    Fix fillpatch logic to deal properly with states when called via Field (#206)

--- amr-wind/wind_energy/ABLFillInflow.cpp
+++ amr-wind/wind_energy/ABLFillInflow.cpp
@@ -18,9 +18,10 @@ void ABLFillInflow::fillpatch(
     int lev,
     amrex::Real time,
     amrex::MultiFab& mfab,
-    const amrex::IntVect& nghost)
+    const amrex::IntVect& nghost,
+    const FieldState fstate)
 {
-    FieldFillPatchOps<FieldBCNoOp>::fillpatch(lev, time, mfab, nghost);
+    FieldFillPatchOps<FieldBCNoOp>::fillpatch(lev, time, mfab, nghost, fstate);
 
     m_bndry_plane.populate_data(lev, time, m_field, mfab);
 }
@@ -29,10 +30,11 @@ void ABLFillInflow::fillpatch_from_coarse(
     int lev,
     amrex::Real time,
     amrex::MultiFab& mfab,
-    const amrex::IntVect& nghost)
+    const amrex::IntVect& nghost,
+    const FieldState fstate)
 {
     FieldFillPatchOps<FieldBCNoOp>::fillpatch_from_coarse(
-        lev, time, mfab, nghost);
+        lev, time, mfab, nghost, fstate);
 
     m_bndry_plane.populate_data(lev, time, m_field, mfab);
 }
@@ -41,9 +43,10 @@ void ABLFillInflow::fillphysbc(
     int lev,
     amrex::Real time,
     amrex::MultiFab& mfab,
-    const amrex::IntVect& nghost)
+    const amrex::IntVect& nghost,
+    const FieldState fstate)
 {
-    FieldFillPatchOps<FieldBCNoOp>::fillphysbc(lev, time, mfab, nghost);
+    FieldFillPatchOps<FieldBCNoOp>::fillphysbc(lev, time, mfab, nghost, fstate);
 
     m_bndry_plane.populate_data(lev, time, m_field, mfab);
 }
