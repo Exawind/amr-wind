9c4f36e92030664ca9b2430a960aebc0142cd1ec
Georgios (Yorgos) Deskos <gdeskosv@gmail.com>
2020-10-29 16:04:20 -0600

    Update VortexPatch.cpp

--- amr-wind/physics/multiphase/VortexPatch.cpp
+++ amr-wind/physics/multiphase/VortexPatch.cpp
@@ -9,9 +9,9 @@ VortexPatch::VortexPatch(CFDSim& sim)
     , m_velocity(sim.repo().get_field("velocity"))
     , m_levelset(sim.repo().get_field("levelset"))
 {
-    amrex::ParmParse pp_vortex_patch("VortexPatch");
-    pp_vortex_patch.queryarr("location", m_loc, 0, AMREX_SPACEDIM);
-    pp_vortex_patch.query("radius", m_radius);
+    amrex::ParmParse pp(identifier());
+    pp.queryarr("location", m_loc, 0, AMREX_SPACEDIM);
+    pp.query("radius", m_radius);
 }
 
 /** Initialize the velocity and levelset fields at the beginning of the
