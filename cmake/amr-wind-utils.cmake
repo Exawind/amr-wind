2f5e4a750eeeb7f775e20894db4d05273ab1ac5d
Jon Rood <jon.rood@nrel.gov>
2020-12-24 12:38:43 -0700

    Move cppcheck to cmake target (#300)
    * Move cppcheck to cmake target and update CI accordingly. Add cppcheck problem-matcher. Reduce a few clang-tidy checks.
    
    * Generate compile_commands.json by default.
    
    * Enable problem-matcher for clang-tidy.

--- cmake/amr-wind-utils.cmake
+++ cmake/amr-wind-utils.cmake
@@ -61,7 +61,7 @@ macro(init_amrex)
   endif()
 endmacro(init_amrex)
 
-macro(init_clang_tidy)
+macro(init_code_checks)
   if(AMR_WIND_ENABLE_CLANG_TIDY)
     find_program(CLANG_TIDY_EXE NAMES "clang-tidy")
     if(CLANG_TIDY_EXE)
@@ -70,4 +70,43 @@ macro(init_clang_tidy)
       message(WARNING "clang-tidy not found.")
     endif()
   endif()
-endmacro(init_clang_tidy)
+
+  if(AMR_WIND_ENABLE_CPPCHECK)
+    find_program(CPPCHECK_EXE NAMES "cppcheck")
+    if(CPPCHECK_EXE)
+      message(STATUS "cppcheck found: ${CPPCHECK_EXE}")
+      include(ProcessorCount)
+      ProcessorCount(NP)
+      if(NP EQUAL 0)
+        set(NP 1)
+      endif()
+      file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/cppcheck)
+      add_custom_target(cppcheck
+          COMMAND ${CMAKE_COMMAND} -E echo "Running cppcheck on project using ${NP} cores..."
+          COMMAND ${CMAKE_COMMAND} -E make_directory cppcheck/cppcheck-wd
+          # cppcheck ignores -isystem directories, so we change them to regular -I include directories (with no spaces either)
+          COMMAND sed "s/isystem /I/g" ${CMAKE_BINARY_DIR}/compile_commands.json > cppcheck_compile_commands.json
+          COMMAND ${CPPCHECK_EXE} --template=gcc --inline-suppr --suppress=internalAstError --suppress=unusedFunction --std=c++14 --language=c++ --enable=all --project=cppcheck_compile_commands.json --cppcheck-build-dir=cppcheck-wd -i ${CMAKE_SOURCE_DIR}/submods/amrex/Src -i ${CMAKE_SOURCE_DIR}/submods/googletest --output-file=cppcheck-full-report.txt -j ${NP}
+          COMMENT "Run cppcheck on project compile_commands.json"
+          BYPRODUCTS cppcheck-full-report.txt
+          WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/cppcheck
+          VERBATIM USES_TERMINAL
+      )
+      add_custom_target(cppcheck-ci
+          # Filter out submodule source files after analysis
+          COMMAND awk -v nlines=2 "/submods/ {for (i=0; i<nlines; i++) {getline}; next} 1" < cppcheck/cppcheck-full-report.txt > cppcheck/cppcheck-short-report.txt
+          COMMAND cat cppcheck/cppcheck-short-report.txt | egrep "information:|error:|performance:|portability:|style:|warning:" | sort > cppcheck-ci-report.txt
+          COMMAND printf "Warnings: " >> cppcheck-ci-report.txt
+          COMMAND cat cppcheck-ci-report.txt | awk "END{print NR-1}" >> cppcheck-ci-report.txt
+          COMMENT "Filter cppcheck results to only AMR-Wind files with results in cppcheck-ci-report.txt"
+          DEPENDS cppcheck
+          BYPRODUCTS cppcheck-ci-report.txt
+          WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+          VERBATIM
+      )
+
+    else()
+      message(WARNING "cppcheck not found.")
+    endif()
+  endif()
+endmacro(init_code_checks)
