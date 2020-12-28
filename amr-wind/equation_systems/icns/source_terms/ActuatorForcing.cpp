fab2d0d5c45efef83abdb49d63dfe574985a40ec
Shreyas Ananthan <shreyas@umd.edu>
2020-12-28 10:15:13 -0700

    Actuator model prototype (3)
    - Add actuator momentum source term
    
    - Fully functional actuator physics interface. Added force computation and body force computation terms.
    
    - Add fixed-wing simulation capability. Use flat plate as an example
    
    - Allow netcdf output of fixed-wing actuator model quantities
    
    - Introduce 3D Gaussian smearing for line

---
+++ amr-wind/equation_systems/icns/source_terms/ActuatorForcing.cpp
@@ -0,0 +1,39 @@
+#include "amr-wind/equation_systems/icns/source_terms/ActuatorForcing.H"
+#include "amr-wind/CFDSim.H"
+#include "amr-wind/wind_energy/actuator/Actuator.H"
+
+#include "AMReX_Gpu.H"
+
+namespace amr_wind {
+namespace pde {
+namespace icns {
+
+ActuatorForcing::ActuatorForcing(const CFDSim& sim)
+    : m_act_src(sim.repo().get_field("actuator_src_term"))
+{
+    if (!sim.physics_manager().contains("Actuator")) {
+        amrex::Abort("ActuatorForcing requires Actuator physics to be active");
+    }
+}
+
+ActuatorForcing::~ActuatorForcing() = default;
+
+void ActuatorForcing::operator()(
+    const int lev,
+    const amrex::MFIter& mfi,
+    const amrex::Box& bx,
+    const FieldState,
+    const amrex::Array4<amrex::Real>& src_term) const
+{
+    const auto varr = m_act_src(lev).const_array(mfi);
+
+    amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+        src_term(i, j, k, 0) += varr(i, j, k, 0);
+        src_term(i, j, k, 1) += varr(i, j, k, 1);
+        src_term(i, j, k, 2) += varr(i, j, k, 2);
+    });
+}
+
+} // namespace icns
+} // namespace pde
+} // namespace amr_wind
---
+++ amr-wind/wind_energy/actuator/wing/FlatPlate.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/wind_energy/actuator/wing/FlatPlate.H"
+#include "amr-wind/wind_energy/actuator/wing/flat_plate_ops.H"
+#include "amr-wind/wind_energy/actuator/ActuatorModel.H"
+
+namespace amr_wind {
+namespace actuator {
+
+template class ActModel<FlatPlate, ActSrcLine>;
+
+}
+} // namespace amr_wind
