09c51ef4fedde34fd053ce9d9b1b1e322981b759
Michael J. Brazell <michaeljbrazell@gmail.com>
2021-01-25 09:29:39 -0800

    added capability to tag iblank cells that are fringe or hole cells (#329)

--- amr-wind/utilities/tagging/OversetRefinement.cpp
+++ amr-wind/utilities/tagging/OversetRefinement.cpp
@@ -14,6 +14,8 @@ void OversetRefinement::initialize(const std::string& key)
 {
     amrex::ParmParse pp(key);
     pp.query("max_level", m_max_lev);
+    pp.query("tag_fringe", m_tag_fringe);
+    pp.query("tag_hole", m_tag_hole);
 }
 
 void OversetRefinement::operator()(
@@ -23,6 +25,9 @@ void OversetRefinement::operator()(
 
     const auto& ibcell = m_sim.repo().get_int_field("iblank_cell");
     const auto& ibfab = ibcell(level);
+
+    const bool tag_fringe = m_tag_fringe;
+    const bool tag_hole = m_tag_hole;
 
 #ifdef _OPENMP
 #pragma omp parallel if (Gpu::notInLaunchRegion())
@@ -50,7 +55,9 @@ void OversetRefinement::operator()(
                 const int ax = amrex::max(axp, axm);
                 const int ay = amrex::max(ayp, aym);
                 const int az = amrex::max(azp, azm);
-                if (amrex::max(ax, ay, az) > 1)
+                if (amrex::max(ax, ay, az) > 1 ||
+                    (tag_fringe && ibarr(i, j, k) == -1) ||
+                    (tag_hole && ibarr(i, j, k) == 0))
                     tag(i, j, k) = amrex::TagBox::SET;
             });
     }
