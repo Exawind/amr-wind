fe36a2962fdd9aa140dc1d73920a0692d0ba82d4
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2021-01-31 17:09:51 -0700

    Fix issues with IDDES. Also use only LES mode

--- amr-wind/turbulence/RANS/KOmegaSSTIDDES.cpp
+++ amr-wind/turbulence/RANS/KOmegaSSTIDDES.cpp
@@ -62,23 +62,7 @@ void KOmegaSSTIDDES<Transport>::update_turbulent_viscosity(
 {
     BL_PROFILE(
         "amr-wind::" + this->identifier() + "::update_turbulent_viscosity");
-    (this->m_tke)->fillpatch(this->m_sim.time().current_time());
-    (this->m_sdr)->fillpatch(this->m_sim.time().current_time());
 
-    auto gradK = (this->m_sim.repo()).create_scratch_field(3, 0);
-    fvm::gradient(*gradK, (this->m_tke)->state(fstate));
-
-    auto gradOmega = (this->m_sim.repo()).create_scratch_field(3, 0);
-    fvm::gradient(*gradOmega, (this->m_sdr)->state(fstate));
-
-    auto& vel = this->m_vel.state(fstate);
-    // Compute strain rate into shear production term
-    fvm::strainrate(this->m_shear_prod, vel);
-    // Compute vorticity
-    auto vorticity = fvm::vorticity(vel);
-
-    auto& mu_turb = this->mu_turb();
-    auto lam_mu = (this->m_transport).mu();
     const amrex::Real beta_star = this->m_beta_star;
     const amrex::Real alpha1 = this->m_alpha1;
     const amrex::Real alpha2 = this->m_alpha2;
@@ -88,14 +72,37 @@ void KOmegaSSTIDDES<Transport>::update_turbulent_viscosity(
     const amrex::Real a1 = this->m_a1;
     const amrex::Real Cdes1 = this->m_Cdes1;
     const amrex::Real Cdes2 = this->m_Cdes2;
-    const amrex::Real Cdt1 = this->m_Cdt1;
-    const amrex::Real Cdt2 = this->m_Cdt2;
+    // const amrex::Real Cdt1 = this->m_Cdt1;
+    // const amrex::Real Cdt2 = this->m_Cdt2;
     const amrex::Real Cw = this->m_Cw;
-    const amrex::Real kappa = this->m_kappa;
+    // const amrex::Real kappa = this->m_kappa;
 
+    auto& mu_turb = this->mu_turb();
+    auto lam_mu = (this->m_transport).mu();
     auto& den = this->m_rho.state(fstate);
+    auto& tke = (*this->m_tke).state(fstate);
+    auto& sdr = (*this->m_sdr).state(fstate);
     auto& repo = mu_turb.repo();
     auto& geom_vec = repo.mesh().Geom();
+    auto& ibcell = repo.get_int_field("iblank_cell");
+    auto& tke_lhs = (this->m_sim).repo().get_field("tke_lhs_src_term");
+    tke_lhs.setVal(0.0);
+    auto& sdr_lhs = (this->m_sim).repo().get_field("sdr_lhs_src_term");
+
+    auto gradK = (this->m_sim.repo()).create_scratch_field(3, 0);
+    fvm::gradient(*gradK, tke);
+
+    auto gradOmega = (this->m_sim.repo()).create_scratch_field(3, 0);
+    fvm::gradient(*gradOmega, sdr);
+
+    auto& vel = this->m_vel.state(fstate);
+    // Compute strain rate into shear production term
+    fvm::strainrate(this->m_shear_prod, vel);
+
+    // Compute vorticity
+    // auto vorticity = fvm::vorticity(vel);
+
+    const amrex::Real deltaT = (this->m_sim).time().deltaT();
 
     const int nlevels = repo.num_active_levels();
     for (int lev = 0; lev < nlevels; ++lev) {
@@ -116,24 +123,30 @@ void KOmegaSSTIDDES<Transport>::update_turbulent_viscosity(
             const auto& sdr_arr = (*this->m_sdr)(lev).array(mfi);
             const auto& wd_arr = (this->m_walldist)(lev).array(mfi);
             const auto& shear_prod_arr = (this->m_shear_prod)(lev).array(mfi);
-            const auto& vort_arr = (*vorticity)(lev).array(mfi);
+            // const auto& vort_arr = (*vorticity)(lev).array(mfi);
             const auto& diss_arr = (this->m_diss)(lev).array(mfi);
             const auto& sdr_src_arr = (this->m_sdr_src)(lev).array(mfi);
+            const auto& sdr_diss_arr = (this->m_sdr_diss)(lev).array(mfi);
             const auto& f1_arr = (this->m_f1)(lev).array(mfi);
+            const auto& tke_lhs_arr = tke_lhs(lev).array(mfi);
+            const auto& sdr_lhs_arr = sdr_lhs(lev).array(mfi);
+            const auto& ib_cell_arr = ibcell(lev).const_array(mfi);
 
             amrex::ParallelFor(
                 bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
                     amrex::Real gko =
-                        -(gradK_arr(i, j, k, 0) * gradOmega_arr(i, j, k, 0) +
-                          gradK_arr(i, j, k, 1) * gradOmega_arr(i, j, k, 1) +
-                          gradK_arr(i, j, k, 2) * gradOmega_arr(i, j, k, 2));
+                        (gradK_arr(i, j, k, 0) * gradOmega_arr(i, j, k, 0) +
+                         gradK_arr(i, j, k, 1) * gradOmega_arr(i, j, k, 1) +
+                         gradK_arr(i, j, k, 2) * gradOmega_arr(i, j, k, 2));
 
                     amrex::Real cdkomega = amrex::max(
                         1e-10, 2.0 * rho_arr(i, j, k) * sigma_omega2 * gko /
-                                   amrex::max(sdr_arr(i, j, k), 1e-15));
-                    amrex::Real tmp1 = 4.0 * rho_arr(i, j, k) * sigma_omega2 *
-                                       tke_arr(i, j, k) /
-                                       (cdkomega * wd_arr(i, j, k));
+                                   (sdr_arr(i, j, k) + 1e-15));
+
+                    amrex::Real tmp1 =
+                        4.0 * rho_arr(i, j, k) * sigma_omega2 *
+                        tke_arr(i, j, k) /
+                        (cdkomega * wd_arr(i, j, k) * wd_arr(i, j, k));
                     amrex::Real tmp2 =
                         std::sqrt(tke_arr(i, j, k)) /
                         (beta_star * sdr_arr(i, j, k) * wd_arr(i, j, k) +
@@ -144,56 +157,68 @@ void KOmegaSSTIDDES<Transport>::update_turbulent_viscosity(
                              rho_arr(i, j, k) +
                          1e-15);
                     amrex::Real tmp4 = shear_prod_arr(i, j, k);
-                    amrex::Real tmp5 = vort_arr(i, j, k);
+                    // amrex::Real tmp5 = vort_arr(i, j, k);
 
-                    amrex::Real tmp_f1 =
-                        std::tanh(amrex::min(amrex::max(tmp2, tmp3), tmp1));
+                    amrex::Real arg1 = amrex::min(amrex::max(tmp2, tmp3), tmp1);
+                    amrex::Real tmp_f1 = std::tanh(arg1 * arg1 * arg1 * arg1);
 
                     amrex::Real alpha = tmp_f1 * (alpha1 - alpha2) + alpha2;
                     amrex::Real beta = tmp_f1 * (beta1 - beta2) + beta2;
 
-                    amrex::Real f2 =
-                        std::tanh(std::pow(amrex::max(2.0 * tmp2, tmp3), 2));
+                    amrex::Real arg2 = amrex::max(2.0 * tmp2, tmp3);
+                    amrex::Real f2 = std::tanh(arg2 * arg2);
+
                     mu_arr(i, j, k) =
                         rho_arr(i, j, k) * a1 * tke_arr(i, j, k) /
                         amrex::max(a1 * sdr_arr(i, j, k), tmp4 * f2);
 
                     f1_arr(i, j, k) = tmp_f1;
 
-                    const amrex::Real alpha_des = 0.25 - wd_arr(i, j, k) / hmax;
-                    const amrex::Real fb = amrex::min(
-                        2.0 * std::exp(-9.0 * alpha_des * alpha_des), 1.0);
-                    const amrex::Real rdt =
-                        (mu_arr(i, j, k) /
-                         (rho_arr(i, j, k) * kappa * kappa * wd_arr(i, j, k) *
-                          wd_arr(i, j, k) *
-                          std::sqrt(0.5 * (tmp4 * tmp4 + tmp5 * tmp5))));
-                    const amrex::Real fdt =
-                        1.0 - std::tanh(std::pow(Cdt1 * rdt, Cdt2));
-                    const amrex::Real fdtilde = amrex::max((1.0 - fdt), fb);
+                    // const amrex::Real alpha_des = 0.25 - wd_arr(i, j, k) /
+                    // hmax; const amrex::Real fb = amrex::min(
+                    //     2.0 * std::exp(-9.0 * alpha_des * alpha_des), 1.0);
+                    // const amrex::Real rdt =
+                    //     (mu_arr(i, j, k) /
+                    //      (rho_arr(i, j, k) * kappa * kappa * wd_arr(i, j, k)
+                    //      *
+                    //       wd_arr(i, j, k) *
+                    //       std::sqrt(0.5 * (tmp4 * tmp4 + tmp5 * tmp5))));
+                    // const amrex::Real fdt =
+                    //     1.0 - std::tanh(std::pow(Cdt1 * rdt, Cdt2));
+                    // const amrex::Real fdtilde = amrex::max((1.0 - fdt), fb);
+                    // const amrex::Real fdtilde = 0.0;
                     const amrex::Real cdes = tmp_f1 * (Cdes1 - Cdes2) + Cdes2;
                     const amrex::Real l_les =
                         cdes *
-                        amrex::min(
-                            Cw * amrex::max(wd_arr(i, j, k), hmax), hmax);
-                    const amrex::Real l_rans = std::sqrt(tke_arr(i, j, k)) /
-                                               (beta_star * sdr_arr(i, j, k));
-                    const amrex::Real l_iddes =
-                        fdtilde * (l_rans - l_les) + l_les;
+                        amrex::min(Cw * amrex::max(wd_arr(i, j, k), hmax), hmax);
+                    // const amrex::Real l_rans = std::sqrt(tke_arr(i, j, k)) /
+                    //                            (beta_star * sdr_arr(i, j,
+                    //                            k));
+                    // const amrex::Real l_iddes =
+                    //     fdtilde * (l_rans - l_les) + l_les;
+                    const amrex::Real l_iddes = l_les;
+
                     diss_arr(i, j, k) = -std::sqrt(tke_arr(i, j, k)) *
                                         tke_arr(i, j, k) / l_iddes;
 
-                    sdr_src_arr(i, j, k) =
-                        rho_arr(i, j, k) *
-                        (alpha * tmp4 * tmp4 -
-                         beta * sdr_arr(i, j, k) * sdr_arr(i, j, k) +
-                         2.0 * (1 - tmp_f1) * sigma_omega2 * gko /
-                             (sdr_arr(i, j, k) + 1e-15));
+                    tke_lhs_arr(i, j, k) =
+                        0.5 * std::sqrt(tke_arr(i, j, k)) / l_iddes * deltaT;
 
-                    shear_prod_arr(i, j, k) = amrex::max(
-                        mu_arr(i, j, k) * tmp4 * tmp4,
-                        10.0 * beta_star * rho_arr(i, j, k) * tke_arr(i, j, k) *
-                            sdr_arr(i, j, k));
+                    shear_prod_arr(i, j, k) =
+                        amrex::max(std::abs(ib_cell_arr(i, j, k)), 0) *
+                        amrex::min(
+                            mu_arr(i, j, k) * tmp4 * tmp4,
+                            10.0 * beta_star * rho_arr(i, j, k) *
+                                tke_arr(i, j, k) * sdr_arr(i, j, k));
+
+                    sdr_lhs_arr(i, j, k) = 0.5 * rho_arr(i, j, k) * beta *
+                                           sdr_arr(i, j, k) * deltaT;
+                    sdr_src_arr(i, j, k) =
+                        rho_arr(i, j, k) * alpha * shear_prod_arr(i, j, k) /
+                            amrex::max(mu_arr(i, j, k), 1.0e-16) +
+                        (1.0 - tmp_f1) * cdkomega;
+                    sdr_diss_arr(i, j, k) = -rho_arr(i, j, k) * beta *
+                                            sdr_arr(i, j, k) * sdr_arr(i, j, k);
                 });
         }
     }
