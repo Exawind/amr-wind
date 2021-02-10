cd7adffce598a4ffa1cbe4dadfa625ba7110b7d2
Shreyas Ananthan <shreyas@umd.edu>
2021-02-10 09:35:24 -0700

    Introduce temporal averaging post-process utility

--- amr-wind/utilities/PostProcessing.cpp
+++ amr-wind/utilities/PostProcessing.cpp
@@ -1,9 +1,30 @@
 #include "amr-wind/utilities/PostProcessing.H"
 #include "amr-wind/CFDSim.H"
+#include "amr-wind/utilities/averaging/TimeAveraging.H"
 
 #include "AMReX_ParmParse.H"
 
+#include <set>
+
 namespace amr_wind {
+
+namespace {
+void perform_checks(
+    std::set<std::string>& registered_types, const std::string ptype)
+{
+    const auto found = registered_types.find(ptype);
+
+    if (found == registered_types.end()) {
+        registered_types.insert(ptype);
+        return;
+    }
+
+    if (ptype == averaging::TimeAveraging::identifier()) {
+        amrex::Abort(
+            "PostProcessing: Multiple instances of TimeAveraging not allowed");
+    }
+}
+} // namespace
 
 PostProcessManager::PostProcessManager(CFDSim& sim) : m_sim(sim) {}
 
@@ -12,10 +33,14 @@ void PostProcessManager::initialize()
     amrex::Vector<std::string> pnames;
     amrex::ParmParse pp("incflo");
     pp.queryarr("post_processing", pnames);
+    std::set<std::string> registered_types;
+
     for (const auto& label : pnames) {
         std::string ptype = "Sampling";
         amrex::ParmParse pp1(label);
         pp1.query("type", ptype);
+
+        perform_checks(registered_types, ptype);
         m_post.emplace_back(PostProcessBase::create(ptype, m_sim, label));
     }
 
---
+++ amr-wind/utilities/averaging/TimeAveraging.cpp
@@ -0,0 +1,73 @@
+#include "amr-wind/utilities/averaging/TimeAveraging.H"
+#include "amr-wind/utilities/averaging/ReAveraging.H"
+#include "amr-wind/CFDSim.H"
+
+#include "AMReX_ParmParse.H"
+
+namespace amr_wind {
+namespace averaging {
+
+TimeAveraging::TimeAveraging(CFDSim& sim, const std::string& label)
+    : m_sim(sim), m_label(label)
+{}
+
+TimeAveraging::~TimeAveraging() = default;
+
+void TimeAveraging::initialize()
+{
+    //! Fields to be averaged
+    amrex::Vector<std::string> fnames;
+    std::string default_avg_type = ReAveraging::identifier();
+
+    {
+        amrex::ParmParse pp(m_label);
+        pp.getarr("fields", fnames);
+        pp.query("averaging_type", default_avg_type);
+        pp.get("averaging_window", m_filter);
+    }
+
+    for (const auto& fname : fnames) {
+        std::string avg_type(default_avg_type);
+        amrex::ParmParse pp1(m_label + "/" + fname);
+        pp1.query("averaging_type", avg_type);
+
+        const std::string key = fname + "_" + avg_type;
+
+        // Guard against multiple registrations of the field
+        const auto found = m_registered.find(key);
+        if (found != m_registered.end()) continue;
+
+        // Create the averaging entity
+        m_averages.emplace_back(
+            FieldTimeAverage::create(avg_type, m_sim, fname));
+
+        // Track fields that have an average
+        m_registered.emplace(key, m_averages.back().get());
+    }
+}
+
+const std::string& TimeAveraging::add_averaging(
+    const std::string& field_name, const std::string& avg_type)
+{
+    const std::string key = field_name + "_" + avg_type;
+    const auto found = m_registered.find(key);
+
+    // If the field was already registered then just return the field
+    if (found != m_registered.end())
+        return m_registered[key]->average_field_name();
+
+    // Create and register new average
+    m_averages.emplace_back(
+        FieldTimeAverage::create(avg_type, m_sim, field_name));
+    return m_averages.back()->average_field_name();
+}
+
+void TimeAveraging::post_advance_work()
+{
+    for (auto& avg : m_averages) {
+        (*avg)(m_sim.time(), m_filter);
+    }
+}
+
+} // namespace averaging
+} // namespace amr_wind
