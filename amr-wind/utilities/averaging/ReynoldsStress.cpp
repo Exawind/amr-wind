d9ab2094a35f04edc07c6c744e1111d2fba5a05d
tonyinme <tonyinme@gmail.com>
2021-02-23 13:08:03 -0700

    Clang format

--- amr-wind/utilities/averaging/ReynoldsStress.cpp
+++ amr-wind/utilities/averaging/ReynoldsStress.cpp
@@ -24,13 +24,13 @@ ReynoldsStress::ReynoldsStress(CFDSim& sim, const std::string& fname)
     , m_average(get_field_or_error(sim.repo(), "velocity_mean"))
     , m_stress(sim.repo().declare_field(
           "Stress",
-          6,  // number of components of the reynolds stress tensor
+          6, // number of components of the reynolds stress tensor
           1, // Ghost cells
           1,
           m_field.field_location()))
     , m_re_stress(sim.repo().declare_field(
           "ReynoldsStress",
-          6,  // number of components of the reynolds stress tensor
+          6, // number of components of the reynolds stress tensor
           1, // Ghost cells
           1,
           m_field.field_location()))
@@ -90,26 +90,28 @@ void ReynoldsStress::operator()(
 
             amrex::ParallelFor(
                 bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-
-                    // The tensor index        
-                    int mn=0;
+                    // The tensor index
+                    int mn = 0;
 
                     for (int n = 0; n < ncomp; ++n) {
                         for (int m = n; m < ncomp; ++m) {
 
                             // AB
-                            const amrex::Real fval2 = fldarr(i, j, k, m) * fldarr(i, j, k, n);
+                            const amrex::Real fval2 =
+                                fldarr(i, j, k, m) * fldarr(i, j, k, n);
                             // <A><B>
-                            const amrex::Real aval2 = avgarr(i, j, k, m) * avgarr(i, j, k, n);
+                            const amrex::Real aval2 =
+                                avgarr(i, j, k, m) * avgarr(i, j, k, n);
                             // The current value
                             const amrex::Real avg = stressarr(i, j, k, mn);
 
                             // The stress <AB>
-                            stressarr(i, j, k, mn) = 
-                               (avg * factor + fval2 * dt) / filter;
+                            stressarr(i, j, k, mn) =
+                                (avg * factor + fval2 * dt) / filter;
 
                             // The Reynolds stress <ab>
-                            restressarr(i, j, k, mn) = stressarr(i, j, k, mn) - aval2;
+                            restressarr(i, j, k, mn) =
+                                stressarr(i, j, k, mn) - aval2;
 
                             ++mn;
                         }
@@ -117,10 +119,9 @@ void ReynoldsStress::operator()(
                 });
         }
     }
-//    m_average.fillpatch(time.new_time());
+    //    m_average.fillpatch(time.new_time());
     m_stress.fillpatch(time.new_time());
     m_re_stress.fillpatch(time.new_time());
-
 }
 
 } // namespace averaging
