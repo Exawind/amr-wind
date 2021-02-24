fbcbc4a499ff3e1a397033bb14456a3930845bbe
Shreyas Ananthan <shreyas@umd.edu>
2021-02-24 09:19:49 -0700

    Fix warnings and field names in ReynoldsStress

--- amr-wind/utilities/averaging/ReynoldsStress.cpp
+++ amr-wind/utilities/averaging/ReynoldsStress.cpp
@@ -23,20 +23,18 @@ ReynoldsStress::ReynoldsStress(CFDSim& sim, const std::string& fname)
     : m_field(get_field_or_error(sim.repo(), "velocity"))
     , m_average(get_field_or_error(sim.repo(), "velocity_mean"))
     , m_stress(sim.repo().declare_field(
-          "Stress",
+          "velocity_stress",
           6, // number of components of the reynolds stress tensor
           1, // Ghost cells
           1,
           m_field.field_location()))
     , m_re_stress(sim.repo().declare_field(
-          "ReynoldsStress",
+          "velocity_reynolds_stress",
           6, // number of components of the reynolds stress tensor
           1, // Ghost cells
           1,
           m_field.field_location()))
-
 {
-
     if (fname != "velocity") {
         amrex::Abort("ReynoldsStress only implemented for velocity field");
     }
@@ -47,7 +45,6 @@ ReynoldsStress::ReynoldsStress(CFDSim& sim, const std::string& fname)
 
     // Do coarse/fine interpolations upon regrid
     m_stress.fillpatch_on_regrid() = true;
-    m_re_stress.fillpatch_on_regrid() = true;
 
     // Register average field with the IO manager
     auto& iomgr = sim.io_manager();
@@ -57,7 +54,7 @@ ReynoldsStress::ReynoldsStress(CFDSim& sim, const std::string& fname)
 
 const std::string& ReynoldsStress::average_field_name()
 {
-    return m_average.name();
+    return m_re_stress.name();
 }
 
 void ReynoldsStress::operator()(
@@ -92,10 +89,8 @@ void ReynoldsStress::operator()(
                 bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
                     // The tensor index
                     int mn = 0;
-
                     for (int n = 0; n < ncomp; ++n) {
                         for (int m = n; m < ncomp; ++m) {
-
                             // AB
                             const amrex::Real fval2 =
                                 fldarr(i, j, k, m) * fldarr(i, j, k, n);
@@ -104,22 +99,19 @@ void ReynoldsStress::operator()(
                                 avgarr(i, j, k, m) * avgarr(i, j, k, n);
                             // The current value
                             const amrex::Real avg = stressarr(i, j, k, mn);
-
                             // The stress <AB>
                             stressarr(i, j, k, mn) =
                                 (avg * factor + fval2 * dt) / filter;
-
                             // The Reynolds stress <ab>
                             restressarr(i, j, k, mn) =
                                 stressarr(i, j, k, mn) - aval2;
-
                             ++mn;
                         }
                     }
                 });
         }
     }
-    //    m_average.fillpatch(time.new_time());
+
     m_stress.fillpatch(time.new_time());
     m_re_stress.fillpatch(time.new_time());
 }
