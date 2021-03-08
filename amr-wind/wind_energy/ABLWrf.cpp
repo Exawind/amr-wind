af97f29c3f806d12331c9e51b42fe4a74e68b478
shashankNREL <shashank.yellapantula@nrel.gov>
2021-03-08 09:59:22 -0700

    moving constructor to cpp file

--- amr-wind/wind_energy/ABLWrf.cpp
+++ amr-wind/wind_energy/ABLWrf.cpp
@@ -1,15 +1,38 @@
-#include "amr-wind/wind_energy/ABLStats.H"
 #include "amr-wind/utilities/io_utils.H"
-#include "amr-wind/utilities/DirectionSelector.H"
-#include "amr-wind/equation_systems/icns/source_terms/ABLForcing.H"
 #include "amr-wind/wind_energy/ABLWrf.H"
-
 #include "AMReX_ParmParse.H"
-#include "AMReX_ParallelDescriptor.H"
+#include "amr-wind/utilities/ncutils/nc_interface.H"
 
-namespace amr_wind{
+namespace amr_wind {
 
-ABLWRFfile::ABLWRFfile()
+ABLWRFfile::ABLWRFfile(std::string filename)
+{
 
+    m_wrf_filename = filename;
+    auto ncf = ncutils::NCFile::open_par(
+        m_wrf_filename, NC_NOWRITE | NC_NETCDF4 | NC_MPIIO,
+        amrex::ParallelContext::CommunicatorSub(), MPI_INFO_NULL);
 
+    m_nheight = ncf.dim("nheight").len();
+    m_ntime = ncf.dim("ntime").len();
+
+    m_wrf_height.resize(m_nheight);
+    m_wrf_time.resize(m_ntime);
+
+    ncf.var("heights").get(m_wrf_height.data());
+    ncf.var("times").get(m_wrf_time.data());
+
+    m_wrf_u.resize(m_nheight * m_ntime);
+    m_wrf_v.resize(m_nheight * m_ntime);
+    m_wrf_temp.resize(m_nheight * m_ntime);
+    m_wrf_tflux.resize(m_ntime);
+
+    ncf.var("wrf_momentum_u").get(m_wrf_u.data());
+    ncf.var("wrf_momentum_v").get(m_wrf_v.data());
+    ncf.var("wrf_temperature").get(m_wrf_temp.data());
+    ncf.var("wrf_tflux").get(m_wrf_tflux.data());
 }
+
+ABLWRFfile::~ABLWRFfile() = default;
+
+} // namespace amr_wind
