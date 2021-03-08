732597b83f3bc4f946bf8827a80d4433e228160e
ShashankNREL <shashank.yellapantula@nrel.gov>
2021-03-08 09:44:15 -0700

    hacking on some errors

---
+++ amr-wind/wind_energy/ABLWrf.cpp
@@ -0,0 +1,15 @@
+#include "amr-wind/wind_energy/ABLStats.H"
+#include "amr-wind/utilities/io_utils.H"
+#include "amr-wind/utilities/DirectionSelector.H"
+#include "amr-wind/equation_systems/icns/source_terms/ABLForcing.H"
+#include "amr-wind/wind_energy/ABLWrf.H"
+
+#include "AMReX_ParmParse.H"
+#include "AMReX_ParallelDescriptor.H"
+
+namespace amr_wind{
+
+ABLWRFfile::ABLWRFfile()
+
+
+}
