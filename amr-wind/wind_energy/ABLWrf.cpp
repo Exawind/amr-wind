59ad13beb6c099e7f23977fcc391b4f647ac08fd
ShashankNREL <shashank.yellapantula@nrel.gov>
2021-03-09 17:03:37 -0700

    Compiled code ready for testing momentum forcing

--- amr-wind/wind_energy/ABLWrf.cpp
+++ amr-wind/wind_energy/ABLWrf.cpp
@@ -12,19 +12,19 @@ ABLWRFfile::ABLWRFfile(const std::string filewrf)
         m_wrf_filename, NC_NOWRITE | NC_NETCDF4 | NC_MPIIO,
         amrex::ParallelContext::CommunicatorSub(), MPI_INFO_NULL);
 
-    m_nheight = ncf.dim("nheight").len();
-    m_ntime = ncf.dim("ntime").len();
+    m_wrf_nheight = ncf.dim("nheight").len();
+    m_wrf_ntime = ncf.dim("ntime").len();
 
-    m_wrf_height.resize(m_nheight);
-    m_wrf_time.resize(m_ntime);
+    m_wrf_height.resize(m_wrf_nheight);
+    m_wrf_time.resize(m_wrf_ntime);
 
     ncf.var("heights").get(m_wrf_height.data());
     ncf.var("times").get(m_wrf_time.data());
 
-    m_wrf_u.resize(m_nheight * m_ntime);
-    m_wrf_v.resize(m_nheight * m_ntime);
-    m_wrf_temp.resize(m_nheight * m_ntime);
-    m_wrf_tflux.resize(m_ntime);
+    m_wrf_u.resize(m_wrf_nheight * m_wrf_ntime);
+    m_wrf_v.resize(m_wrf_nheight * m_wrf_ntime);
+    m_wrf_temp.resize(m_wrf_nheight * m_wrf_ntime);
+    m_wrf_tflux.resize(m_wrf_ntime);
 
     ncf.var("wrf_momentum_u").get(m_wrf_u.data());
     ncf.var("wrf_momentum_v").get(m_wrf_v.data());
@@ -32,23 +32,22 @@ ABLWRFfile::ABLWRFfile(const std::string filewrf)
     ncf.var("wrf_tflux").get(m_wrf_tflux.data());
 }
 
-    const amrex::Vector<amrex::Real>& wrf_heights() const
-    {
-        return m_wrf_height;
-    }
+const amrex::Vector<amrex::Real>& ABLWRFfile::wrf_heights() const
+{
+  return m_wrf_height;
+}
 
-    const amrex::Vector<amrex::Real>& wrf_times() const { return m_wrf_time; }
+const amrex::Vector<amrex::Real>& ABLWRFfile::wrf_times() const { return m_wrf_time; }
 
-    const amrex::Vector<amrex::Real>& wrf_u() const { return m_wrf_u; }
+const amrex::Vector<amrex::Real>& ABLWRFfile::wrf_u() const { return m_wrf_u; }
 
-    const amrex::Vector<amrex::Real>& wrf_v() const { return m_wrf_v; }
+const amrex::Vector<amrex::Real>& ABLWRFfile::wrf_v() const { return m_wrf_v; }
 
-    const amrex::Vector<amrex::Real>& wrf_temp() const { return m_wrf_temp; }
+const amrex::Vector<amrex::Real>& ABLWRFfile::wrf_temp() const { return m_wrf_temp; }
 
-    const amrex::Vector<amrex::Real>& wrf_tflux() const { return m_wrf_tflux; }
+const amrex::Vector<amrex::Real>& ABLWRFfile::wrf_tflux() const { return m_wrf_tflux; }
 
-    int nheights() const { return m_nheight; }
-    int times() const { return m_ntime; }
-
+int ABLWRFfile::nheights() const { return m_wrf_nheight; }
+int ABLWRFfile::times() const { return m_wrf_ntime; }
 
 } // namespace amr_wind
