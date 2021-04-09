961b37b325de32dece05160cbf5f2258c505882c
tonyinme <tonyinme@gmail.com>
2021-04-09 15:27:46 -0600

    Added NREL 5MW Openfast example

---
+++ test/test_files/act_openfast_nrel5mw/fast_inp/5MW_Baseline/ServoData/CMakeFiles/DISCON.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/lmartine/nalu-wind/reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData/DISCON.F90" "/home/lmartine/nalu-wind/build/reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData/CMakeFiles/DISCON.dir/DISCON.F90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "Intel")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_Fortran
+  "DISCON_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/test_files/act_openfast_nrel5mw/fast_inp/5MW_Baseline/ServoData/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lmartine/nalu-wind/reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData
+# Build directory: /home/lmartine/nalu-wind/build/reg_tests/test_files/nrel5MWactuatorLine/5MW_Baseline/ServoData
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
