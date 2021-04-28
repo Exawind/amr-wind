bf53ccd1f2f79e40a369889319a62baaff7a6480
asharma <ashesh.sharma@nrel.gov>
2021-04-27 23:35:14 -0600

    initial commit with ib module mimicing actuator module

---
+++ amr-wind/immersed_boundary/IBContainer.cpp
@@ -0,0 +1,391 @@
+#include "amr-wind/immersed_boundary/IBContainer.H"
+#include "amr-wind/immersed_boundary/IB.H"
+#include "amr-wind/core/gpu_utils.H"
+#include "amr-wind/core/Field.H"
+
+#include "AMReX_Scan.H"
+
+#include <algorithm>
+
+namespace amr_wind {
+namespace ib {
+
+IBCloud::IBCloud(const int nobjects)
+    : num_pts(nobjects, 0), global_id(nobjects, -1), num_objects(nobjects)
+{}
+
+IBContainer::IBContainer(
+    amrex::AmrCore& mesh, const int num_objects)
+    : amrex::AmrParticleContainer<
+          NumPStructReal,
+          NumPStructInt,
+          NumPArrayReal,
+          NumPArrayInt>(&mesh)
+    , m_mesh(mesh)
+    , m_data(num_objects)
+    , m_proc_pos(amrex::ParallelDescriptor::NProcs(), vs::Vector::zero())
+    , m_pos_device(amrex::ParallelDescriptor::NProcs(), vs::Vector::zero())
+    , m_proc_offsets(amrex::ParallelDescriptor::NProcs() + 1, 0)
+    , m_proc_offsets_device(amrex::ParallelDescriptor::NProcs() + 1)
+{}
+
+/** Allocate memory and initialize the particles within the container
+ *
+ *  This method is only called once during the simulation. It allocates the
+ *  arrays for holding the position vector and velocity data on host memory and
+ *  also initializes corresponding particles within the first available particle
+ *  tile within the container. It is expected that the immersed boundary manager instance
+ *  has already populated the number of points per turbine before invoking this
+ *  method.
+ */
+void IBContainer::initialize_container()
+{
+    BL_PROFILE("amr-wind::ib::IBContainer::initialize_container");
+
+    compute_local_coordinates();
+
+    // Initialize global data arrays
+    const int total_pts =
+        std::accumulate(m_data.num_pts.begin(), m_data.num_pts.end(), 0);
+    m_data.position.resize(total_pts);
+    m_data.velocity.resize(total_pts);
+
+    {
+        const int nproc = amrex::ParallelDescriptor::NProcs();
+        amrex::Vector<int> pts_per_proc(nproc, 0);
+#ifdef AMREX_USE_MPI
+        MPI_Allgather(
+            &total_pts, 1, MPI_INT, pts_per_proc.data(), 1, MPI_INT,
+            amrex::ParallelDescriptor::Communicator());
+#else
+        pts_per_proc[0] = total_pts;
+#endif
+        m_proc_offsets[0] = 0;
+        for (int i = 1; i <= nproc; ++i) {
+            m_proc_offsets[i] = m_proc_offsets[i - 1] + pts_per_proc[i - 1];
+        }
+        amrex::Gpu::copy(
+            amrex::Gpu::hostToDevice, m_proc_offsets.begin(),
+            m_proc_offsets.end(), m_proc_offsets_device.begin());
+    }
+
+    initialize_particles(total_pts);
+}
+
+void IBContainer::initialize_particles(const int total_pts)
+{
+    // Initialize particle container data structures.
+    //
+    // We assign all particles into the first available container within this
+    // MPI rank and let redistribute take care of scattering the particles into
+    // the respective MPI rank containing the cell enclosing this particle. The
+    // actual redistribution happens after position vectors are updated in
+    // update_positions.
+
+    // query the particle ID from the container. We should always be starting
+    // from 1.
+    ParticleType::NextID(1u);
+    const auto id_start = ParticleType::NextID();
+    AMREX_ALWAYS_ASSERT(id_start == 1u);
+    const int iproc = amrex::ParallelDescriptor::MyProc();
+
+    // Flag indicating if a tile was found where all particles were deposited.
+    bool assigned = false;
+    const int nlevels = m_mesh.finestLevel() + 1;
+    for (int lev = 0; ((lev < nlevels) && !assigned); ++lev) {
+        for (auto mfi = MakeMFIter(lev); (mfi.isValid() && !assigned); ++mfi) {
+            auto& ptile = GetParticles(
+                lev)[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
+            AMREX_ASSERT(ptile.size() == 0);
+            ptile.resize(total_pts);
+            auto* pstruct = ptile.GetArrayOfStructs()().data();
+
+            amrex::ParallelFor(
+                total_pts, [=] AMREX_GPU_DEVICE(const int ip) noexcept {
+                    auto& pp = pstruct[ip];
+
+                    pp.id() = id_start + ip;
+                    pp.cpu() = iproc;
+                    pp.idata(0) = ip;
+                });
+            assigned = true;
+        }
+    }
+
+    // Indicate that we have initialized the containers and remaining methods
+    // are safe to use
+    m_container_initialized = true;
+    m_is_scattered = false;
+}
+
+void IBContainer::reset_container()
+{
+    const int nlevels = m_mesh.finestLevel() + 1;
+    for (int lev = 0; lev < nlevels; ++lev) {
+        for (ParIterType pti(*this, lev); pti.isValid(); ++pti) {
+            const int np = pti.numParticles();
+            auto* pstruct = pti.GetArrayOfStructs()().data();
+
+            amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(const int ip) noexcept {
+                auto& pp = pstruct[ip];
+                pp.id() = -1;
+            });
+        }
+    }
+    Redistribute();
+
+    const int total_pts = m_data.velocity.size();
+    initialize_particles(total_pts);
+}
+
+/** Update position vectors of the particles within a container based on the
+ *  data provided by immersed boundary instances.
+ *
+ *  This method assumes that the particles have been restored to their
+ *  originating MPI ranks after any scattering. This happens during
+ *  initialization (after AcutatorContainer::initialize_particles) and after
+ *  IBContainer::sample_velocities.
+ *
+ *  After executing this method, the particles have been scattered across the
+ *  domain such that each particle is contained within a tile that has the cell
+ *  enclosing the particle.
+ *
+ */
+void IBContainer::update_positions()
+{
+    BL_PROFILE("amr-wind::ib::IBContainer::update_positions");
+    AMREX_ALWAYS_ASSERT(m_container_initialized && !m_is_scattered);
+
+    const auto dpos = gpu::device_view(m_data.position);
+    const auto dptr = dpos.data();
+    const int nlevels = m_mesh.finestLevel() + 1;
+    for (int lev = 0; lev < nlevels; ++lev) {
+        for (ParIterType pti(*this, lev); pti.isValid(); ++pti) {
+            const int np = pti.numParticles();
+            auto* pstruct = pti.GetArrayOfStructs()().data();
+
+            amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(const int ip) noexcept {
+                auto& pp = pstruct[ip];
+                const auto idx = pp.idata(0);
+
+                auto& pvec = dptr[idx];
+                for (int n = 0; n < AMREX_SPACEDIM; ++n) {
+                    pp.pos(n) = pvec[n];
+                }
+            });
+        }
+    }
+
+    // Scatter particles to appropriate MPI ranks
+    Redistribute();
+
+    // Indicate that it is safe to sample velocities
+    m_is_scattered = true;
+}
+
+/** Interpolate the velocity field using a trilinear interpolation
+ *
+ *  This method performs three tasks:
+ *    - Sample the velocity field and interpolate onto particle location
+ *    - Restore the particles back to their original MPI rank
+ *    - Copy data from particles into the buffer used by immersed boundary instances
+ *
+ *  After invocation if this method, the particles are back in their original
+ *  rank and the position vectors can be updated safely.
+ */
+void IBContainer::sample_velocities(const Field& vel)
+{
+    BL_PROFILE("amr-wind::ib::IBContainer::sample_velocities");
+    AMREX_ALWAYS_ASSERT(m_container_initialized && m_is_scattered);
+
+    // Sample velocity field
+    interpolate_velocities(vel);
+
+    // Recall particles to the MPI ranks that contains their corresponding
+    // turbines
+    // Redistribute();
+
+    // Populate the velocity buffer that all immersed boundary instances can access
+    populate_vel_buffer();
+
+    // Indicate that the particles have been restored to their original MPI rank
+    m_is_scattered = false;
+}
+
+/** Helper method for IBContainer::sample_velocities
+ *
+ *  Loops over the particle tiles and copies velocity data from particles to the
+ *  velocity array.
+ */
+void IBContainer::populate_vel_buffer()
+{
+    BL_PROFILE("amr-wind::ib::IBContainer::populate_vel_buffer");
+    amrex::Vector<vs::Vector> velh(m_proc_offsets.back());
+    amrex::Gpu::DeviceVector<vs::Vector> vel(
+        m_proc_offsets.back(), vs::Vector::zero());
+    auto* varr = vel.data();
+    auto* offsets = m_proc_offsets_device.data();
+    const int nlevels = m_mesh.finestLevel() + 1;
+    for (int lev = 0; lev < nlevels; ++lev) {
+        for (ParIterType pti(*this, lev); pti.isValid(); ++pti) {
+            const int np = pti.numParticles();
+            auto* pstruct = pti.GetArrayOfStructs()().data();
+
+            amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(const int ip) noexcept {
+                auto& pp = pstruct[ip];
+                const auto iproc = pp.cpu();
+                const auto idx = offsets[iproc] + pp.idata(0);
+
+                auto& vvel = varr[idx];
+                for (int n = 0; n < AMREX_SPACEDIM; ++n) {
+                    vvel[n] = pp.rdata(n);
+                }
+            });
+        }
+    }
+
+    amrex::Gpu::copy(
+        amrex::Gpu::deviceToHost, vel.begin(), vel.end(), velh.begin());
+#ifdef AMREX_USE_MPI
+    MPI_Allreduce(
+        MPI_IN_PLACE, &(velh[0][0]), m_proc_offsets.back() * AMREX_SPACEDIM,
+        MPI_DOUBLE, MPI_SUM, amrex::ParallelDescriptor::Communicator());
+#endif
+    {
+        auto& vel_arr = m_data.velocity;
+        const int npts = vel_arr.size();
+        const int ioff = m_proc_offsets[amrex::ParallelDescriptor::MyProc()];
+        for (int i = 0; i < npts; ++i) {
+            vel_arr[i] = velh[ioff + i];
+        }
+    }
+}
+
+/** Helper method for IBContainer::sample_velocities
+ *
+ *  Performs a trilinear interpolation of the velocity field to particle
+ *  locations. It also updates the particle locations such that the next
+ *  Redistribute call restores the particles back to their original MPI rank
+ *  where they were created.
+ */
+void IBContainer::interpolate_velocities(const Field& vel)
+{
+    BL_PROFILE("amr-wind::ib::IBContainer::interpolate_velocities");
+    auto* dptr = m_pos_device.data();
+    const int nlevels = m_mesh.finestLevel() + 1;
+    for (int lev = 0; lev < nlevels; ++lev) {
+        const auto& geom = m_mesh.Geom(lev);
+        const auto dx = geom.CellSizeArray();
+        const auto dxi = geom.InvCellSizeArray();
+        const auto plo = geom.ProbLoArray();
+
+        for (ParIterType pti(*this, lev); pti.isValid(); ++pti) {
+            const int np = pti.numParticles();
+            auto* pstruct = pti.GetArrayOfStructs()().data();
+            const auto varr = vel(lev).const_array(pti);
+
+            amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE(const int ip) noexcept {
+                auto& pp = pstruct[ip];
+                // Determine offsets within the containing cell
+                const amrex::Real x =
+                    (pp.pos(0) - plo[0] - 0.5 * dx[0]) * dxi[0];
+                const amrex::Real y =
+                    (pp.pos(1) - plo[1] - 0.5 * dx[1]) * dxi[1];
+                const amrex::Real z =
+                    (pp.pos(2) - plo[2] - 0.5 * dx[2]) * dxi[2];
+
+                // Index of the low corner
+                const int i = static_cast<int>(amrex::Math::floor(x));
+                const int j = static_cast<int>(amrex::Math::floor(y));
+                const int k = static_cast<int>(amrex::Math::floor(z));
+
+                // Interpolation weights in each direction (linear basis)
+                const amrex::Real wx_hi = (x - i);
+                const amrex::Real wy_hi = (y - j);
+                const amrex::Real wz_hi = (z - k);
+
+                const amrex::Real wx_lo = 1.0 - wx_hi;
+                const amrex::Real wy_lo = 1.0 - wy_hi;
+                const amrex::Real wz_lo = 1.0 - wz_hi;
+
+                const int iproc = pp.cpu();
+
+                for (int ic = 0; ic < AMREX_SPACEDIM; ++ic) {
+                    pp.rdata(ic) =
+                        wx_lo * wy_lo * wz_lo * varr(i, j, k, ic) +
+                        wx_lo * wy_lo * wz_hi * varr(i, j, k + 1, ic) +
+                        wx_lo * wy_hi * wz_lo * varr(i, j + 1, k, ic) +
+                        wx_lo * wy_hi * wz_hi * varr(i, j + 1, k + 1, ic) +
+                        wx_hi * wy_lo * wz_lo * varr(i + 1, j, k, ic) +
+                        wx_hi * wy_lo * wz_hi * varr(i + 1, j, k + 1, ic) +
+                        wx_hi * wy_hi * wz_lo * varr(i + 1, j + 1, k, ic) +
+                        wx_hi * wy_hi * wz_hi * varr(i + 1, j + 1, k + 1, ic);
+
+                    // Reset position vectors so that the particles return back
+                    // to the MPI ranks with the turbines upon redistribution
+                    pp.pos(ic) = dptr[iproc][ic];
+                }
+            });
+        }
+    }
+}
+
+/** Determine position vector of a point within each MPI rank
+ *
+ *  Loops over the boxArray and determines the first patch that belongs to the
+ *  current MPI rank. Uses that to generate a position vector that is known to
+ *  exist in a given rank. Setting the position vectors of the particles to this
+ *  know location will recall particles belonging to this rank during
+ *  Redistribute.
+ */
+void IBContainer::compute_local_coordinates()
+{
+    BL_PROFILE(
+        "amr-wind::ib::IBContainer::compute_local_coordinates");
+    const int nprocs = amrex::ParallelDescriptor::NProcs();
+    const int iproc = amrex::ParallelDescriptor::MyProc();
+
+    // Reset position vectors to zero (required for parallel reduce sum)
+    m_proc_pos.assign(nprocs, vs::Vector::zero());
+
+    // Flag indicating whether a point within the domain belonging to this rank
+    // has been found
+    bool assigned = false;
+    const int nlevels = m_mesh.finestLevel() + 1;
+    for (int lev = 0; ((lev < nlevels) && !assigned); ++lev) {
+        const auto& ba = m_mesh.boxArray(lev);
+        const auto& dm = m_mesh.DistributionMap(lev);
+
+        const int nbx = dm.size();
+        for (int i = 0; (i < nbx) && !assigned; ++i) {
+            if (dm[i] != iproc) continue;
+
+            const auto& geom = m_mesh.Geom(lev);
+            const auto& dx = geom.CellSize();
+            const auto& problo = geom.ProbLo();
+            const auto& bx = ba[i];
+            const int* lo = bx.loVect();
+
+            auto& pvec = m_proc_pos[iproc];
+            pvec.x() = problo[0] + (lo[0] + 0.5) * dx[0];
+            pvec.y() = problo[1] + (lo[1] + 0.5) * dx[1];
+            pvec.z() = problo[2] + (lo[2] + 0.5) * dx[2];
+
+            // Indicate that we have found a point and it is safe to exit the
+            // loop
+            assigned = true;
+        }
+    }
+
+    // Share position vectors with every process
+    amrex::ParallelDescriptor::ReduceRealSum(
+        &(m_proc_pos[0].x()), m_proc_pos.size() * vs::Vector::ncomp);
+    amrex::Gpu::copy(
+        amrex::Gpu::hostToDevice, m_proc_pos.begin(), m_proc_pos.end(),
+        m_pos_device.begin());
+}
+
+void IBContainer::post_regrid_actions() { compute_local_coordinates(); }
+
+} // namespace ib
+} // namespace amr_wind
---
+++ amr-wind/immersed_boundary/IBUtils.cpp
@@ -0,0 +1,122 @@
+#include "amr-wind/immersed_boundary/IBUtils.H"
+#include "amr-wind/immersed_boundary/IBTypes.H"
+
+namespace amr_wind {
+namespace ib {
+namespace utils {
+
+namespace {
+
+/** Convert a bounding box into amrex::Box index space at a given level
+ *
+ *  \param rbx Bounding box as defined in global domain coordinates
+ *  \param geom AMReX geometry information for a given level
+ *  \return The Box instance that defines the index space equivalent to bounding
+ * boxt
+ */
+amrex::Box
+realbox_to_box(const amrex::RealBox& rbx, const amrex::Geometry& geom)
+{
+    const auto* problo = geom.ProbLo();
+    const auto* probhi = geom.ProbHi();
+    const auto* dxi = geom.InvCellSize();
+
+    amrex::IntVect lo, hi;
+    for (int i = 0; i < AMREX_SPACEDIM; ++i) {
+        amrex::Real bbox_min = amrex::max(rbx.lo()[i], problo[i]);
+        amrex::Real bbox_max = amrex::min(rbx.hi()[i], probhi[i]);
+
+        amrex::Real rlo = amrex::Math::floor((bbox_min - problo[i]) * dxi[i]);
+        amrex::Real rhi = amrex::Math::ceil((bbox_max - problo[i]) * dxi[i]);
+
+        lo[i] = static_cast<int>(rlo);
+        hi[i] = static_cast<int>(rhi);
+    }
+
+    return amrex::Box{lo, hi};
+}
+
+} // namespace
+
+std::set<int> determine_influenced_procs(
+    const amrex::AmrCore& mesh, const amrex::RealBox& rbx)
+{
+    std::set<int> procs;
+    const int finest_level = mesh.finestLevel();
+    const int nlevels = mesh.finestLevel() + 1;
+    auto bx = realbox_to_box(rbx, mesh.Geom(0));
+
+    for (int lev = 0; lev < nlevels; ++lev) {
+        const auto& ba = mesh.boxArray(lev);
+        const auto& dm = mesh.DistributionMap(lev);
+
+        // Get all possible intersections at this level
+        const auto& isects = ba.intersections(bx);
+
+        // Extract the processor ranks
+        for (const auto& is : isects) procs.insert(dm[is.first]);
+
+        if (lev < finest_level) bx = bx.refine(mesh.refRatio(lev));
+    }
+
+    return procs;
+}
+
+void determine_root_proc(IBInfo& info, amrex::Vector<int>& ib_proc_count)
+{
+    auto& plist = info.procs;
+    bool assigned = false;
+
+    // If any of the influenced procs is free (i.e., doesn't have a turbine
+    // assigned to it) elect it as the root proc for this turbine and return
+    // early.
+    for (auto ip : plist) {
+        if (ib_proc_count[ip] < 1) {
+            info.root_proc = ip;
+            ++ib_proc_count[ip];
+            assigned = true;
+            break;
+        }
+    }
+
+    // If we found a root proc there is nothing more to do, so return early
+    if (assigned) {
+        const int iproc = amrex::ParallelDescriptor::MyProc();
+        auto in_proc = info.procs.find(iproc);
+        info.ib_in_proc = (in_proc != info.procs.end());
+        info.is_root_proc = (info.root_proc == iproc);
+
+        // By default we request all processes where turbine is active to have
+        // velocities sampled. Individual immersed boundary instances can override this
+        info.sample_vel_in_proc = info.ib_in_proc;
+        return;
+    }
+
+    // If we have reached here, then we have more turbines than processes
+    // available. We will assign the current turbine to the process that is
+    // managing the lowest number of turbines.
+
+    // Determine the MPI rank that contains the fewest turbines
+    auto it = std::min_element(ib_proc_count.begin(), ib_proc_count.end());
+    // Make it the root process for this turbine
+    info.root_proc = std::distance(ib_proc_count.begin(), it);
+    // Make sure the root process is part of the process list
+    plist.insert(info.root_proc);
+    // Increment turbine count with the global tracking array
+    ++(*it);
+
+    {
+        const int iproc = amrex::ParallelDescriptor::MyProc();
+        auto in_proc = info.procs.find(iproc);
+        info.ib_in_proc = (in_proc != info.procs.end());
+        info.is_root_proc = (info.root_proc == iproc);
+
+        // By default we request all processes where turbine is active to have
+        // velocities sampled. Individual immersed boundaries instances can override this
+        info.sample_vel_in_proc = info.ib_in_proc;
+    }
+}
+
+} // namespace utils
+} // namespace ib
+} // namespace amr_wind
