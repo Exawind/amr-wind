2e54ecc446dd2128150eb3708bc59074afba6b3d
gdeskos <gdeskosv@gmail.com>
2021-05-07 13:06:26 -0600

    Added geometries directory + Sphere

---
+++ amr-wind/immersed_boundary/geometries/Sphere.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/immersed_boundary/geometries/Sphere.H"
+#include "amr-wind/immersed_boundary/geometries/sphere_ops.H"
+#include "amr-wind/immersed_boundary/IBModel.H"
+
+namespace amr_wind {
+namespace ib {
+
+template class IBModel<Sphere>;
+
+} // namespace ib
+} // namespace amr_wind
