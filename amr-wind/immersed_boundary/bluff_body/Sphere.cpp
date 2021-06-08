ae42a6ffbd63e431bdfcdfb5be573e91fad43777
gdeskos <gdeskosv@gmail.com>
2021-06-08 15:55:27 -0600

    adding the sphere geometry class

---
+++ amr-wind/immersed_boundary/bluff_body/Sphere.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/immersed_boundary/bluff_body/Sphere.H"
+#include "amr-wind/immersed_boundary/bluff_body/sphere_ops.H"
+#include "amr-wind/immersed_boundary/IBModel.H"
+
+namespace amr_wind {
+namespace ib {
+
+template class IBModel<Sphere>;
+
+} // namespace ib
+} // namespace amr_wind
