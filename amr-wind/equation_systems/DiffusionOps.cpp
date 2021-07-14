405b29d7d2e1bf15ce00ac6de9e3a4467816b355
asharma <ashesh.sharma@nrel.gov>
2021-07-14 12:39:27 -0600

    accomadate mesh mapping in diffusion solve

--- amr-wind/equation_systems/DiffusionOps.cpp
+++ amr-wind/equation_systems/DiffusionOps.cpp
@@ -93,6 +93,9 @@ void DiffSolverIface<LinOp>::linsys_solve_impl()
     FieldState fstate = FieldState::New;
     auto& repo = this->m_pdefields.repo;
     auto& field = this->m_pdefields.field;
+    if (field.is_mesh_mapped()) {
+        amrex::Abort("For density solve, velocity should not be mesh mapped.");
+    }
     const auto& density = m_density.state(fstate);
     const int nlevels = repo.num_active_levels();
     const int ndim = field.num_comp();
