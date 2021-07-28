9dcd447c0b90ec8fe4562b8cd7f97051365e5593
Diederik Beckers <beckersdiederik@gmail.com>
2021-07-28 11:00:00 -0700

    Update QCriterionRefinement.cpp
    Formatting

--- amr-wind/utilities/tagging/QCriterionRefinement.cpp
+++ amr-wind/utilities/tagging/QCriterionRefinement.cpp
@@ -89,7 +89,7 @@ void QCriterionRefinement::operator()(
                     0.5 * (vel(i, j, k + 1, 2) - vel(i, j, k - 1, 2)) * idx[2];
 
                 const auto S2 =
-                    ux * ux + vy * vy + wz * wz + 0.5 * std::pow(uy + vx, 2) + 
+                    ux * ux + vy * vy + wz * wz + 0.5 * std::pow(uy + vx, 2) +
                     0.5 * std::pow(vz + wy, 2) + 0.5 * std::pow(wx + uz, 2);
 
                 const auto W2 = 0.5 * std::pow(uy - vx, 2) +
