0ecbed9074ff6edf7f29e4c5cfe0f944e99912b2
Diederik Beckers <dbeckers@el1.ib0.cm.hpc.nrel.gov>
2021-08-11 11:56:51 -0600

    working error check version for linear function

--- error_check/coarsenPlotfile.cpp
+++ error_check/coarsenPlotfile.cpp
@@ -42,16 +42,18 @@ main (int   argc,
   
         PlotFileData pf1(plotFileName1);
         PlotFileData pf2(plotFileName2);
-        int Nlev = pf2.finestLevel() + 1;
+        int finest_level = pf2.finestLevel();
+        int Nlev = finest_level + 1;
         //int Nlev = 1; pp.query("levels",Nlev);
         int ratio = 2;// pp.query("ratio",ratio);
+        int referenceRefinement = 2;// pp.query("ratio",ratio);
   
         const auto& dx_1 = pf1.cellSize(0);
         const auto& dx_2 = pf2.cellSize(Nlev-1);
   	    std::cout << "dx_1 = " << dx_1 << "; dx_2 = " << dx_2 << std::endl;
-        bool not_match = AMREX_D_TERM(   dx_1[0] != dx_2[0],
-                                      || dx_1[1] != dx_2[1],
-                                      || dx_1[2] != dx_2[2] );
+        bool not_match = AMREX_D_TERM(   dx_1[0] * referenceRefinement != dx_2[0],
+                                      || dx_1[1] * referenceRefinement != dx_2[1],
+                                      || dx_1[2] * referenceRefinement != dx_2[2] );
         if (not_match) {
             amrex::Abort("ERROR: grid dx at finest level does not match");
         }
@@ -95,58 +97,75 @@ main (int   argc,
         const auto& dm = mf1.DistributionMap();
   
   
-        for (int lev=0; lev<Nlev-1; ++lev) {
+  	    amrex::Real L2error = 0.0;
+        for (int lev=0; lev<=finest_level; ++lev) {
   
-  	        int r = std::pow(ratio,Nlev-lev-1);
-  	        std::cout << "coarsening reference solution (level " << Nlev-1  << ") to level " << lev << "; ratio from reference = " << r << std::endl;
+  	        int r = std::pow(ratio,finest_level-lev) * referenceRefinement;
+  	        std::cout << "coarsening reference solution (level " << Nlev  << ") to level " << lev << "; ratio from reference = " << r << std::endl;
   
             Box cpd = Box(fpd).coarsen(r);
             geoms[lev].define(cpd,rb,coordSys,is_per);
             levelSteps[lev] = pf1.levelStep(0);
-            if (lev < Nlev-1) {
-              refRatio[lev] = IntVect(AMREX_D_DECL(ratio,ratio,ratio));
-            }
+            refRatio[lev] = IntVect(AMREX_D_DECL(ratio,ratio,ratio));
   
             BoxArray cba = BoxArray(fba).coarsen(r);
             AMREX_ALWAYS_ASSERT_WITH_MESSAGE(BoxArray(cba).refine(r) == fba,"BoxArray doesn't coarsen correctly");
   
             data[lev] = new MultiFab(cba,dm,nComp,0);
             average_down(mf1,*data[lev],fgeom,geoms[lev],0,nComp,r);
-  
-        }
+            
+			auto mf2_lev = (nComp == 1 ? pf2.get(lev,varnames[0]) : pf2.get(lev));
 
-  	    // compare with refined solution
-  	    amrex::Real L2error = 0.0;
-		for (int lev=0; lev<Nlev; ++lev) {
-			
-			std::cout << "comparing level " << lev << std::endl;
+			MultiFab mf_b;
+		    mf_b.define(mf2_lev.boxArray(), mf2_lev.DistributionMap(), 1, 0);
+			mf_b.ParallelCopy(*data[lev]);
 
-            auto mf2 = (nComp == 1 ? pf2.get(lev,varnames[0]) : pf2.get(lev));
+			amrex::iMultiFab level_mask;
+       	    if (lev < finest_level) {
+				auto mf2_levp1 = (nComp == 1 ? pf2.get(lev + 1,varnames[0]) : pf2.get(lev + 1));
+       	        level_mask = makeFineMask(
+       	            mf2_lev.boxArray(), mf2_lev.DistributionMap(),
+       	            mf2_levp1.boxArray(), amrex::IntVect(2), 1, 0);
+       	    } else {
+       	        level_mask.define(
+       	            mf2_lev.boxArray(), mf2_lev.DistributionMap(),
+       	            1, 0, amrex::MFInfo());
+       	        level_mask.setVal(1);
+       	    }	
 
-  	        for (amrex::MFIter mfi(mf2); mfi.isValid(); ++mfi) {
+  	    	// compare with refined solution
+  	    	
+            const auto& dx = pf2.cellSize(lev);
+  	        for (amrex::MFIter mfi(mf2_lev); mfi.isValid(); ++mfi) {
                 const auto& vbx = mfi.validbox();
-  	            
-				amrex::Array4<amrex::Real const> var1; 
-				if (lev == Nlev - 1){
-					var1 = mf1.array(mfi);
-				} else {
-					var1 = data[lev]->array(mfi);
-				}
   	            std::cout << "box: " << vbx << std::endl;
-  	            auto var2 = mf2.array(mfi);
+  	            auto mask = level_mask.array(mfi); 
+			    auto var1 = mf_b.array(mfi);
+  	            auto var2 = mf2_lev.array(mfi);
   	            amrex::Loop(
                       vbx, [=, &L2error] (int i, int j, int k) noexcept {
-                          const amrex::Real diff = var2(i, j, k, 0) - var1(i, j, k, 0);
+						  const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+                		  const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
+                		  const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
+
+                          const amrex::Real diff = mask(i, j ,k) * (var2(i, j, k, 0) - var1(i, j, k, 0));
                           L2error += std::pow(diff,2);
+
+						 // if(diff != 0.0){
+						 // 	  std::cout << "y + z = " << y + z << std::endl;
+						 //     std::cout << "var1 = " << var1(i,j,k,0) << "; var2 = " << var2(i,j,k,0) << std::endl;
+						 // }
                       });
   	        }
-		}
+  	        std::cout << "level " << lev << " done; L2error squared = " << L2error << std::endl;
+  
+        }
 
 		L2error = std::sqrt(L2error);
   		std::cout << "L2error = " << L2error << std::endl;	
   
         Vector<const MultiFab*> dat(Nlev);
-        for (int i=0; i<Nlev-1; ++i) {
+        for (int i=0; i<Nlev; ++i) {
           dat[i] = data[i];
         }
         WriteMultiLevelPlotfile(getFileRoot(plotFileName1)+"_crse",Nlev,dat,varnames,geoms,pf1.time(),levelSteps,refRatio);
