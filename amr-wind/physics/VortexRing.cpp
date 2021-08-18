51c526a51a5c057681288beeef7b0ca809669985
Diederik Beckers <beckersdiederik@gmail.com>
2021-08-18 16:43:03 -0600

    Updated VortexRing to allow parameters to be specified and correct BCs to be set

--- amr-wind/physics/VortexRing.cpp
+++ amr-wind/physics/VortexRing.cpp
@@ -27,8 +27,17 @@ VortexRing::VortexRing(const CFDSim& sim)
 	, m_velocity(sim.repo().get_field("velocity"))
     , m_density(sim.repo().get_field("density"))
 {
-    amrex::ParmParse pp("incflo");
-    pp.query("density", m_rho);
+	{
+    	amrex::ParmParse pp("incflo");
+    	pp.query("density", m_rho);
+	}
+
+	{
+    	amrex::ParmParse pp("vortexring");
+    	pp.query("R", m_R);
+    	pp.query("Gamma", m_Gamma);
+    	pp.query("alpha", m_alpha);
+	}
 
 	sim.repo().declare_nd_field("vorticity", 3, 1, 1);    
 	sim.repo().declare_nd_field("vectorpotential", 3, 1, 1);    
@@ -57,6 +66,10 @@ void VortexRing::initialize_fields(
     const amrex::Real Ly = probhi[1] - problo[1];
     const amrex::Real Lz = probhi[2] - problo[2];
 
+	const amrex::Real R = m_R;
+	const amrex::Real Gamma = m_Gamma;
+	const amrex::Real alpha = m_alpha;
+
     for (amrex::MFIter mfi(velocity); mfi.isValid(); ++mfi) {
         const auto& dx = geom.CellSizeArray();
 		const auto& nbx = mfi.nodaltilebox();
@@ -64,50 +77,67 @@ void VortexRing::initialize_fields(
 
         amrex::ParallelFor(
             nbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-				const amrex::Real m_R = 1.0;
-				const amrex::Real m_Gamma = 1.0;
-				const amrex::Real m_alpha = 0.54857674;
 
                 const amrex::Real x = problo[0] + i * dx[0];
                 const amrex::Real y = problo[1] + j * dx[1];
                 const amrex::Real z = problo[2] + k * dx[2];
                 const amrex::Real r = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
                 const amrex::Real theta = std::atan2(y, x);
-				const amrex::Real ssq = std::pow(z, 2) + std::pow(r - m_R, 2);
+				const amrex::Real ssq = std::pow(z, 2) + std::pow(r - R, 2);
 				const amrex::Real s = std::sqrt(ssq);
-                vort(i, j, k, 0) = -std::sin(theta) * vorticity_theta(s, ssq, m_R, m_Gamma, m_alpha);
-                vort(i, j, k, 1) = std::cos(theta) * vorticity_theta(s, ssq, m_R, m_Gamma, m_alpha);
+                vort(i, j, k, 0) = -std::sin(theta) * vorticity_theta(s, ssq, R, Gamma, alpha);
+                vort(i, j, k, 1) = std::cos(theta) * vorticity_theta(s, ssq, R, Gamma, alpha);
                 vort(i, j, k, 2) = 0.0;
             });
     }
 
 	amrex::LPInfo info;
     auto& mesh = m_velocity.repo().mesh();
-    amrex::MLNodeLaplacian linop({mesh.Geom(level)}, {mesh.boxArray(level)}, {mesh.DistributionMap(level)}, info, {}, 1.0);
+    amrex::MLNodeLaplacian linop(mesh.Geom(0,level), mesh.boxArray(0, level), mesh.DistributionMap(0, level), info, {}, 1.0);
 
-    linop.setDomainBC({amrex::LinOpBCType::Periodic,amrex::LinOpBCType::Periodic,amrex::LinOpBCType::Periodic},
-                      {amrex::LinOpBCType::Periodic,amrex::LinOpBCType::Periodic,amrex::LinOpBCType::Periodic});
+    amrex::Array<amrex::LinOpBCType, AMREX_SPACEDIM> bclo;
+    amrex::Array<amrex::LinOpBCType, AMREX_SPACEDIM> bchi;
 
-    amrex::MLMG mlmg(linop);
+    const auto& bctype = m_velocity.bc_type();
+    for (int dir = 0; dir < AMREX_SPACEDIM; ++dir) {
+        if (mesh.Geom(0).isPeriodic(dir)) {
+            bclo[dir] = amrex::LinOpBCType::Periodic;
+            bchi[dir] = amrex::LinOpBCType::Periodic;
+        } else {
 
-    if(level == 0) {
-        vectorpotential(level).setVal(0.0,0,3,1);
-    } else {
-        amrex::PhysBCFunctNoOp bcnoop;
-        amrex::Vector<amrex::BCRec> bcrec(1);
-        amrex::InterpFromCoarseLevel(vectorpotential(level), 0.0,
-                                   vectorpotential(level-1), 0, 0, 3,
-                                   mesh.Geom(level-1), mesh.Geom(level),
-                                   bcnoop, 0, bcnoop, 0,
-                                   amrex::IntVect{2},
-                                   & amrex::node_bilinear_interp,
-                                   bcrec, 0);
+            switch (bctype[amrex::Orientation(dir, amrex::Orientation::low)]) {
+                case BC::pressure_inflow:
+                case BC::pressure_outflow: {
+                    bclo[dir] = amrex::LinOpBCType::Dirichlet;
+                    break;
+                }
+                default:
+                    bclo[dir] = amrex::LinOpBCType::Neumann;
+                    break;
+            };
+
+            switch (bctype[amrex::Orientation(dir, amrex::Orientation::high)]) {
+                case BC::pressure_inflow:
+                case BC::pressure_outflow: {
+                    bchi[dir] = amrex::LinOpBCType::Dirichlet;
+                    break;
+                }
+                default:
+                    bchi[dir] = amrex::LinOpBCType::Neumann;
+                    break;
+            };
+        }
     }
 
+    linop.setDomainBC(bclo,bchi);
+    amrex::MLMG mlmg(linop);
+
+    vectorpotential(0).setVal(0.0,0,3,1);
+
 	for(int i=0;i<AMREX_SPACEDIM;++i){
-        auto vectorpot = vectorpotential.subview(i);
-        auto vort = vorticity.subview(i);
-        mlmg.solve({&vectorpot(level)}, {&vort(level)}, 1.0e-6, 0.0);
+        auto vectorpot = vectorpotential.subview(i,1,level+1);
+        auto vort = vorticity.subview(i,1,level+1);
+        mlmg.solve(vectorpot.vec_ptrs(), vort.vec_const_ptrs(), 1.0e-6, 0.0);
     }
 
     for (amrex::MFIter mfi(velocity); mfi.isValid(); ++mfi) {
