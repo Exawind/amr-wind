f22d9c929465b08d14c2d6520d7c1ae42ceea1b3
michael brazell <michael.brazell@nrel.gov>
2021-08-25 09:29:56 -0700

    moved the get fields into post init so physics can be called in arbitrary order

--- amr-wind/physics/TracerTag.cpp
+++ amr-wind/physics/TracerTag.cpp
@@ -4,8 +4,7 @@
 
 namespace amr_wind {
 
-TracerTag::TracerTag(CFDSim& sim)
-    : m_sim(sim), m_act_src(sim.repo().get_field("actuator_src_term"))
+TracerTag::TracerTag(CFDSim& sim) : m_sim(sim)
 {
     auto& teqn = sim.pde_manager().register_transport_pde("TaggingScalar");
     m_tracer = &(teqn.fields().field);
@@ -22,24 +21,55 @@ void TracerTag::initialize_fields(int level, const amrex::Geometry&)
     (*m_tracer)(level).setVal(0.0);
 }
 
+void TracerTag::post_init_actions()
+{
+
+    if (m_sim.repo().field_exists("actuator_src_term"))
+        m_act_src = &(m_sim.repo().get_field("actuator_src_term"));
+
+    if (m_sim.repo().field_exists("iblank_cell"))
+        m_iblank = &(m_sim.repo().get_int_field("iblank_cell"));
+}
+
 void TracerTag::post_advance_work()
 {
+
+    if (!m_act_src && !m_iblank) {
+        amrex::Print() << "Warning TracerTag activated but neither actuators "
+                          "or overset are being used"
+                       << std::endl;
+        return;
+    }
 
     for (int level = 0; level <= m_sim.mesh().finestLevel(); ++level) {
         for (amrex::MFIter mfi((*m_tracer)(level)); mfi.isValid(); ++mfi) {
             const auto& vbx = mfi.tilebox();
             auto tracer = (*m_tracer)(level).array(mfi);
-            auto src = m_act_src(level).array(mfi);
 
-            amrex::ParallelFor(
-                vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                    const amrex::Real srcmag = std::sqrt(
-                        src(i, j, k, 0) * src(i, j, k, 0) +
-                        src(i, j, k, 1) * src(i, j, k, 1) +
-                        src(i, j, k, 2) * src(i, j, k, 2));
+            if (m_act_src) {
+                auto src = (*m_act_src)(level).array(mfi);
 
-                    if (srcmag > m_src_threshold) tracer(i, j, k) = 1.0;
-                });
+                amrex::ParallelFor(
+                    vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                        const amrex::Real srcmag = std::sqrt(
+                            src(i, j, k, 0) * src(i, j, k, 0) +
+                            src(i, j, k, 1) * src(i, j, k, 1) +
+                            src(i, j, k, 2) * src(i, j, k, 2));
+
+                        if (srcmag > m_src_threshold) tracer(i, j, k) = 1.0;
+                    });
+            }
+
+            if (m_iblank) {
+                auto iblank = (*m_iblank)(level).array(mfi);
+                amrex::ParallelFor(
+                    vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                        if (tag_fringe && iblank(i, j, k) == -1)
+                            tracer(i, j, k) = 1.0;
+                        if (tag_hole && iblank(i, j, k) == 0)
+                            tracer(i, j, k) = 1.0;
+                    });
+            }
         }
     }
 }
