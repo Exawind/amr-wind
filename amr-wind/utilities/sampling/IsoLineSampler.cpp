4a2cf29988fdf9d2a19a8c657a86434fe04861f1
Michael Kuhn <mkuhn@nrel.gov>
2021-10-20 23:41:32 -0600

    Set up a unit test for isosampling and in the process needed to add a line sampler.

---
+++ amr-wind/utilities/sampling/IsoLineSampler.cpp
@@ -0,0 +1,104 @@
+#include "amr-wind/utilities/sampling/IsoLineSampler.H"
+#include "amr-wind/CFDSim.H"
+#include "amr-wind/utilities/tensor_ops.H"
+
+#include "AMReX_ParmParse.H"
+
+namespace amr_wind {
+namespace sampling {
+
+IsoLineSampler::IsoLineSampler(const CFDSim& sim) : m_sim(sim) {}
+
+IsoLineSampler::~IsoLineSampler() = default;
+
+void IsoLineSampler::initialize(const std::string& key)
+{
+    amrex::ParmParse pp(key);
+
+    pp.get("num_points", m_npts);
+    pp.getarr("start", m_start);
+    pp.getarr("end", m_end);
+    pp.getarr("iso_orientation", m_oris);
+    amrex::Real mag = 0;
+    for (int i = 0; i < m_oris.size(); ++i) {
+        // Get norm
+        mag += std::pow(m_oris[i], 2);
+    }
+    for (int i = 0; i < m_oris.size(); ++i) {
+        // Normalize orientation vector
+        m_oris[i] /= mag;
+    }
+
+    const int lev = 0;
+    const auto* prob_lo = m_sim.mesh().Geom(lev).ProbLo();
+    const auto* prob_hi = m_sim.mesh().Geom(lev).ProbHi();
+
+    bool all_ok = true;
+    for (int d = 0; d < AMREX_SPACEDIM; ++d) {
+        if (m_start[d] < prob_lo[d]) {
+            all_ok = false;
+            m_start[d] = prob_lo[d];
+        }
+        if (m_start[d] > prob_hi[d]) {
+            all_ok = false;
+            m_start[d] = prob_lo[d];
+        }
+        if (m_end[d] < prob_lo[d]) {
+            all_ok = false;
+            m_end[d] = prob_lo[d];
+        }
+        if (m_end[d] > prob_hi[d]) {
+            all_ok = false;
+            m_end[d] = prob_lo[d];
+        }
+    }
+    if (!all_ok) {
+        amrex::Print() << "WARNING: IsoLineSampler: Out of domain line was "
+                          "truncated to match domain"
+                       << std::endl;
+    }
+}
+
+void IsoLineSampler::sampling_locations(SampleLocType& locs) const
+{
+    locs.resize(m_npts);
+
+    const amrex::Real ndiv = amrex::max(m_npts - 1, 1);
+    amrex::Array<amrex::Real, AMREX_SPACEDIM> dx;
+
+    for (int d = 0; d < AMREX_SPACEDIM; ++d)
+        dx[d] = (m_end[d] - m_start[d]) / ndiv;
+
+    for (int i = 0; i < m_npts; ++i) {
+        for (int d = 0; d < AMREX_SPACEDIM; ++d)
+            locs[i][d] = m_start[d] + i * dx[d];
+    }
+}
+
+void IsoLineSampler::sampling_orientations(SampleLocType& oris) const
+{
+    oris.resize(m_npts);
+
+    for (int i = 0; i < m_npts; ++i) {
+        for (int d = 0; d < AMREX_SPACEDIM; ++d)
+            // orientation is constant for all points in line
+            oris[i][d] = m_oris[d];
+    }
+}
+
+#ifdef AMR_WIND_USE_NETCDF
+void IsoLineSampler::define_netcdf_metadata(const ncutils::NCGroup& grp) const
+{
+    grp.put_attr("sampling_type", identifier());
+    grp.put_attr("start", m_start);
+    grp.put_attr("end", m_end);
+}
+
+void IsoLineSampler::populate_netcdf_metadata(const ncutils::NCGroup&) const {}
+#else
+void IsoLineSampler::define_netcdf_metadata(const ncutils::NCGroup&) const {}
+void IsoLineSampler::populate_netcdf_metadata(const ncutils::NCGroup&) const {}
+#endif
+
+} // namespace sampling
+} // namespace amr_wind
---
+++ unit_tests/utilities/test_iso_sampling.cpp
@@ -0,0 +1,126 @@
+
+#include "aw_test_utils/MeshTest.H"
+
+#include "amr-wind/utilities/sampling/IsoSampling.H"
+#include "amr-wind/utilities/sampling/SamplingContainer.H"
+#include "amr-wind/utilities/sampling/IsoLineSampler.H"
+
+namespace amr_wind_tests {
+
+namespace {
+
+void init_vof(amr_wind::Field& fld, amrex::Real water_level)
+{
+    const auto& mesh = fld.repo().mesh();
+    const int nlevels = fld.repo().num_active_levels();
+    const int ncomp = fld.num_comp();
+
+    amrex::Real offset = 0.0;
+    if (fld.field_location() == amr_wind::FieldLoc::CELL) offset = 0.5;
+
+    for (int lev = 0; lev < nlevels; ++lev) {
+        const auto& dx = mesh.Geom(lev).CellSizeArray();
+        const auto& problo = mesh.Geom(lev).ProbLoArray();
+
+        for (amrex::MFIter mfi(fld(lev)); mfi.isValid(); ++mfi) {
+            auto bx = mfi.growntilebox();
+            const auto& farr = fld(lev).array(mfi);
+
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+                const amrex::Real z = problo[2] + (k + offset) * dx[2];
+
+                for (int d = 0; d < ncomp; d++) {
+                    if (z > water_level) {
+                        farr(i, j, k, d) = 1.0;
+                    } else {
+                        farr(i, j, k, d) = 0.0;
+                    }
+                }
+            });
+        }
+    }
+}
+
+class IsoSamplingImpl : public amr_wind::sampling::IsoSampling
+{
+public:
+    IsoSamplingImpl(amr_wind::CFDSim& sim, const std::string& label)
+        : amr_wind::sampling::IsoSampling(sim, label)
+    {}
+
+protected:
+    void prepare_netcdf_file() override {}
+    void process_output() override
+    {
+        // Test buffer populate for GPU runs
+        std::vector<double> buf(
+            num_total_particles() * var_names().size(), 0.0);
+        sampling_container().populate_buffer(buf);
+    }
+};
+
+} // namespace
+
+class IsoSamplingTest : public MeshTest
+{
+protected:
+    void populate_parameters() override
+    {
+        MeshTest::populate_parameters();
+
+        {
+            amrex::ParmParse pp("amr");
+            amrex::Vector<int> ncell{{32, 32, 64}};
+            pp.add("max_level", 0);
+            pp.add("max_grid_size", 16);
+            pp.addarr("n_cell", ncell);
+        }
+        {
+            amrex::ParmParse pp("geometry");
+            amrex::Vector<amrex::Real> problo{{0.0, 0.0, 0.0}};
+            amrex::Vector<amrex::Real> probhi{{128.0, 128.0, 128.0}};
+
+            pp.addarr("prob_lo", problo);
+            pp.addarr("prob_hi", probhi);
+        }
+    }
+};
+
+TEST_F(IsoSamplingTest, sampling)
+{
+    initialize_mesh();
+    auto& repo = sim().repo();
+    auto& vof = repo.declare_field("vof", 1, 2);
+    init_vof(vof, 0.5*(128.0 - 0.0));
+
+    {
+        amrex::ParmParse pp("isosampling");
+        pp.add("output_frequency", 1);
+        pp.addarr("labels", amrex::Vector<std::string>{"IL1","IL2"});
+    }
+    {
+        amrex::ParmParse pp("isosampling.IL1");
+        pp.add("type", std::string("IsoLineSampler"));
+        pp.add("field", std::string("vof"));
+        pp.add("num_points", 16);
+        pp.addarr("start", amrex::Vector<amrex::Real>{64.0,0.0,0.0});
+        pp.addarr("end", amrex::Vector<amrex::Real>{64.0,128.0,0.0});
+        pp.addarr("iso_orientation", amrex::Vector<amrex::Real>{0.0,0.0,1.0});
+    }
+    {
+        amrex::ParmParse pp("isosampling.IL2");
+        pp.add("type", std::string("IsoLineSampler"));
+        pp.add("field", std::string("vof"));
+        pp.add("num_points", 16);
+        pp.addarr("start", amrex::Vector<amrex::Real>{0.0,0.0,0.0});
+        pp.addarr("end", amrex::Vector<amrex::Real>{0.0,128.0,0.0});
+        pp.addarr("iso_orientation", amrex::Vector<amrex::Real>{1.0,0.0,1.0});
+    }
+
+    // amr_wind::sampling::Sampling probes(sim(), "sampling");
+    IsoSamplingImpl probes(sim(), "isosampling");
+    probes.initialize();
+    //probes.post_advance_work();
+}
+
+} // namespace amr_wind_tests
