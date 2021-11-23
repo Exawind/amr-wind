d88ad46c5fe014dcb41ba6bdd1c2c0cd07df35c3
psakievich <psakievich@gmail.com>
2021-11-23 13:02:42 -0700

    ActDisk: Multiple Spread functions (#494)
    * ActDisk: Multiple Spread functions
    
    This commit allows users to specify multiple
    spreading models for the UniformCtDisk model.
    This requires you to include the "spreading"
    key in the input file.  This is a prototype to
    keep actuator types from becoming too verbose.
    
    * Formatting
    
    * WIP debugging gaussian adm
    
    * Fix Gaussian computation
    
    * Restore default test
    
    * Remove stale comments
    
    * Move spreading polymorph to delegation
    
    Use function pointers to provide a delegate model for configurable
    spreading functions
    
    * Clean up some stale code
    
    * Make functions public for cuda
    
    * update error to amrex::Abort
    
    * Finish changes requested by the review
    
    * One more missed const
    
    * Remove unused variables

--- amr-wind/wind_energy/actuator/disk/ActuatorDisk.cpp
+++ amr-wind/wind_energy/actuator/disk/ActuatorDisk.cpp
@@ -1,10 +1,11 @@
 #include "amr-wind/wind_energy/actuator/disk/UniformCt.H"
 #include "amr-wind/wind_energy/actuator/disk/uniform_ct_ops.H"
 #include "amr-wind/wind_energy/actuator/ActuatorModel.H"
+#include "amr-wind/wind_energy/actuator/disk/disk_spreading.H"
 
 namespace amr_wind {
 namespace actuator {
-
 template class ActModel<UniformCt, ActSrcDisk>;
-}
+
+} // namespace actuator
 } // namespace amr_wind
