ba68c0e2ae043d3c378390c5145ceb9f8db58ee7
asharma <ashesh.sharma@nrel.gov>
2021-12-01 23:44:52 -0700

    change channel flow mesh mapping function

--- amr-wind/mesh_mapping_models/ChannelFlowScaling.cpp
+++ amr-wind/mesh_mapping_models/ChannelFlowScaling.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include "amr-wind/mesh_mapping_models/ChannelFlowScaling.H"
 #include "amr-wind/CFDSim.H"
 
@@ -9,20 +11,31 @@ ChannelFlowScaling::ChannelFlowScaling(const CFDSim& sim)
     : m_mesh_scale_fac_cc(sim.repo().get_field("mesh_scaling_factor_cc"))
     , m_mesh_scale_fac_nd(sim.repo().get_field("mesh_scaling_factor_nd"))
 {
-    amrex::ParmParse pp("ChannelFlowScaling");
-    pp.queryarr("alpha", m_alpha, 0, AMREX_SPACEDIM);
-    pp.queryarr("beta", m_beta, 0, AMREX_SPACEDIM);
-    pp.queryarr("do_map", m_map, 0, AMREX_SPACEDIM);
+    {
+        amrex::ParmParse pp("ChannelFlowScaling");
+        pp.queryarr("beta", m_beta, 0, AMREX_SPACEDIM);
+        pp.queryarr("do_map", m_map, 0, AMREX_SPACEDIM);
+    }
+
+    {
+        amrex::ParmParse pp("amr");
+        pp.getarr("n_cell", n_cell, 0, AMREX_SPACEDIM);
+    }
 }
 
 /** Construct the mesh mapping field
  */
 void ChannelFlowScaling::create_map(int lev, const amrex::Geometry& geom)
 {
+    const amrex::Real eps = m_eps;
+    const auto beta = m_beta;
+    const auto do_map = m_map;
+    const auto num_cell = n_cell;
+
     const auto& dx = geom.CellSizeArray();
     const auto& prob_lo = geom.ProbLoArray();
     const auto& prob_hi = geom.ProbHiArray();
-    amrex::Vector<amrex::Real> ht{
+    amrex::Vector<amrex::Real> len{
         {prob_hi[0] - prob_lo[0], prob_hi[1] - prob_lo[1],
          prob_hi[2] - prob_lo[2]}};
 
@@ -37,62 +50,58 @@ void ChannelFlowScaling::create_map(int lev, const amrex::Geometry& geom)
                 amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
                 amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
 
-                amrex::Real num_x =
-                    -(2 * m_beta[0] * ht[0] *
-                      log((m_beta[0] + 1) / (m_beta[0] - 1)) *
-                      std::pow(
-                          ((m_beta[0] + 1) / (m_beta[0] - 1)),
-                          ((m_alpha[0] + x) / (1 - m_alpha[0]))));
-                amrex::Real num_y =
-                    -(2 * m_beta[1] * ht[1] *
-                      log((m_beta[1] + 1) / (m_beta[1] - 1)) *
-                      std::pow(
-                          ((m_beta[1] + 1) / (m_beta[1] - 1)),
-                          ((m_alpha[1] + y) / (1 - m_alpha[1]))));
-                amrex::Real num_z =
-                    -(2 * m_beta[2] * ht[2] *
-                      log((m_beta[2] + 1) / (m_beta[2] - 1)) *
-                      std::pow(
-                          ((m_beta[2] + 1) / (m_beta[2] - 1)),
-                          ((m_alpha[2] + z) / (1 - m_alpha[2]))));
-
-                amrex::Real den_x =
-                    ((m_alpha[0] - 1) * (2 * m_alpha[0] + 1) *
-                     std::pow(
-                         (std::pow(
-                              ((m_beta[0] + 1) / (m_beta[0] - 1)),
-                              (x / (1 - m_alpha[0]))) +
-                          std::pow(
-                              ((m_beta[0] + 1) / (m_beta[0] - 1)),
-                              (m_alpha[0] / (1 - m_alpha[0])))),
-                         2));
-                amrex::Real den_y =
-                    ((m_alpha[1] - 1) * (2 * m_alpha[1] + 1) *
-                     std::pow(
-                         (std::pow(
-                              ((m_beta[1] + 1) / (m_beta[1] - 1)),
-                              (y / (1 - m_alpha[1]))) +
-                          std::pow(
-                              ((m_beta[1] + 1) / (m_beta[1] - 1)),
-                              (m_alpha[1] / (1 - m_alpha[1])))),
-                         2));
-                amrex::Real den_z =
-                    ((m_alpha[2] - 1) * (2 * m_alpha[2] + 1) *
-                     std::pow(
-                         (std::pow(
-                              ((m_beta[2] + 1) / (m_beta[2] - 1)),
-                              (z / (1 - m_alpha[2]))) +
-                          std::pow(
-                              ((m_beta[2] + 1) / (m_beta[2] - 1)),
-                              (m_alpha[2] / (1 - m_alpha[2])))),
-                         2));
+                amrex::Real x_non_uni =
+                    prob_lo[0] +
+                    len[0] / 2 *
+                        (1 -
+                         std::tanh(
+                             beta[0] * (1 - 2 * (x - prob_lo[0]) / len[0])) /
+                             std::tanh(beta[0]));
+                amrex::Real y_non_uni =
+                    prob_lo[1] +
+                    len[1] / 2 *
+                        (1 -
+                         std::tanh(
+                             beta[1] * (1 - 2 * (y - prob_lo[1]) / len[1])) /
+                             std::tanh(beta[1]));
+                amrex::Real z_non_uni =
+                    prob_lo[2] +
+                    len[2] / 2 *
+                        (1 -
+                         std::tanh(
+                             beta[2] * (1 - 2 * (z - prob_lo[2]) / len[2])) /
+                             std::tanh(beta[2]));
 
                 scale_fac_cc(i, j, k, 0) =
-                    m_map[0] ? num_x / den_x / dx[0] : 1.0;
+                    (do_map[0] && (i >= 0) && (i < num_cell[0]))
+                        ? ((x_non_uni - prob_lo[0]) / (i + 0.5 + eps) / dx[0])
+                        : 1.0;
                 scale_fac_cc(i, j, k, 1) =
-                    m_map[1] ? num_y / den_y / dx[1] : 1.0;
+                    (do_map[1] && (j >= 0) && (j < num_cell[1]))
+                        ? ((y_non_uni - prob_lo[1]) / (j + 0.5 + eps) / dx[1])
+                        : 1.0;
                 scale_fac_cc(i, j, k, 2) =
-                    m_map[2] ? num_z / den_z / dx[2] : 1.0;
+                    (do_map[2] && (k >= 0) && (k < num_cell[2]))
+                        ? ((z_non_uni - prob_lo[2]) / (k + 0.5 + eps) / dx[2])
+                        : 1.0;
+
+                // adjust for ghost cells to be at the boundaries
+                if (do_map[0] && (i < 0)) scale_fac_cc(i, j, k, 0) = 0.0;
+                if (do_map[1] && (j < 0)) scale_fac_cc(i, j, k, 1) = 0.0;
+                if (do_map[2] && (k < 0)) scale_fac_cc(i, j, k, 2) = 0.0;
+
+                if (do_map[0] && (i >= num_cell[0])) {
+                    scale_fac_cc(i, j, k, 0) =
+                        (prob_hi[0] - prob_lo[0]) / (num_cell[0] + eps) / dx[0];
+                }
+                if (do_map[1] && (j >= num_cell[1])) {
+                    scale_fac_cc(i, j, k, 1) =
+                        (prob_hi[1] - prob_lo[1]) / (num_cell[1] + eps) / dx[1];
+                }
+                if (do_map[2] && (k >= num_cell[2])) {
+                    scale_fac_cc(i, j, k, 2) =
+                        (prob_hi[2] - prob_lo[2]) / (num_cell[2] + eps) / dx[2];
+                }
             });
 
         const auto& nbx = mfi.grownnodaltilebox();
@@ -104,64 +113,62 @@ void ChannelFlowScaling::create_map(int lev, const amrex::Geometry& geom)
                 amrex::Real y = prob_lo[1] + j * dx[1];
                 amrex::Real z = prob_lo[2] + k * dx[2];
 
-                amrex::Real num_x =
-                    -(2 * m_beta[0] * ht[0] *
-                      log((m_beta[0] + 1) / (m_beta[0] - 1)) *
-                      std::pow(
-                          ((m_beta[0] + 1) / (m_beta[0] - 1)),
-                          ((m_alpha[0] + x) / (1 - m_alpha[0]))));
-                amrex::Real num_y =
-                    -(2 * m_beta[1] * ht[1] *
-                      log((m_beta[1] + 1) / (m_beta[1] - 1)) *
-                      std::pow(
-                          ((m_beta[1] + 1) / (m_beta[1] - 1)),
-                          ((m_alpha[1] + y) / (1 - m_alpha[1]))));
-                amrex::Real num_z =
-                    -(2 * m_beta[2] * ht[2] *
-                      log((m_beta[2] + 1) / (m_beta[2] - 1)) *
-                      std::pow(
-                          ((m_beta[2] + 1) / (m_beta[2] - 1)),
-                          ((m_alpha[2] + z) / (1 - m_alpha[2]))));
-
-                amrex::Real den_x =
-                    ((m_alpha[0] - 1) * (2 * m_alpha[0] + 1) *
-                     std::pow(
-                         (std::pow(
-                              ((m_beta[0] + 1) / (m_beta[0] - 1)),
-                              (x / (1 - m_alpha[0]))) +
-                          std::pow(
-                              ((m_beta[0] + 1) / (m_beta[0] - 1)),
-                              (m_alpha[0] / (1 - m_alpha[0])))),
-                         2));
-                amrex::Real den_y =
-                    ((m_alpha[1] - 1) * (2 * m_alpha[1] + 1) *
-                     std::pow(
-                         (std::pow(
-                              ((m_beta[1] + 1) / (m_beta[1] - 1)),
-                              (y / (1 - m_alpha[1]))) +
-                          std::pow(
-                              ((m_beta[1] + 1) / (m_beta[1] - 1)),
-                              (m_alpha[1] / (1 - m_alpha[1])))),
-                         2));
-                amrex::Real den_z =
-                    ((m_alpha[2] - 1) * (2 * m_alpha[2] + 1) *
-                     std::pow(
-                         (std::pow(
-                              ((m_beta[2] + 1) / (m_beta[2] - 1)),
-                              (z / (1 - m_alpha[2]))) +
-                          std::pow(
-                              ((m_beta[2] + 1) / (m_beta[2] - 1)),
-                              (m_alpha[2] / (1 - m_alpha[2])))),
-                         2));
+                amrex::Real x_non_uni =
+                    prob_lo[0] +
+                    len[0] / 2 *
+                        (1 -
+                         std::tanh(
+                             beta[0] * (1 - 2 * (x - prob_lo[0]) / len[0])) /
+                             std::tanh(beta[0]));
+                amrex::Real y_non_uni =
+                    prob_lo[1] +
+                    len[1] / 2 *
+                        (1 -
+                         std::tanh(
+                             beta[1] * (1 - 2 * (y - prob_lo[1]) / len[1])) /
+                             std::tanh(beta[1]));
+                amrex::Real z_non_uni =
+                    prob_lo[2] +
+                    len[2] / 2 *
+                        (1 -
+                         std::tanh(
+                             beta[2] * (1 - 2 * (z - prob_lo[2]) / len[2])) /
+                             std::tanh(beta[2]));
 
                 scale_fac_nd(i, j, k, 0) =
-                    m_map[0] ? num_x / den_x / dx[0] : 1.0;
+                    (do_map[0] && (i >= 0) && (i <= num_cell[0]))
+                        ? ((x_non_uni - prob_lo[0]) / (i + 0.5 + eps) / dx[0])
+                        : 1.0;
                 scale_fac_nd(i, j, k, 1) =
-                    m_map[1] ? num_y / den_y / dx[1] : 1.0;
+                    (do_map[1] && (j >= 0) && (j <= num_cell[1]))
+                        ? ((y_non_uni - prob_lo[1]) / (j + 0.5 + eps) / dx[1])
+                        : 1.0;
                 scale_fac_nd(i, j, k, 2) =
-                    m_map[2] ? num_z / den_z / dx[2] : 1.0;
+                    (do_map[2] && (k >= 0) && (k <= num_cell[2]))
+                        ? ((z_non_uni - prob_lo[2]) / (k + 0.5 + eps) / dx[2])
+                        : 1.0;
+
+                // adjust for ghost cells to be at the boundaries
+                if (do_map[0] && (i < 0)) scale_fac_nd(i, j, k, 0) = 0.0;
+                if (do_map[1] && (j < 0)) scale_fac_nd(i, j, k, 1) = 0.0;
+                if (do_map[2] && (k < 0)) scale_fac_nd(i, j, k, 2) = 0.0;
+
+                if (do_map[0] && (i > num_cell[0])) {
+                    scale_fac_nd(i, j, k, 0) =
+                        (prob_hi[0] - prob_lo[0]) / (num_cell[0] + eps) / dx[0];
+                }
+                if (do_map[1] && (j > num_cell[1])) {
+                    scale_fac_nd(i, j, k, 1) =
+                        (prob_hi[1] - prob_lo[1]) / (num_cell[1] + eps) / dx[1];
+                }
+                if (do_map[2] && (k > num_cell[2])) {
+                    scale_fac_nd(i, j, k, 2) =
+                        (prob_hi[2] - prob_lo[2]) / (num_cell[2] + eps) / dx[2];
+                }
             });
     }
+
+    // TODO: Call fill patch operators
 }
 
 } // namespace amr_wind
