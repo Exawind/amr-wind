c7d5ba4b4a2e51db5ab8263400d7c5759401d4ce
asharma <ashesh.sharma@nrel.gov>
2021-12-03 15:44:31 -0700

    revert mesh mapping changes to BoussinesqBubble

--- amr-wind/physics/BoussinesqBubble.cpp
+++ amr-wind/physics/BoussinesqBubble.cpp
@@ -10,7 +10,6 @@ namespace amr_wind {
 BoussinesqBubble::BoussinesqBubble(CFDSim& sim)
     : m_velocity(sim.repo().get_field("velocity"))
     , m_density(sim.repo().get_field("density"))
-    , m_mesh_fac(sim.repo().get_field("mesh_scaling_factor_cc"))
 {
     // Register temperature equation
     auto& teqn = sim.pde_manager().register_transport_pde("Temperature");
@@ -32,14 +31,13 @@ void BoussinesqBubble::initialize_fields(int level, const amrex::Geometry& geom)
     auto& velocity = m_velocity(level);
     auto& density = m_density(level);
     auto& scalars = (*m_temperature)(level);
-    auto& mesh_fac = m_mesh_fac(level);
 
     for (amrex::MFIter mfi(density); mfi.isValid(); ++mfi) {
         const auto& vbx = mfi.validbox();
 
         (*m_field_init)(
             vbx, geom, velocity.array(mfi), density.array(mfi),
-            scalars.array(mfi), mesh_fac.array(mfi));
+            scalars.array(mfi));
     }
 }
 
