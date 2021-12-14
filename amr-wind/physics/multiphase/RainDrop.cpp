4bdada43a0d9ad9706d1e4f5d1d1e1f7bf47f612
Michael Kuhn <mkuhn@nrel.gov>
2021-12-14 16:52:14 -0700

    changes to allow an initial droplet velocity in the RainDrop case.

--- amr-wind/physics/multiphase/RainDrop.cpp
+++ amr-wind/physics/multiphase/RainDrop.cpp
@@ -25,6 +25,7 @@ void RainDrop::initialize_fields(int level, const amrex::Geometry& geom)
 {
     auto& velocity = m_velocity(level);
     auto& levelset = m_levelset(level);
+    auto& volfrac = m_vof(level);
     const auto& dx = geom.CellSizeArray();
     const auto& problo = geom.ProbLoArray();
 
