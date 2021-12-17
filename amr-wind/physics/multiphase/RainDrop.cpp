a362e7fccedc54f7d5c5869a28435584aaf3886e
Michael Kuhn <mkuhn@nrel.gov>
2021-12-17 11:42:07 -0700

    modifying setup so that test case will work with levelset or vof methods.

--- amr-wind/physics/multiphase/RainDrop.cpp
+++ amr-wind/physics/multiphase/RainDrop.cpp
@@ -8,7 +8,6 @@ namespace amr_wind {
 RainDrop::RainDrop(CFDSim& sim)
     : m_velocity(sim.repo().get_field("velocity"))
     , m_levelset(sim.repo().get_field("levelset"))
-    , m_vof(sim.repo().get_field("vof"))
     , m_sim(sim)
 {
     amrex::ParmParse pp(identifier());
@@ -26,7 +25,6 @@ void RainDrop::initialize_fields(int level, const amrex::Geometry& geom)
 {
     auto& velocity = m_velocity(level);
     auto& levelset = m_levelset(level);
-    auto& volfrac = m_vof(level);
     const auto& dx = geom.CellSizeArray();
     const auto& problo = geom.ProbLoArray();
 
@@ -51,25 +49,50 @@ void RainDrop::initialize_fields(int level, const amrex::Geometry& geom)
             });
     }
 
-    // Get VOF array
     auto& mphase = m_sim.physics_manager().get<MultiPhase>();
-    mphase.levelset2vof();
+    // Get VOF array if it is valid
+    bool yes_vof = m_sim.repo().field_exists("vof");
+    amrex::MultiFab* work_ptr;
+    if (yes_vof) {
+        auto& volfrac = m_sim.repo().get_field("vof")(level);
+        mphase.levelset2vof();
+        work_ptr = &volfrac;
+    } else {
+        work_ptr = &levelset;
+    }
 
     // Use density to calculate mixed velocity in multiphase cells
     amrex::Real rhol = mphase.rho1();
     amrex::Real rhog = mphase.rho2();
+    const amrex::Real eps = std::cbrt(2. * dx[0] * dx[1] * dx[2]);
     amrex::Gpu::DeviceVector<amrex::Real> dvel(AMREX_SPACEDIM);
     amrex::Gpu::copy(
         amrex::Gpu::hostToDevice, m_vel.begin(), m_vel.end(), dvel.begin());
     const auto* vptr = dvel.data();
     for (amrex::MFIter mfi(velocity); mfi.isValid(); ++mfi) {
         const auto& vbx = mfi.validbox();
-        auto vof_arr = volfrac.array(mfi);
         auto vel = velocity.array(mfi);
+        auto work_arr = work_ptr->array(mfi);
         amrex::ParallelFor(
             vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
                 // Calculate mass-weighted velocity (gas vel is 0)
-                amrex::Real vof = vof_arr(i, j, k);
+                amrex::Real vof;
+                if (yes_vof) {
+                    // Work array here is vof
+                    vof = work_arr(i, j, k);
+                } else {
+                    // Work array here is phi
+                    if (work_arr(i, j, k) > eps) {
+                        vof = 1.0;
+                    } else if (work_arr(i, j, k) < -eps) {
+                        vof = 0.;
+                    } else {
+                        vof = 0.5 *
+                              (1.0 + work_arr(i, j, k) / eps +
+                               1.0 / M_PI *
+                                   std::sin(work_arr(i, j, k) * M_PI / eps));
+                    }
+                }
                 amrex::Real dens = (1.0 - vof) * rhog + vof * rhol;
                 vel(i, j, k, 0) = vof * rhol * vptr[0] / dens;
                 vel(i, j, k, 1) = vof * rhol * vptr[1] / dens;
