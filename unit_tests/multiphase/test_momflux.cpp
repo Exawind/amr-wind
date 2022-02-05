fe4bf6608479521405ca271d550a66c2f781c3e3
Michael Kuhn <mkuhn@nrel.gov>
2022-02-05 01:07:27 -0700

    Updating multiply_fluxes to multiply all components of the fluxes, and improving the momflux test to address all 3 coord directions.

--- unit_tests/multiphase/test_momflux.cpp
+++ unit_tests/multiphase/test_momflux.cpp
@@ -74,188 +74,235 @@ protected:
             pp.add("viscosity_fluid2", (amrex::Real)0.0);
         }
     }
+
+    void initialize_volume_fractions(
+        const int dir,
+        const amrex::Geometry&,
+        const amrex::Box& bx,
+        amrex::Array4<amrex::Real>& vof_arr)
+    {
+
+        // grow the box by 1 so that x,y,z go out of bounds and min(max()) corrects
+        // it and it fills the ghosts with wall values
+        amrex::ParallelFor(grow(bx, 1), [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+            int icheck;
+            switch (dir) {
+            case 0:
+                icheck = i;
+                break;
+            case 1:
+                icheck = j;
+                break;
+            case 2:
+                icheck = k;
+                break;
+            }
+            if (icheck > 0) {
+                vof_arr(i, j, k) = 0.0;
+            } else {
+                vof_arr(i, j, k) = 1.0;
+            }
+        });
+        // Left half is liquid, right half is gas
+    }
+
+    void testing_coorddir(const int dir)
+    {
+        constexpr double tol = 1.0e-16;
+
+        populate_parameters();
+        {
+            amrex::ParmParse pp("geometry");
+            amrex::Vector<int> periodic{{1, 1, 1}};
+            pp.addarr("is_periodic", periodic);
+        }
+
+        initialize_mesh();
+
+        auto& repo = sim().repo();
+
+        // Set up icns PDE, access to VOF PDE
+        auto& pde_mgr = sim().pde_manager();
+        auto& mom_eqn = pde_mgr.register_icns();
+        mom_eqn.initialize();
+
+        // Initialize physics for the sake of MultiPhase routines
+        sim().init_physics();
+
+        // Initialize constant velocity field
+        amrex::Array<amrex::Real, 3> varr = {0};
+        varr[dir] = m_vel;
+        auto& velocity =
+            mom_eqn.fields().field.state(amr_wind::FieldState::Old);
+        init_field3(velocity, varr[0], varr[1], varr[2]);
+        amrex::Real uvel, vvel, wvel;
+        uvel = varr[0]; vvel = varr[1]; wvel = varr[2];
+
+        // Initialize volume fraction field
+        auto& vof = repo.get_field("vof");
+        auto& geom = repo.mesh().Geom();
+        run_algorithm(vof, [&](const int lev, const amrex::MFIter& mfi) {
+            auto vof_arr = vof(lev).array(mfi);
+            const auto& bx = mfi.validbox();
+            initialize_volume_fractions(dir, geom[lev], bx, vof_arr);
+        });
+        // Sync density field with vof
+        auto& mphase = sim().physics_manager().get<amr_wind::MultiPhase>();
+        mphase.set_density_via_vof();
+
+        // Perform pre-advection step to get MAC velocity field (should be
+        // uniform)
+        mom_eqn.pre_advection_actions(amr_wind::FieldState::Old);
+
+        // Perform VOF solve
+        for (auto& seqn : pde_mgr.scalar_eqns()) {
+            if (seqn->fields().field.base_name() == "vof") {
+                seqn->initialize();
+                seqn->compute_advection_term(amr_wind::FieldState::Old);
+                seqn->post_solve_actions();
+            }
+        }
+
+        // Zero unused momentum terms: src (pressure)
+        auto& grad_p = repo.get_field("gp");
+        run_algorithm(grad_p, [&](const int lev, const amrex::MFIter& mfi) {
+            auto gp = grad_p(lev).array(mfi);
+            const auto& bx = mfi.validbox();
+            amrex::ParallelFor(
+                bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                    gp(i, j, k, 0) = 0.0;
+                    gp(i, j, k, 1) = 0.0;
+                    gp(i, j, k, 2) = 0.0;
+                });
+        });
+        // Setup mask_cell array to avoid errors in solve
+        auto& mask_cell = repo.declare_int_field("mask_cell", 1, 1);
+        mask_cell.setVal(1);
+
+        // Perform momentum solve
+        mom_eqn.compute_advection_term(amr_wind::FieldState::Old);
+        mom_eqn.compute_diffusion_term(amr_wind::FieldState::New);
+        mom_eqn.compute_source_term(amr_wind::FieldState::New);
+        mom_eqn.compute_predictor_rhs(DiffusionType::Explicit);
+
+        // Get MAC velocities for use in testing
+        auto& umac = repo.get_field("u_mac");
+        auto& vmac = repo.get_field("v_mac");
+        auto& wmac = repo.get_field("w_mac");
+
+        // Get advected alpha
+        std::string sdir = "d";
+        switch (dir) {
+        case 0:
+            sdir = "x";
+            break;
+        case 1:
+            sdir = "y";
+            break;
+        case 2:
+            sdir = "z";
+            break;
+        }
+        auto& advalpha_f = repo.get_field("advalpha_" + sdir);
+
+        // Get convective term
+        auto& conv_term =
+            mom_eqn.fields().conv_term.state(amr_wind::FieldState::New);
+
+        // Get velocity solution
+        auto& vel_new = mom_eqn.fields().field.state(amr_wind::FieldState::Old);
+
+        // Base level
+        int lev = 0;
+        const auto& dx = geom[lev].CellSizeArray();
+        const auto& problo = geom[lev].ProbLoArray();
+        for (amrex::MFIter mfi(vof(lev)); mfi.isValid(); ++mfi) {
+            const auto& bx = mfi.validbox();
+
+            const auto& um = umac(lev).array(mfi);
+            const auto& vm = vmac(lev).array(mfi);
+            const auto& wm = wmac(lev).array(mfi);
+            const auto& af = advalpha_f(lev).array(mfi);
+            const auto& vel = vel_new(lev).array(mfi);
+            const auto& dqdt = conv_term(lev).array(mfi);
+
+            amrex::ParallelFor(
+                bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                    int icheck;
+                    switch (dir) {
+                    case 0:
+                        icheck = i;
+                        break;
+                    case 1:
+                        icheck = j;
+                        break;
+                    case 2:
+                        icheck = k;
+                        break;
+                    }
+                    // x is face location
+                    const amrex::Real x = problo[dir] + icheck * dx[dir];
+
+                    // UMAC
+                    EXPECT_NEAR(um(i, j, k), uvel, tol);
+                    // U
+                    EXPECT_NEAR(vel(i, j, k, 0), uvel, tol);
+
+                    // VMAC
+                    EXPECT_NEAR(vm(i, j, k), vvel, tol);
+                    // V
+                    EXPECT_NEAR(vel(i, j, k, 1), vvel, tol);
+
+                    // WMAC
+                    EXPECT_NEAR(wm(i, j, k), wvel, tol);
+                    // W
+                    EXPECT_NEAR(vel(i, j, k, 2), wvel, tol);
+
+                    // Test volume fractions at faces
+                    if (x == 0.5) {
+                        // Center face (coming from left cell)
+                        EXPECT_NEAR(af(i, j, k), 1.0, tol);
+                    } else {
+                        if (x == 0.0) {
+                            // Left face (coming from right cell, periodic BC)
+                            EXPECT_NEAR(af(i, j, k), 0.0, tol);
+                        }
+                    }
+
+                    // Test momentum fluxes by checking convective term
+                    if (icheck == 0) {
+                        // Left cell (gas entering, liquid leaving)
+                        EXPECT_NEAR(
+                            dqdt(i, j, k, dir),
+                            m_vel * m_vel * (m_rho2 - m_rho1) / 0.5, tol);
+                    } else {
+                        if (icheck == 1) {
+                            // Right cell (liquid entering, gas leaving)
+                            EXPECT_NEAR(
+                                dqdt(i, j, k, dir),
+                                m_vel * m_vel * (m_rho1 - m_rho2) / 0.5, tol);
+                        }
+                    }
+                });
+        }
+    }
     const amrex::Real m_rho1 = 1000.0;
     const amrex::Real m_rho2 = 1.0;
-    const amrex::Real m_uvel = 5.0;
-    const amrex::Real m_vvel = 0.0;
-    const amrex::Real m_wvel = 0.0;
-    const amrex::Real dt = 0.1 * 0.5 / m_uvel;
+    const amrex::Real m_vel = 5.0;
+    const amrex::Real dt = 0.1 * 0.5 / m_vel;
 };
 
-namespace {
-
-void initialize_volume_fractions(
-    const amrex::Geometry&,
-    const amrex::Box& bx,
-    amrex::Array4<amrex::Real>& vof_arr)
+TEST_F(MassMomFluxOpTest, fluxfaceX)
 {
-
-    // grow the box by 1 so that x,y,z go out of bounds and min(max()) corrects
-    // it and it fills the ghosts with wall values
-    amrex::ParallelFor(grow(bx, 1), [=] AMREX_GPU_DEVICE(int i, int j, int k) {
-        if (i > 0) {
-            vof_arr(i, j, k) = 0.0;
-        } else {
-            vof_arr(i, j, k) = 1.0;
-        }
-    });
-    // Left half is liquid, right half is gas
+    testing_coorddir(0);
 }
-
-} // namespace
-
-TEST_F(MassMomFluxOpTest, fluxface)
+TEST_F(MassMomFluxOpTest, fluxfaceY)
 {
-
-    constexpr double tol = 1.0e-16;
-
-    populate_parameters();
-    {
-        amrex::ParmParse pp("geometry");
-        amrex::Vector<int> periodic{{1, 1, 1}};
-        pp.addarr("is_periodic", periodic);
-    }
-
-    initialize_mesh();
-
-    auto& repo = sim().repo();
-
-    // Set up icns PDE, access to VOF PDE
-    auto& pde_mgr = sim().pde_manager();
-    auto& mom_eqn = pde_mgr.register_icns();
-    mom_eqn.initialize();
-
-    // Initialize physics for the sake of MultiPhase routines
-    sim().init_physics();
-
-    // Initialize constant velocity field
-    auto& velocity = mom_eqn.fields().field.state(amr_wind::FieldState::Old);
-    init_field3(velocity, m_uvel, m_vvel, m_wvel);
-
-    // Initialize volume fraction field
-    auto& vof = repo.get_field("vof");
-    auto& geom = repo.mesh().Geom();
-    run_algorithm(vof, [&](const int lev, const amrex::MFIter& mfi) {
-        auto vof_arr = vof(lev).array(mfi);
-        const auto& bx = mfi.validbox();
-        initialize_volume_fractions(geom[lev], bx, vof_arr);
-    });
-    // Sync density field with vof
-    auto& mphase = sim().physics_manager().get<amr_wind::MultiPhase>();
-    mphase.set_density_via_vof();
-
-    // Perform pre-advection step to get MAC velocity field (should be uniform)
-    mom_eqn.pre_advection_actions(amr_wind::FieldState::Old);
-
-    // Perform VOF solve
-    for (auto& seqn : pde_mgr.scalar_eqns()) {
-        if (seqn->fields().field.base_name() == "vof") {
-            seqn->initialize();
-            seqn->compute_advection_term(amr_wind::FieldState::Old);
-            seqn->post_solve_actions();
-        }
-    }
-
-    // Zero unused momentum terms: src (pressure)
-    auto& grad_p = repo.get_field("gp");
-    run_algorithm(grad_p, [&](const int lev, const amrex::MFIter& mfi) {
-        auto gp = grad_p(lev).array(mfi);
-        const auto& bx = mfi.validbox();
-        amrex::ParallelFor(
-            bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                gp(i, j, k, 0) = 0.0;
-                gp(i, j, k, 1) = 0.0;
-                gp(i, j, k, 2) = 0.0;
-            });
-    });
-    // Setup mask_cell array to avoid errors in solve
-    auto& mask_cell = repo.declare_int_field("mask_cell", 1, 1);
-    mask_cell.setVal(1);
-
-    // Perform momentum solve
-    mom_eqn.compute_advection_term(amr_wind::FieldState::Old);
-    mom_eqn.compute_diffusion_term(amr_wind::FieldState::New);
-    mom_eqn.compute_source_term(amr_wind::FieldState::New);
-    mom_eqn.compute_predictor_rhs(DiffusionType::Explicit);
-
-    // Get MAC velocities for use in testing
-    auto& umac = repo.get_field("u_mac");
-    auto& vmac = repo.get_field("v_mac");
-    auto& wmac = repo.get_field("w_mac");
-
-    // Get advected alpha
-    auto& advalphax = repo.get_field("advalpha_x");
-
-    // Get convective term
-    auto& conv_term =
-        mom_eqn.fields().conv_term.state(amr_wind::FieldState::New);
-
-    // Get velocity solution
-    auto& vel_new = mom_eqn.fields().field.state(amr_wind::FieldState::Old);
-
-    // Base level
-    int lev = 0;
-    const auto& dx = geom[lev].CellSizeArray();
-    const auto& problo = geom[lev].ProbLoArray();
-    for (amrex::MFIter mfi(vof(lev)); mfi.isValid(); ++mfi) {
-        const auto& bx = mfi.validbox();
-
-        const auto& um = umac(lev).array(mfi);
-        const auto& vm = vmac(lev).array(mfi);
-        const auto& wm = wmac(lev).array(mfi);
-        const auto& vel = vel_new(lev).array(mfi);
-        const auto& afx = advalphax(lev).array(mfi);
-        const auto& dqdt =
-            conv_term(lev).array(mfi);
-
-        amrex::ParallelFor(
-            bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                // x is face location
-                const amrex::Real x = problo[0] + i * dx[0];
-
-                // UMAC
-                EXPECT_NEAR(um(i, j, k), m_uvel, tol);
-                // U
-                EXPECT_NEAR(vel(i, j, k, 0), m_uvel, tol);
-
-                // VMAC
-                EXPECT_NEAR(vm(i, j, k), m_vvel, tol);
-                // V
-                EXPECT_NEAR(vel(i, j, k, 1), m_vvel, tol);
-
-                // WMAC
-                EXPECT_NEAR(wm(i, j, k), m_wvel, tol);
-                // W
-                EXPECT_NEAR(vel(i, j, k, 2), m_wvel, tol);
-
-
-                // Test volume fractions at faces
-                if (x == 0.5) {
-                    // Center face (coming from left cell)
-                    EXPECT_NEAR(afx(i, j, k), 1.0, tol);
-                } else {
-                    if (x == 0.0) {
-                        // Left face (coming from right cell, periodic BC)
-                        EXPECT_NEAR(afx(i, j, k), 0.0, tol);
-                    }
-                }
-
-                // Test momentum fluxes by checking convective term
-                if (i == 0) {
-                    // Left cell (gas entering, liquid leaving)
-                    EXPECT_NEAR(
-                        dqdt(i, j, k, 0),
-                        m_uvel * m_uvel * (m_rho2 - m_rho1) / 0.5, tol);
-                } else {
-                    if (i == 1) {
-                        // Right cell (liquid entering, gas leaving)
-                        EXPECT_NEAR(
-                            dqdt(i, j, k, 0),
-                            m_uvel * m_uvel * (m_rho1 - m_rho2) / 0.5, tol);
-                    }
-                }
-            });
-    }
+    testing_coorddir(1);
+}
+TEST_F(MassMomFluxOpTest, fluxfaceZ)
+{
+    testing_coorddir(2);
 }
 
 } // namespace amr_wind_tests
