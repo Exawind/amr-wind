3a2c5c254d58ec0baa0810033a7d077f4089a643
Jon Rood <jon.rood@nrel.gov>
2022-02-15 21:15:40 -0700

    Remove unmatched cppcheck suppressions for cppcheck 2.7.

--- amr-wind/turbulence/LES/OneEqKsgs.cpp
+++ amr-wind/turbulence/LES/OneEqKsgs.cpp
@@ -12,7 +12,6 @@ namespace amr_wind {
 namespace turbulence {
 
 template <typename Transport>
-// cppcheck-suppress uninitMemberVar
 OneEqKsgs<Transport>::OneEqKsgs(CFDSim& sim)
     : TurbModelBase<Transport>(sim)
     , m_vel(sim.repo().get_field("velocity"))
@@ -232,7 +231,6 @@ void OneEqKsgsM84<Transport>::post_advance_work()
     // Update sdr field based on sfs ke
 
     auto& tke = *(this->m_tke);
-    // cppcheck-suppress constVariable
     auto& sdr = *(this->m_sdr);
     const amrex::Real Ce = this->m_Ce;
 
