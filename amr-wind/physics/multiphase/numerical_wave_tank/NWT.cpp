e2c22a3abe2c2f8282f4eda7be0bb6c83b1a940f
gdeskos <gdeskosv@gmail.com>
2022-02-16 14:48:00 -0700

    changes in the Numerical wave tank

--- amr-wind/physics/multiphase/numerical_wave_tank/NWT.cpp
+++ amr-wind/physics/multiphase/numerical_wave_tank/NWT.cpp
@@ -12,6 +12,7 @@ NWT::NWT(CFDSim& sim)
     : m_sim(sim)
     , m_velocity(sim.repo().get_field("velocity"))
     , m_levelset(sim.repo().get_field("levelset"))
+    , m_density(sim.repo().get_field("density"))
     , m_vof(sim.repo().get_field("vof"))
 {
     amrex::ParmParse pp(identifier());
@@ -33,53 +34,48 @@ NWT::~NWT() = default;
  */
 void NWT::initialize_fields(int level, const amrex::Geometry& geom)
 {
-
     auto& velocity = m_velocity(level);
     auto& levelset = m_levelset(level);
+    auto& density = m_density(level);
     velocity.setVal(0.0, 0, AMREX_SPACEDIM);
 
     const auto& dx = geom.CellSizeArray();
     const auto& problo = geom.ProbLoArray();
+
+    const auto& mphase = m_sim.physics_manager().get<MultiPhase>();
+    const amrex::Real rho1 = mphase.rho1();
+    const amrex::Real rho2 = mphase.rho2();
+
     const amrex::Real zsl = m_zsl;
 
     for (amrex::MFIter mfi(levelset); mfi.isValid(); ++mfi) {
         const auto& vbx = mfi.growntilebox();
         auto vel = velocity.array(mfi);
         auto phi = levelset.array(mfi);
+        auto rho = density.array(mfi);
 
         amrex::ParallelFor(
             vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                // const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
                 const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
-                // const amrex::Real kappa = 2.0 * utils::pi() / lambda;
-                // const amrex::Real epsilon = alpha * kappa;
-                // Compute free surface amplitude
-                // const amrex::Real eta = zsl;
-                // +
-                // alpha * ((1.0 - 1.0 / 16.0 * epsilon *
-                // epsilon) *
-                //             std::cos(kappa * x) +
-                //         0.5 * epsilon * std::cos(2.0 * kappa
-                //         * x) + 3.0 / 8.0 * epsilon * epsilon
-                //         *
-                //             std::cos(3.0 * kappa * x));
-
                 phi(i, j, k) = zsl - z;
                 vel(i, j, k, 0) = 0.0;
                 vel(i, j, k, 1) = 0.0;
                 vel(i, j, k, 2) = 0.0;
-                // compute velocities
-                // const amrex::Real g = 9.81;
-                // const amrex::Real Omega =
-                //    std::sqrt(g * kappa * (1.0 + epsilon * epsilon));
-                // if (z < eta) {
-                //    vel(i, j, k, 0) = Omega * alpha * std::exp(kappa * z)
-                //    *
-                //                      std::cos(kappa * x);
-                //    vel(i, j, k, 2) = Omega * alpha * std::exp(kappa * z)
-                //    *
-                //                      std::sin(kappa * x);
-                //}
+
+                amrex::Real smooth_heaviside;
+                amrex::Real eps = std::cbrt(2. * dx[0] * dx[1] * dx[2]);
+                if (phi(i, j, k) > eps) {
+                    smooth_heaviside = 1.0;
+                } else if (phi(i, j, k) < -eps) {
+                    smooth_heaviside = 0.;
+                } else {
+                    smooth_heaviside =
+                        0.5 *
+                        (1.0 + phi(i, j, k) / eps +
+                         1.0 / M_PI * std::sin(phi(i, j, k) * M_PI / eps));
+                }
+                rho(i, j, k) =
+                    rho1 * smooth_heaviside + rho2 * (1.0 - smooth_heaviside);
             });
     }
 }
