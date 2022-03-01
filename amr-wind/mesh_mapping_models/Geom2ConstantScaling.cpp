e22772e10aa9790fa99d052d2f64075277bb0d69
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2022-02-28 19:22:57 -0700

    Fix math errors in Geom2ConstantScaling

--- amr-wind/mesh_mapping_models/Geom2ConstantScaling.cpp
+++ amr-wind/mesh_mapping_models/Geom2ConstantScaling.cpp
@@ -6,7 +6,7 @@
 #include "AMReX_ParmParse.H"
 
 namespace amr_wind {
-namespace abl_map {
+namespace geom2constant_map {
 
 namespace {
 
@@ -32,26 +32,28 @@ AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE amrex::Real x_inv(
     const amrex::Real sratio,
     const amrex::Real delta0)
 {
-    return std::round(std::log(1.0-(trans_loc-delta0/sratio)*sratio*(1.0-sratio)/delta0)/std::log(sratio));
+    return int(std::log(1.0-(trans_loc-delta0/sratio)*sratio*(1.0-sratio)/delta0)/std::log(sratio));
 }
 
 AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE amrex::Real eval_fac(
     const amrex::Real x,
-    const amrex::Real trans_width,
     const amrex::Real trans_loc,
+    const amrex::Real trans_width,
     const amrex::Real sratio,
     const amrex::Real delta0,
-    const amrex::Real len)
+    const amrex::Real dxscale,
+    const int domap)
 {
     // This is the derivative of eval_coord() below
     const amrex::Real xinv = x_inv(trans_loc, sratio, delta0);
-    const amrex::Real h = 0.5*(1.0+std::tanh((x-xinv)/trans_width));
-    const amrex::Real hprime = 0.5*std::pow(std::acosh((x-xinv)/trans_width),2.0)/trans_width;
+    const amrex::Real h = 0.5*(1.0+std::tanh((x*dxscale-xinv)/trans_width));
+    const amrex::Real hprime = 0.5*std::pow(std::cosh((x*dxscale-xinv)/trans_width),-2.0)/trans_width;
     const amrex::Real lmatch = x_geom(xinv, sratio, delta0);
     const amrex::Real slope = lmatch-x_geom(xinv-1.0, sratio, delta0);
     const amrex::Real xprime_geom = delta0/sratio/(sratio-1.0)*std::pow(sratio, x-1.0)*std::log(sratio);
 
-    return (1.0-h)*xprime_geom - hprime*x_geom(x,sratio,delta0) + h*slope + hprime*x_const(x,slope,xinv,lmatch);
+    if (domap==0) return 1.0;
+    return ((1.0-h)*xprime_geom - hprime*x_geom(x,sratio,delta0) + h*slope + hprime*x_const(x,slope,xinv,lmatch))*dxscale;
 }
 
 AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE amrex::Real eval_coord(
@@ -60,14 +62,16 @@ AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE amrex::Real eval_coord(
     const amrex::Real trans_width,
     const amrex::Real sratio,
     const amrex::Real delta0,
-    const amrex::Real len)
+    const amrex::Real dxscale,
+    const int domap)
 {
     const amrex::Real xinv = x_inv(trans_loc, sratio, delta0);
-    const amrex::Real h = 0.5*(1.0+std::tanh((x-xinv)/trans_width));
+    const amrex::Real h = 0.5*(1.0+std::tanh((x*dxscale-xinv)/trans_width));
     const amrex::Real lmatch = x_geom(xinv, sratio, delta0);
     const amrex::Real slope = lmatch-x_geom(xinv-1.0, sratio, delta0);
 
-    return x_geom(x,sratio,delta0)*(1.0-h) + h*x_const(x,slope,xinv,lmatch);
+    if (domap==0) return x;
+    return x_geom(x*dxscale,sratio,delta0)*(1.0-h) + h*x_const(x*dxscale,slope,xinv,lmatch);
 }
 
 } // namespace
@@ -108,6 +112,13 @@ void Geom2ConstantScaling::create_cell_node_map(
     const auto eps = m_eps;
 
     const auto& dx = geom.CellSizeArray();
+    amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> dxscale{{1.0, 1.0, 1.0}};
+    for (int i=0; i<AMREX_SPACEDIM; i++) {
+      if (do_map[i]) {
+	dxscale[i] = 1.0/dx[i];
+      }
+    }
+
     const auto& prob_lo = geom.ProbLoArray();
     const auto& prob_hi = geom.ProbHiArray();
 
@@ -115,10 +126,10 @@ void Geom2ConstantScaling::create_cell_node_map(
         {prob_hi[0] - prob_lo[0], prob_hi[1] - prob_lo[1],
          prob_hi[2] - prob_lo[2]}};
 
-    amrex::Print() << "Transition Width:    " << transwid[0] << transwid[1] << transwid[2]
+    amrex::Print() << "Transition Width:    " << transwid[0] <<" "<< transwid[1] <<" "<< transwid[2]
                        << std::endl;
 
-    amrex::Print() << "Transition Location:    " << transloc[0] << transloc[1] << transloc[2]
+    amrex::Print() << "Transition Location:    " << transloc[0] <<" "<< transloc[1] <<" "<< transloc[2]
                        << std::endl;
 
     for (amrex::MFIter mfi((*m_mesh_scale_fac_cc)(lev)); mfi.isValid(); ++mfi) {
@@ -130,16 +141,16 @@ void Geom2ConstantScaling::create_cell_node_map(
             (*m_mesh_scale_detJ_cc)(lev).array(mfi);
         amrex::ParallelFor(
             bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
-                amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
-                amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
+	      amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
+	      amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
+	      amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
 
                 amrex::Real fac_x = 
-                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+    		    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real fac_y = 
-                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real fac_z = 
-                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x > prob_lo[0]) && (x < prob_hi[0]) && (y > prob_lo[1]) &&
@@ -161,16 +172,16 @@ void Geom2ConstantScaling::create_cell_node_map(
             (*m_mesh_scale_detJ_nd)(lev).array(mfi);
         amrex::ParallelFor(
             nbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + i * dx[0];
-                amrex::Real y = prob_lo[1] + j * dx[1];
-                amrex::Real z = prob_lo[2] + k * dx[2];
+	      amrex::Real x = prob_lo[0] + i * dx[0];
+	      amrex::Real y = prob_lo[1] + j * dx[1];
+	      amrex::Real z = prob_lo[2] + k * dx[2];
 
                 amrex::Real fac_x = 
-                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real fac_y = 
-                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real fac_z = 
-                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x >= prob_lo[0] - eps) && (x <= prob_hi[0] + eps) &&
@@ -206,6 +217,13 @@ void Geom2ConstantScaling::create_face_map(int lev, const amrex::Geometry& geom)
     const auto eps = m_eps;
 
     const auto& dx = geom.CellSizeArray();
+    amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> dxscale{{1.0, 1.0, 1.0}};
+    for (int i=0; i<AMREX_SPACEDIM; i++) {
+      if (do_map[i]) {
+	dxscale[i] = 1.0/dx[i];
+      }
+    }
+
     const auto& prob_lo = geom.ProbLoArray();
     const auto& prob_hi = geom.ProbHiArray();
 
@@ -222,16 +240,16 @@ void Geom2ConstantScaling::create_face_map(int lev, const amrex::Geometry& geom)
             (*m_mesh_scale_detJ_xf)(lev).array(mfi);
         amrex::ParallelFor(
             bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + i * dx[0];
-                amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
-                amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
+	      amrex::Real x = prob_lo[0] + i * dx[0];
+	      amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
+	      amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
 
                 amrex::Real fac_x = 
-                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real fac_y = 
-                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real fac_z = 
-                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x >= prob_lo[0] - eps) && (x <= prob_hi[0] + eps) &&
@@ -257,16 +275,16 @@ void Geom2ConstantScaling::create_face_map(int lev, const amrex::Geometry& geom)
             (*m_mesh_scale_detJ_yf)(lev).array(mfi);
         amrex::ParallelFor(
             bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
-                amrex::Real y = prob_lo[1] + j * dx[1];
-                amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
+	      amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
+	      amrex::Real y = prob_lo[1] + j * dx[1];
+	      amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
 
                 amrex::Real fac_x = 
-                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real fac_y = 
-                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real fac_z = 
-                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x > prob_lo[0]) && (x < prob_hi[0]) &&
@@ -292,16 +310,16 @@ void Geom2ConstantScaling::create_face_map(int lev, const amrex::Geometry& geom)
             (*m_mesh_scale_detJ_zf)(lev).array(mfi);
         amrex::ParallelFor(
             bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
-                amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
-                amrex::Real z = prob_lo[2] + k * dx[2];
+	        amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
+	        amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
+	        amrex::Real z = prob_lo[2] + k * dx[2];
 
                 amrex::Real fac_x = 
-                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+                    eval_fac(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real fac_y = 
-                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_fac(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real fac_z = 
-                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_fac(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x > prob_lo[0]) && (x < prob_hi[0]) && (y > prob_lo[1]) &&
@@ -350,6 +368,13 @@ void Geom2ConstantScaling::create_non_uniform_mesh(
     const auto eps = m_eps;
 
     const auto& dx = geom.CellSizeArray();
+    amrex::GpuArray<amrex::Real, AMREX_SPACEDIM> dxscale{{1.0, 1.0, 1.0}};
+    for (int i=0; i<AMREX_SPACEDIM; i++) {
+      if (do_map[i]) {
+	dxscale[i] = 1.0/dx[i];
+      }
+    }
+
     const auto& prob_lo = geom.ProbLoArray();
     const auto& prob_hi = geom.ProbHiArray();
 
@@ -365,16 +390,16 @@ void Geom2ConstantScaling::create_non_uniform_mesh(
             (*m_non_uniform_coord_cc)(lev).array(mfi);
         amrex::ParallelFor(
             bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
-                amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
-                amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
+	        amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
+	        amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
+	        amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
 
                 amrex::Real x_non_uni =
-                    eval_coord(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+                    eval_coord(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real y_non_uni =
-                    eval_coord(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_coord(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real z_non_uni =
-                    eval_coord(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_coord(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x > prob_lo[0]) && (x < prob_hi[0]) && (y > prob_lo[1]) &&
@@ -390,16 +415,16 @@ void Geom2ConstantScaling::create_non_uniform_mesh(
             (*m_non_uniform_coord_nd)(lev).array(mfi);
         amrex::ParallelFor(
             nbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                amrex::Real x = prob_lo[0] + i * dx[0];
-                amrex::Real y = prob_lo[1] + j * dx[1];
-                amrex::Real z = prob_lo[2] + k * dx[2];
+  	        amrex::Real x = prob_lo[0] + i * dx[0];
+		amrex::Real y = prob_lo[1] + j * dx[1];
+		amrex::Real z = prob_lo[2] + k * dx[2];
 
                 amrex::Real x_non_uni =
-                    eval_coord(x, transloc[0], transwid[0], sratio[0], delta0[0], len[0]);
+                    eval_coord(x, transloc[0], transwid[0], sratio[0], delta0[0], dxscale[0], do_map[0]);
                 amrex::Real y_non_uni =
-                    eval_coord(y, transloc[1], transwid[1], sratio[1], delta0[1], len[1]);
+                    eval_coord(y, transloc[1], transwid[1], sratio[1], delta0[1], dxscale[1], do_map[1]);
                 amrex::Real z_non_uni =
-                    eval_coord(z, transloc[2], transwid[2], sratio[2], delta0[2], len[2]);
+                    eval_coord(z, transloc[2], transwid[2], sratio[2], delta0[2], dxscale[2], do_map[2]);
 
                 bool in_domain =
                     ((x >= prob_lo[0] - eps) && (x <= prob_hi[0] + eps) &&
@@ -413,5 +438,31 @@ void Geom2ConstantScaling::create_non_uniform_mesh(
     }
 }
 
-} // namespace abl_map
+  // Used for debugging eval_coord function
+  amrex::Real Geom2ConstantScaling::dump_coord(
+					       const amrex::Real x,
+					       const amrex::Real trans_loc,
+					       const amrex::Real trans_width,
+					       const amrex::Real sratio,
+					       const amrex::Real delta0,
+					       const amrex::Real len,
+					       const int domap)
+  {
+    return eval_coord(x, trans_loc, trans_width, sratio, delta0, len, domap);
+  }
+
+  // Used for debugging eval_fac function
+  amrex::Real Geom2ConstantScaling::dump_fac(
+					     const amrex::Real x,
+					     const amrex::Real trans_loc,
+					     const amrex::Real trans_width,
+					     const amrex::Real sratio,
+					     const amrex::Real delta0,
+					     const amrex::Real len,
+					     const int domap)
+  {
+    return eval_fac(x, trans_loc, trans_width, sratio, delta0, len, domap);
+  }
+
+} // namespace geom2constant_map
 } // namespace amr_wind
