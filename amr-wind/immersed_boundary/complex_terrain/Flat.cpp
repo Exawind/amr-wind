7ef2388a598f57b58889dd921d7645964e4abbaa
neilmatula <72527988+neilmatula@users.noreply.github.com>
2022-03-01 14:56:26 -0700

    Update IB hill to use nearest distance to boundary, and add flat plate (#12)

---
+++ amr-wind/immersed_boundary/complex_terrain/Flat.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/immersed_boundary/complex_terrain/Flat.H"
+#include "amr-wind/immersed_boundary/complex_terrain/flat_ops.H"
+#include "amr-wind/immersed_boundary/IBModel.H"
+
+namespace amr_wind {
+namespace ib {
+
+template class IBModel<Flat>;
+
+} // namespace ib
+} // namespace amr_wind
