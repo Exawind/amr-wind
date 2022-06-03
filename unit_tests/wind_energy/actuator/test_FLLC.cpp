6d66bd265de67a6dd17d885ada90aa6145a37391
Philip Sakievich <psakiev@sandia.gov>
2022-06-03 09:56:07 -0600

    Remove 'base' from the FLLCOp name

--- unit_tests/wind_energy/actuator/test_FLLC.cpp
+++ unit_tests/wind_energy/actuator/test_FLLC.cpp
@@ -1,6 +1,6 @@
 #include "aw_test_utils/AmrexTest.H"
 #include "amr-wind/wind_energy/actuator/FLLC.H"
-#include "amr-wind/wind_energy/actuator/FLLCOpBase.H"
+#include "amr-wind/wind_energy/actuator/FLLCOp.H"
 
 namespace amr_wind {
 namespace actuator {
