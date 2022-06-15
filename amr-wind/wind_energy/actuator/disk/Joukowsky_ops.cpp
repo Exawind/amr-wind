465355cb4f149e935f0833ef7498b706ebf80231
Philip Sakievich <psakiev@sandia.gov>
2022-06-15 14:27:09 -0600

    Output TSR

--- amr-wind/wind_energy/actuator/disk/Joukowsky_ops.cpp
+++ amr-wind/wind_energy/actuator/disk/Joukowsky_ops.cpp
@@ -84,6 +84,7 @@ void prepare_netcdf_file(
     grp.def_var("xyz_v", NC_DOUBLE, {nv_name, "ndim"});
     grp.def_var("vref", NC_DOUBLE, {nt_name, "ndim"});
     grp.def_var("vdisk", NC_DOUBLE, {nt_name, "ndim"});
+    grp.def_var("tsr", NC_DOUBLE, {nt_name, "ndim"});
     grp.def_var("ct", NC_DOUBLE, {nt_name});
     grp.def_var("cp", NC_DOUBLE, {nt_name});
     grp.def_var("density", NC_DOUBLE, {nt_name});
@@ -130,6 +131,7 @@ void write_netcdf(
         &(data.reference_velocity[0]), {nt, 0}, {1, AMREX_SPACEDIM});
     grp.var("vdisk").put(
         &(data.mean_disk_velocity[0]), {nt, 0}, {1, AMREX_SPACEDIM});
+    grp.var("tsr").put(&data.current_tip_speed_ratio, {nt}, {1});
     grp.var("ct").put(&data.current_ct, {nt}, {1});
     grp.var("cp").put(&data.current_cp, {nt}, {1});
     grp.var("density").put(&data.density, {nt}, {1});
