43c06a3f36cb0f5bbf719bc38a9ff618fec61d85
neilmatula <nmatula@sandia.gov>
2022-06-29 14:28:33 -0600

    For the Joukowsky ADM, change input from rad/s to RPM, and add a default for the vortex core size.

--- amr-wind/wind_energy/actuator/disk/Joukowsky_ops.cpp
+++ amr-wind/wind_energy/actuator/disk/Joukowsky_ops.cpp
@@ -11,8 +11,7 @@ void check_for_parse_conflicts(const utils::ActParser& pp)
 {
     auto error_collector = ops::base::check_for_parse_conflicts(pp);
     // clang-format off
-    base::collect_parse_dependencies(pp, "thrust_coeff", "angular_velocity", error_collector);
-    base::collect_parse_dependencies(pp, "use_root_correction", "vortex_core_size", error_collector);
+    base::collect_parse_dependencies(pp, "thrust_coeff", "rpm", error_collector);
     // clang-format on
     ops::base::check_error_stream(error_collector);
 }
@@ -22,7 +21,12 @@ void optional_parameters(JoukowskyData& meta, const utils::ActParser& pp)
     ops::base::optional_parameters(meta, pp);
     pp.query("use_root_correction", meta.use_root_correction);
     pp.query("use_tip_correction", meta.use_tip_correction);
-    pp.query("vortex_core_size", meta.vortex_core_size);
+    if (pp.contains("vortex_core_size")) {
+        pp.get("vortex_core_size", meta.vortex_core_size);
+    } else {
+        // Default to 20% of radius
+        meta.vortex_core_size = 0.1 * meta.diameter;
+    }
     pp.query("root_correction_exponent", meta.root_correction_exponent);
     pp.query("root_correction_coefficient", meta.root_correction_coefficient);
     pp.query("num_blades", meta.num_blades);
@@ -33,7 +37,11 @@ void required_parameters(JoukowskyData& meta, const utils::ActParser& pp)
     ops::base::required_parameters(meta, pp);
     pp.get("num_points_t", meta.num_vel_pts_t);
     pp.get("num_points_r", meta.num_vel_pts_r);
-    pp.getarr("angular_velocity", meta.angular_velocity);
+    pp.getarr("rpm", meta.angular_velocity);
+    // Convert from rpm to rad/s
+    for (int i = 0; i < meta.angular_velocity.size(); ++i) {
+        meta.angular_velocity[i] = meta.angular_velocity[i] * M_PI / 30.0;
+    }
 }
 
 void parse_and_gather_params(const utils::ActParser& pp, JoukowskyData& data)
