53ad8f3fe3339a9fc6bcfc7744d0c26ae74952b2
Michael LoCascio <mlocasci@el3.ib0.cm.hpc.nrel.gov>
2022-08-09 08:58:56 -0600

    fixed scope of variable

--- amr-wind/equation_systems/icns/source_terms/ABLForcing.cpp
+++ amr-wind/equation_systems/icns/source_terms/ABLForcing.cpp
@@ -30,10 +30,9 @@ ABLForcing::ABLForcing(const CFDSim& sim) : m_time(sim.time())
         amrex::Real data_time;
         amrex::Real data_speed;
         amrex::Real data_deg;
-        amrex::Real data_rad;
         while (ifh >> data_time) {
             ifh >> data_speed >> data_deg;
-            data_rad = utils::radians(data_deg);
+            amrex::Real data_rad = utils::radians(data_deg);
             m_time_table.push_back(data_time);
             m_speed_table.push_back(data_speed);
             m_direction_table.push_back(data_rad);
