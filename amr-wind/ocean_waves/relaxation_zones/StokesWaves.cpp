39d87c2f70a7e6358a4a7ec7a30e1d2243f0bcb4
Georgios (Yorgos) Deskos <gdeskosv@gmail.com>
2022-09-09 11:39:38 -0600

    adding forgotten relaxation zones

---
+++ amr-wind/ocean_waves/relaxation_zones/StokesWaves.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/ocean_waves/relaxation_zones/StokesWaves.H"
+#include "amr-wind/ocean_waves/relaxation_zones/stokes_waves_ops.H"
+#include "amr-wind/ocean_waves/OceanWavesModel.H"
+
+namespace amr_wind {
+namespace ocean_waves {
+
+template class OWModel<StokesWaves>;
+
+} // namespace ocean_waves
+} // namespace amr_wind
