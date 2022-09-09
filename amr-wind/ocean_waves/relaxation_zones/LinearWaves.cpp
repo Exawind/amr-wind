6813ad5d907d3669c2c3453da176900d77757342
Georgios (Yorgos) Deskos <gdeskosv@gmail.com>
2022-09-09 11:39:38 -0600

    adding forgotten relaxation zones

---
+++ amr-wind/ocean_waves/relaxation_zones/LinearWaves.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/ocean_waves/relaxation_zones/LinearWaves.H"
+#include "amr-wind/ocean_waves/relaxation_zones/linear_waves_ops.H"
+#include "amr-wind/ocean_waves/OceanWavesModel.H"
+
+namespace amr_wind {
+namespace ocean_waves {
+
+template class OWModel<LinearWaves>;
+
+} // namespace ocean_waves
+} // namespace amr_wind
