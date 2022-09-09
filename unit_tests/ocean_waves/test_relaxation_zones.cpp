e45e18e904da3c3bb0dd46e3576b70ecbca6cd58
Georgios (Yorgos) Deskos <gdeskosv@gmail.com>
2022-09-09 10:52:46 -0600

    adding generation/absorption unit tests

--- unit_tests/ocean_waves/test_relaxation_zones.cpp
+++ unit_tests/ocean_waves/test_relaxation_zones.cpp
@@ -8,7 +8,27 @@ namespace amr_wind_tests {
 TEST_F(OceanWavesMeshTest, WaveGeneration)
 {
     constexpr amrex::Real tol = 1.0e-12;
+    populate_parameters();
     utils::populate_ocean_waves_params();
+
+    initialize_mesh();
+    auto& frepo = mesh().field_repo();
+    auto& velocityf = frepo.declare_field("velocity", 3, 0);
+    auto& densityf = frepo.declare_field("density");
+    auto& voff = frepo.declare_field("vof");
+}
+
+TEST_F(OceanWavesMeshTest, WaveAbsorption)
+{
+    constexpr amrex::Real tol = 1.0e-12;
+    populate_parameters();
+    utils::populate_ocean_waves_params();
+
+    initialize_mesh();
+    auto& frepo = mesh().field_repo();
+    auto& velocityf = frepo.declare_field("velocity", 3, 0);
+    auto& densityf = frepo.declare_field("density");
+    auto& voff = frepo.declare_field("vof");
 }
 
 } // namespace amr_wind_tests
