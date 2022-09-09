1d7236abfda25611d6519b0c531ce53cb1aaf5b5
Georgios (Yorgos) Deskos <gdeskosv@gmail.com>
2022-09-09 10:33:24 -0600

    adding unit tests

---
+++ unit_tests/ocean_waves/test_relaxation_zones.cpp
@@ -0,0 +1,15 @@
+#include "abl_test_utils.H"
+#include "amr-wind/utilities/trig_ops.H"
+#include "aw_test_utils/iter_tools.H"
+#include "aw_test_utils/test_utils.H"
+
+
+namespace amr_wind_tests {
+
+TEST_F(OceanWaveMeshTest, WaveGeneration)
+{
+    constexpr amrex::Real tol = 1.0e-12;
+
+}
+
+} // namespace amr_wind_tests
