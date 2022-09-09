0cdf358115e28cb2d29af518e7799d63ea92bd65
Georgios (Yorgos) Deskos <gdeskosv@gmail.com>
2022-09-09 10:49:05 -0600

    adding CMakeLists + ocean_wave_test_utils classes

--- unit_tests/ocean_waves/test_relaxation_zones.cpp
+++ unit_tests/ocean_waves/test_relaxation_zones.cpp
@@ -1,15 +1,14 @@
-#include "abl_test_utils.H"
+#include "ocean_waves_test_utils.H"
 #include "amr-wind/utilities/trig_ops.H"
 #include "aw_test_utils/iter_tools.H"
 #include "aw_test_utils/test_utils.H"
 
-
 namespace amr_wind_tests {
 
-TEST_F(OceanWaveMeshTest, WaveGeneration)
+TEST_F(OceanWavesMeshTest, WaveGeneration)
 {
     constexpr amrex::Real tol = 1.0e-12;
-
+    utils::populate_ocean_waves_params();
 }
 
 } // namespace amr_wind_tests
