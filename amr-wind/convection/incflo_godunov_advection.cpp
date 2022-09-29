4e904d9275eb5d89a951742ab57108b12eda3d89
Michael Kuhn <mkuhn@nrel.gov>
2022-09-28 21:05:31 -0600

    two flux schemes to be employed by the multiphase solver - upwind and minmod - unit tests included

--- amr-wind/convection/incflo_godunov_advection.cpp
+++ amr-wind/convection/incflo_godunov_advection.cpp
@@ -1,6 +1,8 @@
 #include "amr-wind/convection/incflo_godunov_ppm.H"
 #include "amr-wind/convection/incflo_godunov_ppm_nolim.H"
 #include "amr-wind/convection/incflo_godunov_weno.H"
+#include "amr-wind/convection/incflo_godunov_minmod.H"
+#include "amr-wind/convection/incflo_godunov_upwind.H"
 #include "amr-wind/convection/Godunov.H"
 #include <AMReX_Geometry.H>
 
@@ -125,8 +127,39 @@ void godunov::compute_fluxes(
             });
         break;
     }
+    case godunov::scheme::MINMOD: {
+        amrex::ParallelFor(
+            bxg1, ncomp,
+            [=] AMREX_GPU_DEVICE(int i, int j, int k, int n) noexcept {
+                Godunov_minmod_fpu_x(
+                    i, j, k, n, l_dt, dx, Imx(i, j, k, n), Ipx(i, j, k, n), q,
+                    umac, pbc[n], dlo.x, dhi.x);
+                Godunov_minmod_fpu_y(
+                    i, j, k, n, l_dt, dy, Imy(i, j, k, n), Ipy(i, j, k, n), q,
+                    vmac, pbc[n], dlo.y, dhi.y);
+                Godunov_minmod_fpu_z(
+                    i, j, k, n, l_dt, dz, Imz(i, j, k, n), Ipz(i, j, k, n), q,
+                    wmac, pbc[n], dlo.z, dhi.z);
+            });
+        break;
+    }
+    case godunov::scheme::UPWIND: {
+        amrex::ParallelFor(
+            bxg1, ncomp,
+            [=] AMREX_GPU_DEVICE(int i, int j, int k, int n) noexcept {
+                Godunov_upwind_fpu(
+                    i, j, k, n, Imx(i, j, k, n), Ipx(i, j, k, n), q);
+                Godunov_upwind_fpu(
+                    i, j, k, n, Imy(i, j, k, n), Ipy(i, j, k, n), q);
+                Godunov_upwind_fpu(
+                    i, j, k, n, Imz(i, j, k, n), Ipz(i, j, k, n), q);
+            });
+        break;
+    }
     default: {
-        amrex::Abort("Only PPM_NOLIM, WENOZ, and WENOJS use this code path");
+        amrex::Abort(
+            "Only PPM_NOLIM, WENOZ, and WENOJS use this code path, or in "
+            "multiphase simulations, MINMOD and UPWIND also use it");
     }
     }
 
