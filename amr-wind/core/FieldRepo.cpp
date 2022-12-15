56f20bd26b9f20e06414a0048f09aceacfa0f634
Jon Rood <jon.rood@nrel.gov>
2022-12-15 15:18:55 -0700

    Fixes for updated github action virtual environments (#752)

--- amr-wind/core/FieldRepo.cpp
+++ amr-wind/core/FieldRepo.cpp
@@ -153,11 +153,13 @@ FieldRepo::get_field(const std::string& name, const FieldState fstate) const
     const auto found = m_fid_map.find(fname);
     if (found == m_fid_map.end()) {
         amrex::Abort("Cannot find field: " + name);
+        exit(1); // To appease the compiler
+    } else if (found->second < static_cast<unsigned>(m_field_vec.size())) {
+        return *m_field_vec[found->second];
+    } else {
+        amrex::Abort("Cannot find field: " + name);
+        exit(1); // To appease the compiler
     }
-
-    AMREX_ASSERT(found->second < static_cast<unsigned>(m_field_vec.size()));
-    // cppcheck-suppress derefInvalidIteratorRedundantCheck
-    return *m_field_vec[found->second];
 }
 
 Field& FieldRepo::get_mesh_mapping_field(FieldLoc floc) const
@@ -279,11 +281,13 @@ FieldRepo::get_int_field(const std::string& name, const FieldState fstate) const
     const auto found = m_int_fid_map.find(fname);
     if (found == m_int_fid_map.end()) {
         amrex::Abort("Cannot find field: " + name);
+        exit(1); // To appease the compiler
+    } else if (found->second < static_cast<unsigned>(m_int_field_vec.size())) {
+        return *m_int_field_vec[found->second];
+    } else {
+        amrex::Abort("Cannot find field: " + name);
+        exit(1); // To appease the compiler
     }
-
-    AMREX_ASSERT(found->second < static_cast<unsigned>(m_int_field_vec.size()));
-    // cppcheck-suppress derefInvalidIteratorRedundantCheck
-    return *m_int_field_vec[found->second];
 }
 
 bool FieldRepo::int_field_exists(
