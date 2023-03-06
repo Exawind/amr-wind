90b5c53e9d65a5f24f3eca55fc4dc9409c455239
Michael Kuhn <mkuhn@nrel.gov>
2023-03-06 14:59:28 -0700

    fix reference_pressure name in init

--- amr-wind/setup/init.cpp
+++ amr-wind/setup/init.cpp
@@ -127,9 +127,9 @@ void incflo::InitialIterations()
     }
 
     // Add mean pressure back if available
-    if (m_repo.field_exists("p0")) {
+    if (m_repo.field_exists("reference_pressure")) {
         auto& pressure = m_repo.get_field("p");
-        auto& p0 = m_repo.get_field("p0");
+        auto& p0 = m_repo.get_field("reference_pressure");
         for (int lev = 0; lev <= finest_level; lev++) {
             amrex::MultiFab::Add(
                 pressure(lev), p0(lev), 0, 0, 1, pressure.num_grow()[0]);
