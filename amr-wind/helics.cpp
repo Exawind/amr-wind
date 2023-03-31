c85b1e1fefe0ab2331d998de033793a2fcb1d4ef
Michael J. Brazell <Michael.Brazell@nrel.gov>
2023-03-31 09:41:55 -0700

    Update helics.cpp

--- amr-wind/helics.cpp
+++ amr-wind/helics.cpp
@@ -151,7 +151,6 @@ void helics_storage::recv_messages_from_controller()
             // skip the time stamp and source strings from return_list
             return_list.pop_front();
             return_list.pop_front();
-            
             for (int i = m_turbine_yaw_to_amrwind.size() - 1; i >= 0; --i) {
                 m_turbine_yaw_to_amrwind[i] = return_list.front();
                 return_list.pop_front();
