2c18db64a0f0a7e18b51b733b84872232fea7f98
Michael Kuhn <mkuhn@nrel.gov>
2023-04-19 13:30:40 -0600

    level not needed, add power of 3

--- amr-wind/wind_energy/ABLFieldInit.cpp
+++ amr-wind/wind_energy/ABLFieldInit.cpp
@@ -204,7 +204,7 @@ void ABLFieldInit::perturb_temperature(
 
 //! Initialize sfs tke field at the beginning of the simulation
 void ABLFieldInit::init_tke(
-    const int lev, const amrex::Geometry& geom, amrex::MultiFab& tke_fab) const
+    const amrex::Geometry& geom, amrex::MultiFab& tke_fab) const
 {
     tke_fab.setVal(m_tke_init, 1);
 
@@ -223,12 +223,13 @@ void ABLFieldInit::init_tke(
         const auto& bx = mfi.growntilebox(1);
         const auto& tke = tke_fab.array(mfi);
 
+        // Profile definition from Beare et al. (2006)
         amrex::ParallelFor(
             bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
                 const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
                 if (z < tke_cutoff_height) {
-                    tke(i, j, k) =
-                        tke_init_factor * (1. - z / tke_cutoff_height);
+                    tke(i, j, k) = tke_init_factor *
+                                   std::pow(1. - z / tke_cutoff_height, 3.0);
                 } else {
                     tke(i, j, k) = 0.;
                 }
