a0a6357334ed6d1a6711aa527b0a4c118cf12996
gdeskos <gdeskosv@gmail.com>
2023-08-09 11:29:17 -0600

    changing Coriolis and Geostrophic forces to admit a two-component formulation

--- amr-wind/equation_systems/icns/source_terms/GeostrophicForcing.cpp
+++ amr-wind/equation_systems/icns/source_terms/GeostrophicForcing.cpp
@@ -22,28 +22,28 @@ namespace amr_wind::pde::icns {
  */
 GeostrophicForcing::GeostrophicForcing(const CFDSim& /*unused*/)
 {
-    amrex::Real coriolis_factor;
-    {
-        // Read the rotational time period (in seconds)
-        amrex::ParmParse pp("CoriolisForcing");
-        amrex::Real rot_time_period = 86400.0;
-        pp.query("rotational_time_period", rot_time_period);
-        coriolis_factor = 2.0 * utils::two_pi() / rot_time_period;
-        amrex::Print() << "Geostrophic forcing: Coriolis factor = "
-                       << coriolis_factor << std::endl;
+    amrex::Real coriolis_factor = 0.0;
 
-        amrex::Real latitude = 90.0;
-        pp.query("latitude", latitude);
-        AMREX_ALWAYS_ASSERT(
-            std::abs(latitude - 90.0) <
-            static_cast<amrex::Real>(vs::DTraits<float>::eps()));
-    }
+    // Read the rotational time period (in seconds)
+    amrex::ParmParse ppc("CoriolisForcing");
+    // Read the rotational time period (in seconds) -- This is 23hrs and 56
+    // minutes and 4.091 seconds
+    amrex::Real rot_time_period = 86164.091;
+    ppc.query("rotational_time_period", rot_time_period);
 
-    {
-        // Read the geostrophic wind speed vector (in m/s)
-        amrex::ParmParse pp("GeostrophicForcing");
-        pp.getarr("geostrophic_wind", m_target_vel);
-    }
+    amrex::Real omega = 2.0 * utils::two_pi() / rot_time_period;
+    amrex::Real latitude = 90;
+    ppc.get("latitude", latitude);
+    latitude = utils::radians(latitude);
+    amrex::Real sinphi = std::sin(latitude);
+
+    coriolis_factor = 2.0 * omega * sinphi;
+    amrex::Print() << "Geostrophic forcing: Coriolis factor = "
+                   << coriolis_factor << std::endl;
+
+    // Read the geostrophic wind speed vector (in m/s)
+    amrex::ParmParse ppg("GeostrophicForcing");
+    ppg.getarr("geostrophic_wind", m_target_vel);
 
     m_g_forcing = {
         -coriolis_factor * m_target_vel[1], coriolis_factor * m_target_vel[0],
--- unit_tests/wind_energy/test_abl_src.cpp
+++ unit_tests/wind_energy/test_abl_src.cpp
@@ -178,7 +178,7 @@ TEST_F(ABLMeshTest, geostrophic_forcing)
     populate_parameters();
 
     amrex::ParmParse pp("CoriolisForcing");
-    pp.add("latitude", 90.0);
+    pp.add("latitude", 54.0);
 
     initialize_mesh();
 
@@ -199,7 +199,8 @@ TEST_F(ABLMeshTest, geostrophic_forcing)
         geostrophic_forcing(lev, mfi, bx, amr_wind::FieldState::New, src_arr);
     });
 
-    constexpr amrex::Real corfac = 2.0 * amr_wind::utils::two_pi() / 86400.0;
+    constexpr amrex::Real corfac =
+        4.0 * amr_wind::utils::two_pi() / 86164.091 * 0.80901699437;
     const amrex::Array<amrex::Real, AMREX_SPACEDIM> golds{
         {-corfac * 6.0, corfac * 10.0, 0.0}};
     for (int i = 0; i < AMREX_SPACEDIM; ++i) {
@@ -347,7 +348,7 @@ TEST_F(ABLMeshTest, hurricane_forcing)
 TEST_F(ABLMeshTest, coriolis_const_vel)
 {
     constexpr amrex::Real tol = 1.0e-12;
-    constexpr amrex::Real corfac = 2.0 * amr_wind::utils::two_pi() / 86400.0;
+    constexpr amrex::Real corfac = 4.0 * amr_wind::utils::two_pi() / 86164.091;
     // Latitude is set to 45 degrees in the input file so sinphi = cosphi
     const amrex::Real latfac = std::sin(amr_wind::utils::radians(45.0));
     // Initialize a random value for the velocity component
@@ -429,7 +430,7 @@ TEST_F(ABLMeshTest, coriolis_height_variation)
     // ABL unit test mesh has 64 cells in z
     constexpr int kdim = 63;
     constexpr amrex::Real tol = 1.0e-12;
-    constexpr amrex::Real corfac = 2.0 * amr_wind::utils::two_pi() / 86400.0;
+    constexpr amrex::Real corfac = 4.0 * amr_wind::utils::two_pi() / 86164.091;
     // Latitude is set to 45 degrees in the input file so sinphi = cosphi
     const amrex::Real latfac = std::sin(amr_wind::utils::radians(45.0));
 
