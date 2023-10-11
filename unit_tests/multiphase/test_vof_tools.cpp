999bf1262b576ef28e5237e127834e0e359d2571
Michael Kuhn <mkuhn@nrel.gov>
2023-10-11 12:26:49 -0600

    clarity in variable names (CI)

--- unit_tests/multiphase/test_vof_tools.cpp
+++ unit_tests/multiphase/test_vof_tools.cpp
@@ -488,19 +488,19 @@ TEST_F(VOFToolTest, replace_masked_vof)
     const int ncomp = 1;
     const int nghost = 3;
     const int nghost_int = 1;
-    auto& vof = repo.declare_field("vof", ncomp, nghost);
+    auto& vof_new = repo.declare_field("vof", ncomp, nghost);
     auto& vof_mod = repo.declare_field("vof_mod", ncomp, nghost);
     auto& iblank = repo.declare_int_field("iblank_cell", ncomp, nghost_int);
 
     // Use as if entire domain is from nalu
     iblank.setVal(-1);
     // Initialize and sharpen vof
-    init_vof(vof);
-    amr_wind::multiphase::sharpen_acquired_vof(1, iblank, vof);
+    init_vof(vof_new);
+    amr_wind::multiphase::sharpen_acquired_vof(1, iblank, vof_new);
     // Initialize other field (working copy of vof)
     vof_mod.setVal(100.0);
     // Replace masked vof values with new ones
-    amr_wind::multiphase::replace_masked_vof(1, iblank, vof_mod, vof);
+    amr_wind::multiphase::replace_masked_vof(1, iblank, vof_mod, vof_new);
 
     // Check results
     amrex::Real error_total = sharpen_test_impl(vof_mod);
