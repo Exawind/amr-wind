b82062096634597cd56c2adb7f5dc5a33f3306dc
Eliot Quon <eliot.quon@nrel.gov>
2023-11-29 13:07:34 -0700

    Fix formatting

--- amr-wind/wind_energy/coded_inlet_fields.h
+++ amr-wind/wind_energy/coded_inlet_fields.h
@@ -28,5 +28,5 @@ void temperature(double time, double x, double y, double z, double& pttemp) {
 extern "C" {
 
 void velocity(double time, double x, double y, double z, double (&ptvel)[3]);
-void temperature(double time, double x, double y, double z, double &pttemp);
+void temperature(double time, double x, double y, double z, double& pttemp);
 }
