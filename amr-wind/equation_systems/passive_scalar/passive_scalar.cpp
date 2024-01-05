65be85ff731e4b42c01bc20f6eacd5a2dcfb7bbe
Marc T. Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-01-05 15:58:38 -0700

    Add a passive scalar equation (#949)

---
+++ amr-wind/equation_systems/passive_scalar/passive_scalar.cpp
@@ -0,0 +1,11 @@
+#include "amr-wind/equation_systems/passive_scalar/passive_scalar.H"
+#include "amr-wind/equation_systems/AdvOp_Godunov.H"
+#include "amr-wind/equation_systems/AdvOp_MOL.H"
+#include "amr-wind/equation_systems/BCOps.H"
+
+namespace amr_wind::pde {
+
+template class PDESystem<PassiveScalar, fvm::Godunov>;
+template class PDESystem<PassiveScalar, fvm::MOL>;
+
+} // namespace amr_wind::pde
