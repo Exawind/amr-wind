9988fc8e47b60d4d0d00f84eee3b369747357fd4
Harish <harish.gopalan@gmail.com>
2024-03-06 13:17:24 -0700

    Add files via upload

--- amr-wind/turbulence/LES/Kosovic.cpp
+++ amr-wind/turbulence/LES/Kosovic.cpp
@@ -1,5 +1,4 @@
 #include <cmath>
-
 #include "amr-wind/turbulence/LES/Kosovic.H"
 #include "amr-wind/turbulence/TurbModelDefs.H"
 #include "amr-wind/fvm/nonLinearSum.H"
@@ -23,45 +22,24 @@ Kosovic<Transport>::Kosovic(CFDSim& sim)
 {
     amrex::ParmParse pp("Kosovic");
     pp.query("Cb", m_Cb);
-    m_Cs = std::sqrt(8 * (1 + m_Cb) / (27 * 3.14 * 3.14));
+    m_Cs = std::sqrt(8 * (1 + m_Cb) / (27 * M_PI * M_PI));
     m_C1 = std::sqrt(960) * m_Cb / (7 * (1 + m_Cb) * m_Sk);
     m_C2 = m_C1;
-    amrex::Print() << "C1:" << m_C1 << std::endl;
     pp.query("refMOL", m_refMOL);
-    if (std::abs(m_refMOL) > 1000)
-        amrex::Print()
-            << " Length scale assumes Neutral Stratification with Ref MOL:"
-            << m_refMOL << std::endl;
-    else if (m_refMOL < 0)
-        amrex::Print()
-            << " Length scale suggests Unstable  Stratification with Ref MOL:"
-            << m_refMOL << std::endl;
-    else
-        amrex::Print()
-            << " Length scale suggests Stable  Stratification with Ref MOL::"
-            << m_refMOL << std::endl;
     pp.query("surfaceRANS", m_surfaceRANS);
     if (m_surfaceRANS) {
         m_surfaceFactor = 1;
         pp.query("switchLoc", m_switchLoc);
         pp.query("surfaceRANSExp", m_surfaceRANSExp);
-        amrex::Print() << " Enabling RANS model in the surface layer upto "
-                       << m_switchLoc << "  m" << std::endl;
-        amrex::Print() << " Exponential term power coefficient:"
-                       << m_surfaceRANSExp << std::endl;
     } else {
         m_surfaceFactor = 0;
-        amrex::Print() << " No RANS model in the surface layer" << std::endl;
     }
     pp.query("writeTerms", m_writeTerms);
     if (m_writeTerms) {
         this->m_sim.io_manager().register_io_var("Nij");
         this->m_sim.io_manager().register_io_var("divNij");
-    } else
-        amrex::Print() << " Not writing the non-linear terms" << std::endl;
+    }
     pp.query("LESOff", m_LESTurnOff);
-    amrex::Print() << " Turning off SGS model above " << m_LESTurnOff << "   m"
-                   << std::endl;
 }
 
 template <typename Transport>
@@ -82,7 +60,6 @@ void Kosovic<Transport>::update_turbulent_viscosity(
     // a temporary buffer
     fvm::strainrate(mu_turb, vel);
     // Non-linear component Nij is computed here and goes into Body Forcing
-    // fvm::gradient(m_gradU,vel);
     fvm::nonlinearsum(m_Nij, vel);
     fvm::divergence(m_divNij, m_Nij);
     const int nlevels = repo.num_active_levels();
@@ -105,41 +82,28 @@ void Kosovic<Transport>::update_turbulent_viscosity(
                 bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
                     const amrex::Real rho = rho_arr(i, j, k);
                     const amrex::Real x3 = problo[2] + (k + 0.5) * dz;
-                    // if(i==76 && j==76)
-                    // amrex::Print()<<i<<"   "<<j<<"  "<<k<<"  "<<x3<<"
-                    // "<<dz<<std::endl;
                     const amrex::Real fmu = std::exp(-x3 / m_switchLoc);
-                    amrex::Real phiM = 1;
-                    if (m_refMOL < 0)
-                        phiM = std::pow(1 - 16 * x3 / m_refMOL, -0.25);
-                    else
-                        phiM = 1 + 5 * x3 / m_refMOL;
+                    const amrex::Real phiM =
+                        (m_refMOL < 0) ? std::pow(1 - 16 * x3 / m_refMOL, -0.25)
+                                       : 1 + 5 * x3 / m_refMOL;
                     const amrex::Real ransL =
                         std::pow(0.41 * (k + 1) * dz / phiM, 2);
                     amrex::Real turnOff = std::exp(-x3 / m_LESTurnOff);
-                    // if(x3>=m_LESTurnOff)
-                    //   turnOff=0;
-                    // const amrex::Real
-                    // masonScaleInv=1.0/(std::pow(0.41*(k+1)*dz,2))+1/smag_factor;
-                    // amrex::Real
-                    // hybridScale=amrex::min(smag_factor,std::pow(0.41*(k+1)*dz,2));
-                    // amrex::Real
-                    // lscale=m_surfaceFactor*hybridScale+(1-m_surfaceFactor)*smag_factor;
-                    amrex::Real lscale =
+                    amrex::Real viscosityScale =
                         m_surfaceFactor *
                             (std::pow(1 - fmu, m_surfaceRANSExp) * smag_factor +
                              std::pow(fmu, m_surfaceRANSExp) * ransL) +
                         (1 - m_surfaceFactor) * smag_factor;
-                    mu_arr(i, j, k) *= rho * lscale * turnOff;
-                    // lscale=m_surfaceFactor*hybridScale+(1-m_surfaceFactor)*smag_factor*0.25*m_C1;
-                    lscale = m_surfaceFactor *
-                                 (std::pow(1 - fmu, m_surfaceRANSExp) *
-                                      smag_factor * 0.25 * m_C1 +
-                                  std::pow(fmu, m_surfaceRANSExp) * ransL) +
-                             (1 - m_surfaceFactor) * smag_factor * 0.25 * m_C1;
-                    divNijLevel(i, j, k, 0) *= rho * lscale * turnOff;
-                    divNijLevel(i, j, k, 1) *= rho * lscale * turnOff;
-                    divNijLevel(i, j, k, 2) *= rho * lscale * turnOff;
+                    mu_arr(i, j, k) *= rho * viscosityScale * turnOff;
+                    amrex::Real stressScale =
+                        m_surfaceFactor *
+                            (std::pow(1 - fmu, m_surfaceRANSExp) * smag_factor *
+                                 0.25 * m_C1 +
+                             std::pow(fmu, m_surfaceRANSExp) * ransL) +
+                        (1 - m_surfaceFactor) * smag_factor * 0.25 * m_C1;
+                    divNijLevel(i, j, k, 0) *= rho * stressScale * turnOff;
+                    divNijLevel(i, j, k, 1) *= rho * stressScale * turnOff;
+                    divNijLevel(i, j, k, 2) *= rho * stressScale * turnOff;
                 });
         }
     }
