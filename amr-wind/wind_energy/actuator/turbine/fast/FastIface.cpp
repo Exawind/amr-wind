ff63a8ae74874c92e4a6c03f66252c67c5787c36
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-03-27 12:01:37 -0600

    Add documentation and remove non-scdx path

--- amr-wind/wind_energy/actuator/turbine/fast/FastIface.cpp
+++ amr-wind/wind_energy/actuator/turbine/fast/FastIface.cpp
@@ -219,7 +219,6 @@ void FastIface::fast_init_turbine(FastTurbine& fi)
     char inp_file[fast_strlen()];
     copy_filename(fi.input_file, inp_file);
 
-#ifdef AMR_WIND_FAST_USE_SCDX
     fast_func(
         FAST_OpFM_Init, &fi.tid_local, &fi.stop_time, inp_file, &fi.tid_global,
         &m_num_sc_inputs_glob, &m_num_sc_inputs, &m_num_sc_outputs,
@@ -227,14 +226,6 @@ void FastIface::fast_init_turbine(FastTurbine& fi)
         &fi.num_pts_tower, fi.base_pos, &abort_lev, &fi.dt_fast, &fi.num_blades,
         &fi.num_blade_elem, &fi.chord_cluster_type, &fi.to_cfd, &fi.from_cfd,
         &fi.to_sc, &fi.from_sc);
-#else
-    fast_func(
-        FAST_OpFM_Init, &fi.tid_local, &fi.stop_time, inp_file, &fi.tid_global,
-        &m_num_sc_inputs, &m_num_sc_outputs, &fi.num_pts_blade,
-        &fi.num_pts_tower, fi.base_pos, &abort_lev, &fi.dt_fast, &fi.num_blades,
-        &fi.num_blade_elem, &fi.chord_cluster_type, &fi.to_cfd, &fi.from_cfd,
-        &fi.to_sc, &fi.from_sc);
-#endif
 
     {
 #ifdef AMR_WIND_USE_OPENFAST
