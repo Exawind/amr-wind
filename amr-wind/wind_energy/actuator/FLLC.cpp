8df13ade0af0b1a11e747330528e737dfa52a2be
tonyinme <tonyinme@gmail.com>
2024-05-07 16:25:25 -0600

    Added non-uniform points

--- amr-wind/wind_energy/actuator/FLLC.cpp
+++ amr-wind/wind_energy/actuator/FLLC.cpp
@@ -1,4 +1,5 @@
 #include "amr-wind/wind_energy/actuator/FLLC.H"
+#include "amr-wind/utilities/linear_interpolation.H"
 
 namespace amr_wind::actuator {
 
@@ -38,6 +39,35 @@ void FLLCInit(
     data.dr[0] = vs::mag(view.pos[1] - view.pos[0]) / 2.;
     data.dr[npts - 1] = vs::mag(view.pos[npts - 1] - view.pos[npts - 2]) / 2.;
 
+    namespace interp = ::amr_wind::interp;
+
+    // Non-uniform radial distribution variables
+    size_t idx = 0;
+    amrex::Real dr;
+    amrex::Real dr1;
+    amrex::Real eps1;
+    amrex::Real dr2;
+    amrex::Real eps2;
+
+    // Non-uniform radial distribution calc
+    data.r_n.push_back(
+        data.span_distance_vel[0]); // Initialize the first location
+    while (data.r_n.back() < data.span_distance_vel.back()) {
+        amrex::Real r_ = data.r_n.back(); // The latest radial position
+        // Interpolate the value of epsilon to the current radial location
+        eps1 = interp::linear(data.span_distance_vel, data.optimal_epsilon, r_);
+        dr1 = eps1 / data.eps_dr; // spacing needed to match condition
+        // Same calculation as before but for the new location
+        eps2 = interp::linear(
+            data.span_distance_vel, data.optimal_epsilon, r_ + dr1);
+        dr2 = eps2 / data.eps_dr;
+        // This will ensure that the spacing will always follow the constraint
+        dr = std::min(dr1, dr2);
+        assert(dr > 0.);
+        data.r_n.push_back(std::min(r_ + dr, data.span_distance_vel.back()));
+        idx++;
+    }
+
     data.initialized = true;
 }
 
@@ -49,6 +79,8 @@ void FLLCParse(const utils::ActParser& pp, FLLCData& data)
     std::string typeString = "variable_chord";
     pp.query("fllc_type", typeString);
     data.correction_type = FLLCTypeMap.at(typeString);
+    pp.query("non-uniform", data.nonu);
+    pp.query("eps_dr", data.eps_dr);
 
     if (!pp.contains("epsilon") || !pp.contains("epsilon_chord")) {
         amrex::Abort(
