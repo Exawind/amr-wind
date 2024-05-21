8f464f055e6056c89ef9192db3f86cf865b65244
tonyinme <tonyinme@gmail.com>
2024-05-21 13:13:01 -0600

    Modified FLLC unit test and added nonuniform test

--- unit_tests/wind_energy/actuator/test_FLLC.cpp
+++ unit_tests/wind_energy/actuator/test_FLLC.cpp
@@ -22,12 +22,48 @@ TEST(TestFLLCData, data_initializes_with_cviews)
     view.chord = ::amr_wind::utils::slice(dummy_real, 0, num_points);
     view.vel_rel = ::amr_wind::utils::slice(dummy_vec, 0, num_points);
 
+    data.nonuniform = false;
+
     FLLCInit(data, view, 1.0);
 
     ASSERT_EQ(num_points, data.les_velocity.size());
     ASSERT_EQ(num_points, data.optimal_velocity.size());
     ASSERT_EQ(num_points, data.lift.size());
     ASSERT_EQ(num_points, data.grad_lift.size());
+}
+
+TEST(TestFLLCData, data_initializes_with_cviews_nonuniform)
+{
+    const int num_points = 5;
+
+    VecList dummy_vec(num_points);
+    RealList dummy_real(num_points);
+    TensorList dummy_tensor(num_points);
+
+    FLLCData data;
+
+    ComponentView view;
+    view.pos = ::amr_wind::utils::slice(dummy_vec, 0, num_points);
+    view.force = ::amr_wind::utils::slice(dummy_vec, 0, num_points);
+    view.epsilon = ::amr_wind::utils::slice(dummy_vec, 0, num_points);
+    view.orientation = ::amr_wind::utils::slice(dummy_tensor, 0, num_points);
+    view.chord = ::amr_wind::utils::slice(dummy_real, 0, num_points);
+    view.vel_rel = ::amr_wind::utils::slice(dummy_vec, 0, num_points);
+
+    data.nonuniform = true;
+    data.eps_dr = 1.1;
+
+    for (int ip = 0; ip < num_points; ++ip) {
+        view.pos[ip] = {0, static_cast<amrex::Real>(ip), 0};
+        view.chord[ip] = 1.;
+    }
+
+    FLLCInit(data, view, 1.0);
+
+    const int npts_r = static_cast<int>(data.nonuniform_r.size());
+    const int npts_dr = static_cast<int>(data.nonuniform_dr.size());
+
+    ASSERT_EQ(npts_r, npts_dr);
 }
 
 } // namespace amr_wind::actuator
