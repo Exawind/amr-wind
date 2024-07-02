37abe4be44aa7ada83281234a287c52cf5015e52
hgopalan <harish.gopalan@gmail.com>
2024-07-02 13:04:54 -0600

    Lint-clang-tidy formatting

--- amr-wind/physics/TerrainDrag.cpp
+++ amr-wind/physics/TerrainDrag.cpp
@@ -97,8 +97,8 @@ void TerrainDrag::post_init_actions()
             auto levelBlanking = blanking.array(mfi);
             auto levelDrag = drag.array(mfi);
             auto levelz0 = terrainz0.array(mfi);
-            const amrex::Real terrainSize = m_xterrain.size();
-            const amrex::Real roughnessSize = m_xrough.size();
+            int terrainSize = m_xterrain.size();
+            int roughnessSize = m_xrough.size();
             amrex::ParallelFor(
                 vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
                     // compute the source term
