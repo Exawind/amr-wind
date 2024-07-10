98d484b9343fe9dde5efcca7a854465432a5ce42
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-07-10 09:49:04 -0600

    remove unneeded change

--- amr-wind/wind_energy/actuator/Actuator.cpp
+++ amr-wind/wind_energy/actuator/Actuator.cpp
@@ -56,28 +56,19 @@ void Actuator::post_init_actions()
     BL_PROFILE("amr-wind::actuator::Actuator::post_init_actions");
 
     amrex::Vector<int> act_proc_count(amrex::ParallelDescriptor::NProcs(), 0);
-    {
-        BL_PROFILE(
-            "amr-wind::actuator::Actuator::post_init_actions::determine_root_"
-            "proc");
-        for (auto& act : m_actuators) {
-            act->determine_root_proc(act_proc_count);
-        }
+    for (auto& act : m_actuators) {
+        act->determine_root_proc(act_proc_count);
     }
 
     {
-        BL_PROFILE("amr-wind::actuator::Actuator::post_init_actions::sanity");
         // Sanity check that we have processed the turbines correctly
         int nact =
             std::accumulate(act_proc_count.begin(), act_proc_count.end(), 0);
         AMREX_ALWAYS_ASSERT(num_actuators() == nact);
     }
 
-    {
-        BL_PROFILE("amr-wind::actuator::Actuator::post_init_actions::init_src");
-        for (auto& act : m_actuators) {
-            act->init_actuator_source();
-        }
+    for (auto& act : m_actuators) {
+        act->init_actuator_source();
     }
 
     // Ensure velocity fills ghost cells prior to sampling
