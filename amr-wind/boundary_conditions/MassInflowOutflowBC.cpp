79e4669a6f70fa3329ee24c3ede314aa98ae905e
Mukul Dave <mhdave@lbl.gov>
2024-07-30 18:24:36 -0700

    change logic to use IntVect instead of booleans

--- amr-wind/boundary_conditions/MassInflowOutflowBC.cpp
+++ amr-wind/boundary_conditions/MassInflowOutflowBC.cpp
@@ -14,14 +14,14 @@ void MassInflowOutflowBC::operator()(Field& /*field*/, const FieldState /*rho_st
 {
     const auto& repo = m_field.repo();
     const auto& velocity = repo.get_field("velocity");
-    //const auto bcvals = m_field.bc_values_device();
+    // const auto bcvals = m_field.bc_values_device();
     const int ncomp = m_field.num_comp();
-    const int idx = static_cast<int>(m_ori);
+    // const int idx = static_cast<int>(m_ori);
     const int idim = m_ori.coordDir();
     const auto islow = m_ori.isLow();
     const auto ishigh = m_ori.isHigh();
     const int nlevels = m_field.repo().num_active_levels();
-    const bool ib = (idim == 0), jb = (idim == 1), kb = (idim == 2);
+    const amrex::IntVect iv_dir = {idim == 0, idim == 1, idim == 2};
 
     for (int lev = 0; lev < nlevels; ++lev) {
         const auto& domain = repo.mesh().Geom(lev).Domain();
@@ -35,7 +35,7 @@ void MassInflowOutflowBC::operator()(Field& /*field*/, const FieldState /*rho_st
 #endif
         for (amrex::MFIter mfi(m_field(lev), mfi_info); mfi.isValid(); ++mfi) {
             auto bx = mfi.validbox();
-            bx.grow({!ib, !jb, !kb});   // filling 1 ghost cell is enough? – check
+            bx.grow({!iv_dir[0], !iv_dir[1], !iv_dir[2]});
             const auto& bc_a = m_field(lev).array(mfi);
             const auto& vel = velocity(lev).array(mfi);
 
@@ -43,9 +43,11 @@ void MassInflowOutflowBC::operator()(Field& /*field*/, const FieldState /*rho_st
                 amrex::ParallelFor(
                     amrex::bdryLo(bx, idim),
                     [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                        if (vel(i-ib, j-jb, k-kb, idim) < 0) {
+                        const amrex::IntVect iv = {i, j, k};
+                        const amrex::IntVect ivm = iv - iv_dir;
+                        if (vel(ivm[0], ivm[1], ivm[2], idim) < 0) {
                             for (int n = 0; n < ncomp; n++) {
-                                bc_a(i-ib, j-jb, k-kb, n) = bc_a(i, j, k, n);
+                                bc_a(ivm[0], ivm[1], ivm[2], n) = bc_a(i, j, k, n);
                             }
                         }
                     });
@@ -55,9 +57,11 @@ void MassInflowOutflowBC::operator()(Field& /*field*/, const FieldState /*rho_st
                 amrex::ParallelFor(
                     amrex::bdryHi(bx, idim),
                     [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                        const amrex::IntVect iv = {i, j, k};
+                        const amrex::IntVect ivm = iv - iv_dir;
                         if (vel(i, j, k, idim) > 0) {
                             for (int n = 0; n < ncomp; n++) {
-                                bc_a(i, j, k, n) = bc_a(i-ib, j-jb, k-kb, n);
+                                bc_a(i, j, k, n) = bc_a(ivm[0], ivm[1], ivm[2], n);
                             }
                         }
                     });
