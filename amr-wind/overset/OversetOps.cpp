0baf2525b1eb0eaf5397bab0d0e44f41656f74ad
Michael Kuhn <mkuhn@nrel.gov>
2024-08-05 12:50:57 -0600

    proper use of refRatio

--- amr-wind/overset/OversetOps.cpp
+++ amr-wind/overset/OversetOps.cpp
@@ -334,11 +334,9 @@ void OversetOps::sharpen_nalu_data()
 
         // Average down fluxes across levels for consistency
         for (int lev = nlevels - 1; lev > 0; --lev) {
-            amrex::IntVect rr =
-                geom[lev].Domain().size() / geom[lev - 1].Domain().size();
             amrex::average_down_faces(
-                GetArrOfConstPtrs(fluxes[lev]), fluxes[lev - 1], rr,
-                geom[lev - 1]);
+                GetArrOfConstPtrs(fluxes[lev]), fluxes[lev - 1],
+                repo.mesh().refRatio(lev - 1), geom[lev - 1]);
         }
 
         // Get pseudo dt (dtau)
