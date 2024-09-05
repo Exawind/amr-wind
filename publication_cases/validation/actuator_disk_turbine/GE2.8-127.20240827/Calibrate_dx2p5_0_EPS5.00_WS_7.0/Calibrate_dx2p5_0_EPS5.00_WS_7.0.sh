9fb5ea6e802859ceab465e1c4de2b420b8071728
Michael Kuhn <mkuhn@nrel.gov>
2024-09-05 13:45:12 -0600

    add actuator disk cases

---
+++ publication_cases/validation/actuator_disk_turbine/GE2.8-127.20240827/Calibrate_dx2p5_0_EPS5.00_WS_7.0/Calibrate_dx2p5_0_EPS5.00_WS_7.0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=8
+#SBATCH --time=4:00:00        # Wall clock time (HH:MM:SS) - once the job exceeds this time, the job will be terminated (default is 5 minutes)
+#SBATCH --account=fy210193        # WC ID
+#SBATCH --job-name=GE2p8    # Name of job
+#SBATCH --partition=batch,short # partition/queue name: short or batch
+#SBATCH --qos=normal           # Quality of Service: long, large, priority or normal 
+# Number of nodes - the number of nodes you have requested (for a list of SLURM environment variables see "man sbatch")
+export nodes=$SLURM_JOB_NUM_NODES
+
+module purge; module load cde/v3/gcc/10.3.0 cde/v3/openmpi/4.1.2-gcc-10.3.0 cde/v3/hdf5/1.10.6-gcc-10.3.0-openmpi-4.1.2 cde/v3/netcdf-c/4.8.1-gcc-10.3.0-openmpi-4.1.2 cde/v3/cmake/3.23.1
+
+# Number MPI processes to run on each node (a.k.a. PPN)
+# CTS1 has 36 cores per node and Ghost
+#Chama has 16 cores per node
+export cores=16
+export ncpus=$((nodes * cores))
+export OMP_PROC_BIND=spread 
+export OMP_PLACES=threads
+
+mpiexec --bind-to core --npernode $cores --n $ncpus /projects/wind_uq/lcheung/AMRWindBuilds/hfm.20240102/amr-wind/build/amr_wind Calibrate_dx2p5_0_EPS5.00_WS_7.0.inp
+    
