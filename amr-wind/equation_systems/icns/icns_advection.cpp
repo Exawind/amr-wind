Exawind/amr-wind

commit 3e23581b132532bf70b09c38217ff9c46204f047

Inflow-outflow boundary condition (#1066)
---------

Co-authored-by: Marc T. Henry de Frahan <marchdf@gmail.com>

amr-wind/equation_systems/icns/icns_advection.cpp
@@ -64,6 +64,17 @@ MacProjOp::MacProjOp(
     m_has_overset = m_has_overset && !disable_ovst_mac;
 }
 
+void MacProjOp::enforce_inout_solvability(
+    const amrex::Vector<amrex::Array<amrex::MultiFab*, AMREX_SPACEDIM>>&
+        a_umac) noexcept
+{
+    auto& velocity = m_repo.get_field("velocity");
+    amrex::BCRec const* bc_type = velocity.bcrec_device().data();
+    const amrex::Vector<amrex::Geometry>& geom = m_repo.mesh().Geom();
+
+    HydroUtils::enforceInOutSolvability(a_umac, bc_type, geom);
+}
+
 void MacProjOp::init_projector(const MacProjOp::FaceFabPtrVec& beta) noexcept
 {
     m_mac_proj = std::make_unique<Hydro::MacProjector>(
@@ -264,6 +275,12 @@ void MacProjOp::operator()(const FieldState fstate, const amrex::Real dt)
                     density.num_comp(), 0);
             }
         }
+    }
+
+    const bool has_inout_bndry =
+        (m_repo.get_field("velocity")).has_inout_bndry();
+    if (has_inout_bndry) {
+        enforce_inout_solvability(mac_vec);
     }
 
     m_mac_proj->setUMAC(mac_vec);

amr-wind/projection/incflo_apply_nodal_projection.cpp
@@ -5,6 +5,7 @@
 #include "amr-wind/utilities/console_io.H"
 #include "amr-wind/core/field_ops.H"
 #include "amr-wind/projection/nodal_projection_ops.H"
+#include "hydro_utils.H"
 
 using namespace amrex;
 
@@ -46,6 +47,7 @@ amr_wind::nodal_projection::get_projection_bc(
                 r[dir] = LinOpBCType::Dirichlet;
                 break;
             }
+            case BC::mass_inflow_outflow:
             case BC::mass_inflow: {
                 r[dir] = LinOpBCType::inflow;
                 break;
@@ -74,6 +76,23 @@ void amr_wind::nodal_projection::apply_dirichlet_vel(
                 vel[nbx](i, j, k, n) = 0.0;
             }
         });
+}
+
+void amr_wind::nodal_projection::enforce_inout_solvability(
+    amr_wind::Field& velocity,
+    const Vector<Geometry>& geom,
+    const int num_levels)
+{
+    BCRec const* bc_type = velocity.bcrec_device().data();
+    Vector<Array<MultiFab*, AMREX_SPACEDIM>> vel_vec(num_levels);
+
+    for (int lev = 0; lev < num_levels; ++lev) {
+        vel_vec[lev][0] = new MultiFab(velocity(lev), amrex::make_alias, 0, 1);
+        vel_vec[lev][1] = new MultiFab(velocity(lev), amrex::make_alias, 1, 1);
+        vel_vec[lev][2] = new MultiFab(velocity(lev), amrex::make_alias, 2, 1);
+    }
+
+    HydroUtils::enforceInOutSolvability(vel_vec, bc_type, geom, true);
 }
 
 /** Perform nodal projection
@@ -329,7 +348,20 @@ void incflo::ApplyProjection(
         if (!proj_for_small_dt and !incremental) {
             amr_wind::nodal_projection::set_inflow_velocity(
                 m_sim.physics_manager(), velocity, lev, time, *vel[lev], 1);
+
+            // fill periodic boundaries to avoid corner cell issues
+            vel[lev]->FillBoundary(geom[lev].periodicity());
         }
+    }
+
+    // Need to apply custom Neumann funcs for inflow-outflow BC
+    // after setting the inflow vels above
+    // and then enforce solvability by matching outflow to inflow.
+    if (!proj_for_small_dt and !incremental and velocity.has_inout_bndry()) {
+        velocity.apply_bc_funcs(amr_wind::FieldState::New);
+
+        amr_wind::nodal_projection::enforce_inout_solvability(
+            velocity, m_repo.mesh().Geom(), m_repo.num_active_levels());
     }
 
     if (is_anelastic) {

amr-wind/boundary_conditions/BCInterface.H
@@ -49,7 +49,7 @@ public:
     virtual void operator()(const amrex::Real value = 0.0);
 
     //! User-defined functions for Dirichlet-type boundaries
-    std::pair<const std::string, const std::string> get_dirichlet_udfs();
+    amrex::Array<const std::string, 3> get_dirichlet_udfs();
 
 protected:
     //! Setup AMReX mathematical BC types

amr-wind/boundary_conditions/BCInterface.cpp
@@ -1,6 +1,7 @@
 #include "amr-wind/boundary_conditions/BCInterface.H"
 #include "amr-wind/core/FieldRepo.H"
 #include "amr-wind/boundary_conditions/FixedGradientBC.H"
+#include "amr-wind/boundary_conditions/MassInflowOutflowBC.H"
 #include "AMReX_ParmParse.H"
 
 namespace amr_wind {
@@ -64,6 +65,8 @@ void BCIface::read_bctype()
             ibctype[ori] = BC::pressure_outflow;
         } else if ((bcstr == "mass_inflow") || (bcstr == "mi")) {
             ibctype[ori] = BC::mass_inflow;
+        } else if ((bcstr == "mass_inflow_outflow") || (bcstr == "mio")) {
+            ibctype[ori] = BC::mass_inflow_outflow;
         } else if ((bcstr == "no_slip_wall") || (bcstr == "nsw")) {
             ibctype[ori] = BC::no_slip_wall;
         } else if ((bcstr == "slip_wall") || (bcstr == "sw")) {
@@ -100,18 +103,27 @@ void BCIface::set_bcfuncs()
         if (bct == BC::fixed_gradient) {
             m_field.register_custom_bc<FixedGradientBC>(ori);
         }
+
+        if ((m_field.name() == "velocity") // only velocity for now
+            && (bct == BC::mass_inflow_outflow)) {
+
+            m_field.register_custom_bc<MassInflowOutflowBC>(ori);
+        }
     }
 }
 
-std::pair<const std::string, const std::string> BCIface::get_dirichlet_udfs()
+amrex::Array<const std::string, 3> BCIface::get_dirichlet_udfs()
 {
     const auto& fname = m_field.name();
     const auto& bctype = m_field.bc_type();
     const std::string inflow_key = fname + ".inflow_type";
+    const std::string inflow_outflow_key = fname + ".inflow_outflow_type";
     const std::string wall_key = fname + ".wall_type";
     std::string inflow_udf{"ConstDirichlet"};
+    std::string inflow_outflow_udf{"ConstDirichlet"};
     std::string wall_udf{"ConstDirichlet"};
     bool has_inflow_udf = false;
+    bool has_inflow_outflow_udf = false;
     bool has_wall_udf = false;
 
     for (amrex::OrientationIter oit; oit != nullptr; ++oit) {
@@ -135,6 +147,22 @@ std::pair<const std::string, const std::string> BCIface::get_dirichlet_udfs()
             }
         }
 
+        if (bct == BC::mass_inflow_outflow) {
+            if (pp.contains(inflow_outflow_key.c_str())) {
+                std::string val;
+                pp.get(inflow_outflow_key.c_str(), val);
+
+                if (has_inflow_outflow_udf && (inflow_outflow_udf != val)) {
+                    amrex::Abort(
+                        "BC: Inflow-outflow UDF must be same for all "
+                        "inflow-outflow faces");
+                } else {
+                    inflow_outflow_udf = val;
+                    has_inflow_outflow_udf = true;
+                }
+            }
+        }
+
         if (bct == BC::slip_wall) {
             if (pp.contains(wall_key.c_str())) {
                 std::string val;
@@ -151,7 +179,7 @@ std::pair<const std::string, const std::string> BCIface::get_dirichlet_udfs()
         }
     }
 
-    return {inflow_udf, wall_udf};
+    return {inflow_udf, inflow_outflow_udf, wall_udf};
 }
 
 void BCVelocity::set_bcrec()
@@ -191,6 +219,15 @@ void BCVelocity::set_bcrec()
                 set_bcrec_lo(dir, amrex::BCType::ext_dir);
             } else {
                 set_bcrec_hi(dir, amrex::BCType::ext_dir);
+            }
+            break;
+
+        case BC::mass_inflow_outflow:
+            m_field.set_inout_bndry();
+            if (side == amrex::Orientation::low) {
+                set_bcrec_lo(dir, amrex::BCType::direction_dependent);
+            } else {
+                set_bcrec_hi(dir, amrex::BCType::direction_dependent);
             }
             break;
 
@@ -287,6 +324,15 @@ void BCScalar::set_bcrec()
             }
             break;
 
+        case BC::mass_inflow_outflow:
+            m_field.set_inout_bndry();
+            if (side == amrex::Orientation::low) {
+                set_bcrec_lo(dir, amrex::BCType::direction_dependent);
+            } else {
+                set_bcrec_hi(dir, amrex::BCType::direction_dependent);
+            }
+            break;
+
         case BC::slip_wall:
         case BC::wall_model:
         case BC::fixed_gradient:
@@ -310,7 +356,8 @@ void BCScalar::read_values()
     auto& bcval = m_field.bc_values();
     const int ndim = m_field.num_comp();
     const auto udfs = get_dirichlet_udfs();
-    const auto const_dirichlet_inflow = udfs.first == "ConstDirichlet";
+    const auto const_dirichlet_inflow = udfs[0] == "ConstDirichlet";
+    const auto const_dirichlet_inflow_outflow = udfs[1] == "ConstDirichlet";
 
     for (amrex::OrientationIter oit; oit != nullptr; ++oit) {
         auto ori = oit();
@@ -318,7 +365,9 @@ void BCScalar::read_values()
         const auto bct = bctype[ori];
 
         amrex::ParmParse pp(bcid);
-        if ((bct == BC::mass_inflow) && (const_dirichlet_inflow)) {
+        if (((bct == BC::mass_inflow) && (const_dirichlet_inflow)) ||
+            ((bct == BC::mass_inflow_outflow) &&
+             (const_dirichlet_inflow_outflow))) {
             pp.getarr(fname.c_str(), bcval[ori], 0, ndim);
         } else {
             pp.queryarr(fname.c_str(), bcval[ori], 0, ndim);

amr-wind/boundary_conditions/CMakeLists.txt
@@ -4,6 +4,7 @@ target_sources(${amr_wind_lib_name}
       BCInterface.cpp
       FixedGradientBC.cpp
       scalar_bcs.cpp
+      MassInflowOutflowBC.cpp
    )
 
 add_subdirectory(wall_models)

amr-wind/boundary_conditions/MassInflowOutflowBC.H
@@ -1,0 +1,31 @@
+#ifndef MASSINFLOWOUTFLOWBC_H
+#define MASSINFLOWOUTFLOWBC_H
+
+#include "amr-wind/core/FieldBCOps.H"
+#include "amr-wind/core/FieldRepo.H"
+
+#include "AMReX_Orientation.H"
+
+namespace amr_wind {
+
+/** Custom Neumann fills for the inflow-outflow BC
+ *  \ingroup field_bc
+ *
+ *  Used to fill the outflow boundary cells for mass-inflow-outflow BC
+ */
+class MassInflowOutflowBC : public FieldBCIface
+{
+public:
+    MassInflowOutflowBC(Field& field, amrex::Orientation ori);
+
+    void operator()(Field& field, const FieldState /*rho_state*/) override;
+
+private:
+    Field& m_field;
+
+    amrex::Orientation m_ori;
+};
+
+} // namespace amr_wind
+
+#endif /* MASSINFLOWOUTFLOWBC_H */

amr-wind/boundary_conditions/MassInflowOutflowBC.cpp
@@ -1,0 +1,75 @@
+
+#include "amr-wind/boundary_conditions/MassInflowOutflowBC.H"
+
+namespace amr_wind {
+
+MassInflowOutflowBC::MassInflowOutflowBC(Field& field, amrex::Orientation ori)
+    : m_field(field), m_ori(ori)
+{}
+
+void MassInflowOutflowBC::operator()(
+    Field& /*field*/, const FieldState /*rho_state*/)
+{
+    const auto& repo = m_field.repo();
+    const auto& velocity = repo.get_field("velocity");
+    const int ncomp = m_field.num_comp();
+    const int idim = m_ori.coordDir();
+    const auto islow = m_ori.isLow();
+    const auto ishigh = m_ori.isHigh();
+    const int nlevels = m_field.repo().num_active_levels();
+    const amrex::IntVect iv_dir = {
+        static_cast<int>(idim == 0), static_cast<int>(idim == 1),
+        static_cast<int>(idim == 2)};
+
+    for (int lev = 0; lev < nlevels; ++lev) {
+        const auto& domain = repo.mesh().Geom(lev).Domain();
+
+        amrex::MFItInfo mfi_info{};
+        if (amrex::Gpu::notInLaunchRegion()) {
+            mfi_info.SetDynamic(true);
+        }
+#ifdef AMREX_USE_OMP
+#pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
+#endif
+        for (amrex::MFIter mfi(m_field(lev), mfi_info); mfi.isValid(); ++mfi) {
+            auto bx = mfi.validbox();
+            bx.grow(
+                {static_cast<int>(idim != 0), static_cast<int>(idim != 1),
+                 static_cast<int>(idim != 2)});
+            const auto& bc_a = m_field(lev).array(mfi);
+            const auto& vel = velocity(lev).array(mfi);
+
+            if (islow && (bx.smallEnd(idim) == domain.smallEnd(idim))) {
+                amrex::ParallelFor(
+                    amrex::bdryLo(bx, idim),
+                    [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                        const amrex::IntVect iv = {i, j, k};
+                        const amrex::IntVect ivm = iv - iv_dir;
+                        if (vel(ivm[0], ivm[1], ivm[2], idim) < 0) {
+                            for (int n = 0; n < ncomp; n++) {
+                                bc_a(ivm[0], ivm[1], ivm[2], n) =
+                                    bc_a(i, j, k, n);
+                            }
+                        }
+                    });
+            }
+
+            if (ishigh && (bx.bigEnd(idim) == domain.bigEnd(idim))) {
+                amrex::ParallelFor(
+                    amrex::bdryHi(bx, idim),
+                    [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                        const amrex::IntVect iv = {i, j, k};
+                        const amrex::IntVect ivm = iv - iv_dir;
+                        if (vel(i, j, k, idim) > 0) {
+                            for (int n = 0; n < ncomp; n++) {
+                                bc_a(i, j, k, n) =
+                                    bc_a(ivm[0], ivm[1], ivm[2], n);
+                            }
+                        }
+                    });
+            }
+        }
+    }
+}
+
+} // namespace amr_wind

amr-wind/boundary_conditions/scalar_bcs.H
@@ -29,7 +29,7 @@ void register_scalar_dirichlet(
     Field& field,
     const amrex::AmrCore& mesh,
     const SimTime& time,
-    const std::pair<const std::string, const std::string>& udfs);
+    const amrex::Array<const std::string, 3>& udfs);
 
 } // namespace amr_wind::scalar_bc
 

commit ce97fe863a44633042f76c23b14288f07253bf19

Fix GPU segfault introduced in #1066

amr-wind/equation_systems/icns/icns_advection.cpp
@@ -69,7 +69,7 @@ void MacProjOp::enforce_inout_solvability(
         a_umac) noexcept
 {
     auto& velocity = m_repo.get_field("velocity");
-    amrex::BCRec const* bc_type = velocity.bcrec_device().data();
+    amrex::BCRec const* bc_type = velocity.bcrec().data();
     const amrex::Vector<amrex::Geometry>& geom = m_repo.mesh().Geom();
 
     HydroUtils::enforceInOutSolvability(a_umac, bc_type, geom);

amr-wind/projection/incflo_apply_nodal_projection.cpp
@@ -83,7 +83,7 @@ void amr_wind::nodal_projection::enforce_inout_solvability(
     const Vector<Geometry>& geom,
     const int num_levels)
 {
-    BCRec const* bc_type = velocity.bcrec_device().data();
+    BCRec const* bc_type = velocity.bcrec().data();
     Vector<Array<MultiFab*, AMREX_SPACEDIM>> vel_vec(num_levels);
 
     for (int lev = 0; lev < num_levels; ++lev) {