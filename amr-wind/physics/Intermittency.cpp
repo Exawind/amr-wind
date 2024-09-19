bd949609b0c53c6801ba727d6197f4ffdb968bc5
Ilker Topcuoglu <ilker.topcuoglu@nrel.gov>
2024-09-19 16:14:28 -0600

    Adding set_default_fillpatch_bc() for intermittency, and AMREX_ALWAYS_ASSERT() for num_ghost and fld.num_grow() in TiogaInterface::register_solution()

--- amr-wind/physics/Intermittency.cpp
+++ amr-wind/physics/Intermittency.cpp
@@ -6,17 +6,19 @@
 namespace amr_wind {
 
 Intermittency::Intermittency(const CFDSim& sim)
-    : m_intermittency(sim.repo().declare_field("intermittency",1,0,1))
+    : m_intermittency(sim.repo().declare_field(
+          "intermittency", 1, sim.pde_manager().num_ghost_state()))
 {
     amrex::ParmParse pp("incflo");
     pp.query("gamma_intermittency", m_gamma);
+    auto& gamma_int_fld = sim.repo().get_field("intermittency");
+    gamma_int_fld.set_default_fillpatch_bc(sim.time());
 }
 
 /** Initialize the intermittency field at the beginning of the
  *  simulation.
  */
-void Intermittency::initialize_fields(
-    int level, const amrex::Geometry& geom)
+void Intermittency::initialize_fields(int level, const amrex::Geometry& geom)
 {
     using namespace utils;
 
