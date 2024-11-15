9ecbeae933d72cb46809237726092b4994377409
Marc T. Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-11-15 10:15:51 -0700

    Allow string periodic for periodic BCs (#1347)

--- amr-wind/boundary_conditions/BCInterface.cpp
+++ amr-wind/boundary_conditions/BCInterface.cpp
@@ -47,7 +47,8 @@ void BCIface::read_bctype()
 
         // Protect against copy/paste errors where user intended to add a BC but
         // forgot to turn off periodic in that direction, or vice versa.
-        if (geom.isPeriodic(ori.coordDir()) && bcstr != "null") {
+        if (geom.isPeriodic(ori.coordDir()) &&
+            ((bcstr != "null") && (bcstr != "periodic"))) {
             amrex::Abort(
                 "Setting " + bcstr + " BC on a periodic face " + bcid +
                 " is not allowed");
