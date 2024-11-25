936fc258a881460efedbb526a9f18c3f4da2a795
Harish <harish.gopalan@gmail.com>
2024-11-25 16:33:21 -0700

    Some clean-up.

--- amr-wind/physics/ForestDrag.cpp
+++ amr-wind/physics/ForestDrag.cpp
@@ -6,6 +6,7 @@
 #include "AMReX_ParReduce.H"
 #include "amr-wind/utilities/trig_ops.H"
 #include "amr-wind/utilities/IOManager.H"
+#include "amr-wind/utilities/index_operations.H"
 
 namespace amr_wind::forestdrag {
 
@@ -42,6 +43,10 @@ ForestDrag::ForestDrag(CFDSim& sim)
         m_forests.push_back(f);
     }
     file.close();
+    m_d_forests.resize(m_forests.size());
+    amrex::Gpu::copy(
+        amrex::Gpu::hostToDevice, m_forests.begin(), m_forests.end(),
+        m_d_forests.begin());
     m_sim.io_manager().register_output_var("forest_drag");
     m_sim.io_manager().register_output_var("forest_blank");
 }
@@ -58,17 +63,16 @@ void ForestDrag::post_init_actions()
         auto& velocity = m_velocity(level);
         auto& drag = m_forest_drag(level);
         auto& blank = m_forest_blank(level);
-        unsigned forestSize = m_forests.size();
+        const auto* forests_ptr = m_d_forests.data();
+        const auto forestSize = m_d_forests.size();
         for (unsigned ii = 0; ii < forestSize; ++ii) {
-            amrex::Real treelaimax = m_forests[ii].calc_lm();
-            const amrex::Real type_forest = m_forests[ii].m_type_forest;
-            const amrex::Real x_forest = m_forests[ii].m_x_forest;
-            const amrex::Real y_forest = m_forests[ii].m_y_forest;
-            const amrex::Real height_forest = m_forests[ii].m_height_forest;
-            const amrex::Real diameter_forest = m_forests[ii].m_diameter_forest;
-            const amrex::Real cd_forest = m_forests[ii].m_cd_forest;
-            const amrex::Real lai_forest = m_forests[ii].m_lai_forest;
-            amrex::Real treeZm = 0.0;
+            const amrex::Real treelaimax = forests_ptr[ii].calc_lm();
+            const amrex::Real x_forest = forests_ptr[ii].m_x_forest;
+            const amrex::Real y_forest = forests_ptr[ii].m_y_forest;
+            const amrex::Real height_forest = forests_ptr[ii].m_height_forest;
+            const amrex::Real diameter_forest =
+                forests_ptr[ii].m_diameter_forest;
+            const amrex::Real cd_forest = forests_ptr[ii].m_cd_forest;
 #ifdef AMREX_USE_OMP
 #pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
 #endif
@@ -76,36 +80,38 @@ void ForestDrag::post_init_actions()
                 const auto& vbx = mfi.validbox();
                 auto levelDrag = drag.array(mfi);
                 auto levelBlank = blank.array(mfi);
-                amrex::ParallelFor(
-                    vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                        // compute the source term
-                        amrex::Real af = 0;
-                        const amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
-                        const amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
-                        const amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
-                        const amrex::Real radius = std::sqrt(
-                            (x - x_forest) * (x - x_forest) +
-                            (y - y_forest) * (y - y_forest));
-                        if (z <= height_forest &&
-                            radius <= (0.5 * diameter_forest)) {
-                            if (type_forest == 1) {
-                                af = lai_forest / height_forest;
-                            } else if (type_forest == 2) {
-                                const amrex::Real ratio =
-                                    (height_forest - treeZm) /
-                                    (height_forest - z);
-                                if (z < treeZm) {
-                                    af = treelaimax * std::pow(ratio, 6.0) *
-                                         std::exp(6 * (1 - ratio));
-                                } else if (z <= height_forest) {
-                                    af = treelaimax * std::pow(ratio, 0.5) *
-                                         std::exp(0.5 * (1 - ratio));
-                                }
+                const amrex::Real x1 = x_forest - 0.5 * diameter_forest;
+                const amrex::Real y1 = y_forest - 0.5 * diameter_forest;
+                const amrex::Real z1 = prob_lo[2];
+                const amrex::Real x2 = x_forest + 0.5 * diameter_forest;
+                const amrex::Real y2 = y_forest + 0.5 * diameter_forest;
+                const amrex::Real z2 = height_forest;
+                const amrex::RealBox& real_treebox{x1, y1, z1, x2, y2, z2};
+                const auto valid_treebox =
+                    amr_wind::utils::realbox_to_box(real_treebox, geom);
+                if (vbx.contains(valid_treebox)) {
+                    amrex::ParallelFor(
+                        vbx,
+                        [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                            // compute the source term
+                            const amrex::Real x =
+                                prob_lo[0] + (i + 0.5) * dx[0];
+                            const amrex::Real y =
+                                prob_lo[1] + (j + 0.5) * dx[1];
+                            const amrex::Real z =
+                                prob_lo[2] + (k + 0.5) * dx[2];
+                            const amrex::Real radius = std::sqrt(
+                                (x - x_forest) * (x - x_forest) +
+                                (y - y_forest) * (y - y_forest));
+                            if (z <= height_forest &&
+                                radius <= (0.5 * diameter_forest)) {
+                                levelBlank(i, j, k) = ii + 1;
+                                levelDrag(i, j, k) =
+                                    cd_forest *
+                                    forests_ptr[ii].calc_af(z, treelaimax);
                             }
-                            levelBlank(i, j, k) = ii + 1;
-                            levelDrag(i, j, k) = cd_forest * af;
-                        }
-                    });
+                        });
+                }
             }
         }
     }
