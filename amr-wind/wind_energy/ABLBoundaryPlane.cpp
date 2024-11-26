2682c66c28d169d7c4463f6ea5bf028e6d173696
Marc T. Henry de Frahan <marc.henrydefrahan@nrel.gov>
2024-11-26 11:01:40 -0700

    Fix debug build bug for bndry planes (#1375)

--- amr-wind/wind_energy/ABLBoundaryPlane.cpp
+++ amr-wind/wind_energy/ABLBoundaryPlane.cpp
@@ -1011,6 +1011,7 @@ amrex::Vector<amrex::BoxArray> ABLBoundaryPlane::read_bndry_native_boxarrays(
             const auto vismf = std::make_unique<amrex::VisMF>(mf_name);
             auto ba = vismf->boxArray();
             ba.growLo(normal, -1);
+            ba.growHi(normal, -1);
             AMREX_ALWAYS_ASSERT(ba.size() == 1);
             bndry_boxes[ilev].push_back(ba[0]);
         }
@@ -1020,7 +1021,6 @@ amrex::Vector<amrex::BoxArray> ABLBoundaryPlane::read_bndry_native_boxarrays(
         amrex::BoxArray ba(
             bndry_boxes[ilev].data(),
             static_cast<int>(bndry_boxes[ilev].size()));
-        amrex::Print() << "at lev : " << ilev << " ba: " << ba << std::endl;
         bndry_bas[ilev] = amrex::BoxArray(ba.minimalBox());
     }
     return bndry_bas;
