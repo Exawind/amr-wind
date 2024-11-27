6f865e86bfebfc318ed062a74c9ead74290fb03a
Harish <harish.gopalan@gmail.com>
2024-11-27 09:23:58 -0700

    Unit Test

---
+++ unit_tests/wind_energy/test_abl_forest.cpp
@@ -0,0 +1,71 @@
+#include "aw_test_utils/MeshTest.H"
+#include "aw_test_utils/iter_tools.H"
+#include "aw_test_utils/test_utils.H"
+#include "amr-wind/physics/ForestDrag.H"
+#include "amr-wind/core/field_ops.H"
+
+namespace {
+void write_forest(const std::string& fname)
+{
+    std::ofstream os(fname);
+    //! Write forest
+    os << "1  1024 512 45 200  0.2 6 0.8 \n";
+    os << "1  1024 1024 35 200  0.2 6 0.8 \n";
+    os << "1  1024 1224 75 200  0.2 6 0.8 \n";
+    os << "2  1024 1524 120 200 0.2 10 0.8 \n";
+}
+
+} // namespace
+
+namespace amr_wind_tests {
+
+// Testing the terrain drag reading to ensure that terrain is properly setup
+class ForestTest : public MeshTest
+{
+protected:
+    void populate_parameters() override
+    {
+        MeshTest::populate_parameters();
+        // Make computational domain like ABL mesh
+        {
+            amrex::ParmParse pp("amr");
+            amrex::Vector<int> ncell{{32, 32, 16}};
+            pp.addarr("n_cell", ncell);
+            pp.add("blocking_factor", 2);
+        }
+
+        {
+            amrex::ParmParse pp("geometry");
+            amrex::Vector<amrex::Real> probhi{{1024, 1024, 512}};
+            pp.addarr("prob_hi", probhi);
+        }
+    }
+    std::string m_forest_fname = "forest.amrwind";
+};
+
+TEST_F(ForestTest, forest)
+{
+    constexpr amrex::Real maxtol = 4.0;
+    // Write target wind file
+    write_forest(m_forest_fname);
+    populate_parameters();
+    initialize_mesh();
+    auto& pde_mgr = sim().pde_manager();
+    pde_mgr.register_icns();
+    sim().init_physics();
+    amrex::ParmParse pp("incflo");
+    amrex::Vector<std::string> physics{"forestDrag"};
+    pp.addarr("physics", physics);
+    amr_wind::forestdrag::ForestDrag forest_drag(sim());
+    const int nlevels = sim().repo().num_active_levels();
+    for (int lev = 0; lev < nlevels; ++lev) {
+        const auto& geom = sim().repo().mesh().Geom(lev);
+        forest_drag.initialize_fields(lev, geom);
+    }
+    const auto& forest_blank = sim().repo().get_field("forest_blank");
+    //! Maximun Value
+    const amrex::Real value_max = amr_wind::field_ops::global_max_magnitude(forest_blank);
+    EXPECT_EQ(value_max, maxtol);
+}
+
+} // namespace amr_wind_tests
