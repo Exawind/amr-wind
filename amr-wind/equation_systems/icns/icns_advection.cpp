a0f1654fc6cb18e99f58b40cd7f8c1a483d238fd
prakash <prakash.mohan@nrel.gov>
2024-12-04 15:04:28 -0700

    bugfix compile error for reused pp variable

--- amr-wind/equation_systems/icns/icns_advection.cpp
+++ amr-wind/equation_systems/icns/icns_advection.cpp
@@ -62,8 +62,10 @@ MacProjOp::MacProjOp(
     pp_ovst.query("disable_coupled_mac_proj", disable_ovst_mac);
     m_has_overset = m_has_overset && !disable_ovst_mac;
 #ifdef AMR_WIND_USE_FFT
-    amrex::ParmParse pp("mac_proj");
-    pp.query("use_fft", m_use_fft);
+    {
+        amrex::ParmParse pp("mac_proj");
+        pp.query("use_fft", m_use_fft);
+    }
 #endif
 }
 
