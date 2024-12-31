f206f28198006eed9096e8f8ada8ddd42c0a1075
mbkuhn <31661049+mbkuhn@users.noreply.github.com>
2024-12-31 21:37:07 +0000

    Deploying to gh-pages from @ Exawind/amr-wind@e13a6a4dfaa32e2bf4f226418f438c4d8c23602c 🚀

---
+++ doxygen/html/search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['abort_5ffunc_0',['abort_func',['../nc__interface_8cpp.html#aa67f9bcab4a9da3e92f2e2f3b4ec5979',1,'nc_interface.cpp']]]
+];
---
+++ doxygen/html/search/enumvalues_c.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['sid_0',['sid',['../structamr__wind_1_1sampling_1_1_i_ix.html#a23299e2daa159d3d7e25ddddfd02a08eae5684de089c4e96f0675449720d2632a',1,'amr_wind::sampling::IIx']]],
+  ['sigma1_1',['SIGMA1',['../namespaceamr__wind_1_1sampling_1_1sampling__utils.html#a7217a7ba09593692f8e1e24b60c0a73bae2f33497edb409cb56f1a629a45885a8',1,'amr_wind::sampling::sampling_utils']]],
+  ['sigma2_2',['SIGMA2',['../namespaceamr__wind_1_1sampling_1_1sampling__utils.html#a7217a7ba09593692f8e1e24b60c0a73ba5d84cb93256c8877622a9c9df30ce1a3',1,'amr_wind::sampling::sampling_utils']]],
+  ['sigma3_3',['SIGMA3',['../namespaceamr__wind_1_1sampling_1_1sampling__utils.html#a7217a7ba09593692f8e1e24b60c0a73ba7f0c8d0b3cc00d507904f71c3dcf7631',1,'amr_wind::sampling::sampling_utils']]],
+  ['slip_5fwall_4',['slip_wall',['../incflo__enums_8_h.html#a6e1412fca31cbd35d0f50f12fcc70b0fa03ee6de9fd100444c5c0567ea01bd538',1,'incflo_enums.H']]],
+  ['surface_5ftemperature_5',['SURFACE_TEMPERATURE',['../structamr__wind_1_1_m_o_data.html#a224d3e389ce06f0e7e4303a79e5dd994a317cef85d14b4909f1d76becb00dc0f1',1,'amr_wind::MOData']]],
+  ['symmetric_5fwall_6',['symmetric_wall',['../incflo__enums_8_h.html#a6e1412fca31cbd35d0f50f12fcc70b0fa91785621009395fc79a6aa9b27424b89',1,'incflo_enums.H']]]
+];
