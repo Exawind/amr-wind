556ccb16a9d68045dabf3a86ad54ee26085e3d66
Michael B Kuhn <31661049+mbkuhn@users.noreply.github.com>
2025-01-08 14:54:44 -0700

    Fill inflow routines for ocean waves (#1424)
    * just getting started
    
    * rename files, set up CMakeLists
    
    * setting up framework for routines within new files, modeled after ABLFillMPL; added important exceptions; compiling now
    
    * hook into ocean waves physics
    
    * incorporate set_inflow
    
    * include wave_generation as BC for set_inflow_sibling
    
    * use ocean waves to fill sibling inflow
    
    * formatting
    
    * remove redundant loop
    
    * remove erroneous comment
    
    * remove comments about immersed boundaries
    
    * trim ow boundary calls, variables will be updated from ocean waves model
    
    * have time specified for updating relaxation zones (ow fields)
    
    * remove comment, no longer applicable
    
    * remove grouping function "relaxation_zones()", make sure work is not duplicated
    
    * add comment to highlight needed changes
    
    * remove intermediate function
    
    * formatting
    
    * remove missed declaration
    
    * ignore unused
    
    * correct profiling call
    
    * setting up special fill logic for sibling fields
    
    * remove comment
    
    * limit lowest x to xlo so data in boundary is calculated at boundary (for linear and stokes waves)
    
    * include wave generation bc for sibling fill
    
    * fix error in density fill
    
    * ghost cells don't make sense if fillpatch is called later
    
    * unit test for ow boundary fills
    
    * add parallel sums
    
    * another parallel sum
    
    * more reasonable test mesh and parameters
    
    * convert other test to MeshTest so it doesn't rely on previous test for mesh
    
    * CI
    
    * another thing that should be in the other unit test
    
    * missed a spot

---
+++ amr-wind/ocean_waves/boundary_ops/OceanWavesBoundary.cpp
@@ -0,0 +1,215 @@
+#include "amr-wind/CFDSim.H"
+#include "amr-wind/ocean_waves/boundary_ops/OceanWavesBoundary.H"
+#include "amr-wind/ocean_waves/boundary_ops/OceanWavesFillInflow.H"
+#include "amr-wind/utilities/index_operations.H"
+#include "amr-wind/utilities/constants.H"
+#include "amr-wind/core/Physics.H"
+#include "amr-wind/wind_energy/ABL.H"
+#include "amr-wind/physics/multiphase/MultiPhase.H"
+
+namespace amr_wind {
+
+OceanWavesBoundary::OceanWavesBoundary(CFDSim& sim)
+    : m_time(sim.time())
+    , m_repo(sim.repo())
+    , m_mesh(sim.mesh())
+    , m_ow_velocity(sim.repo().get_field("ow_velocity"))
+    , m_ow_vof(sim.repo().get_field("ow_vof"))
+{
+    // Check for if boundary planes are being used; disable if so
+    if (sim.physics_manager().contains("ABL")) {
+        if (sim.physics_manager().get<amr_wind::ABL>().bndry_plane().mode() ==
+            io_mode::input) {
+            // Turn off ow_bndry; will rely on bndry_plane for fills
+            m_activate_ow_bndry = false;
+        }
+        if (sim.physics_manager().get<amr_wind::ABL>().abl_mpl().is_active()) {
+            amrex::Abort(
+                "OceanWavesBoundary: not currently compatible with ABL MPL "
+                "implementation.");
+        }
+    }
+    // Get liquid density, will only be used if vof is present
+    if (sim.physics_manager().contains("MultiPhase")) {
+        m_rho1 = sim.physics_manager().get<amr_wind::MultiPhase>().rho1();
+    }
+}
+
+void OceanWavesBoundary::post_init_actions()
+{
+    BL_PROFILE("amr-wind::OceanWavesBoundary::post_init_actions");
+    if (m_activate_ow_bndry) {
+        m_repo.get_field("velocity")
+            .register_fill_patch_op<OceanWavesFillInflow>(
+                m_mesh, m_time, *this);
+        if (m_repo.field_exists("vof")) {
+            m_repo.get_field("vof")
+                .register_fill_patch_op<OceanWavesFillInflow>(
+                    m_mesh, m_time, *this);
+            m_repo.get_field("density")
+                .register_fill_patch_op<OceanWavesFillInflow>(
+                    m_mesh, m_time, *this);
+        }
+    }
+}
+
+void OceanWavesBoundary::set_velocity(
+    const int lev,
+    const amrex::Real /*time*/,
+    const Field& fld,
+    amrex::MultiFab& mfab,
+    const int dcomp,
+    const int orig_comp) const
+{
+
+    if (!m_activate_ow_bndry) {
+        return;
+    }
+
+    BL_PROFILE("amr-wind::OceanWavesBoundary::set_velocity");
+
+    const auto& geom = m_mesh.Geom(lev);
+    const auto& bctype = fld.bc_type();
+    const int nghost = 1;
+    const auto& domain = geom.growPeriodicDomain(nghost);
+
+    for (amrex::OrientationIter oit; oit != nullptr; ++oit) {
+        auto ori = oit();
+        if ((bctype[ori] != BC::mass_inflow) &&
+            (bctype[ori] != BC::mass_inflow_outflow) &&
+            (bctype[ori] != BC::wave_generation)) {
+            continue;
+        }
+
+        const int idir = ori.coordDir();
+        const auto& dbx = ori.isLow() ? amrex::adjCellLo(domain, idir, nghost)
+                                      : amrex::adjCellHi(domain, idir, nghost);
+
+        for (amrex::MFIter mfi(mfab); mfi.isValid(); ++mfi) {
+            auto gbx = amrex::grow(mfi.validbox(), nghost);
+            const auto& bx =
+                utils::face_aware_boundary_box_intersection(gbx, dbx, ori);
+            if (!bx.ok()) {
+                continue;
+            }
+
+            const auto& targ_vof = m_ow_vof(lev).const_array(mfi);
+            const auto& targ_arr = m_ow_velocity(lev).const_array(mfi);
+            const auto& arr = mfab[mfi].array();
+            const int numcomp = mfab.nComp();
+
+            amrex::ParallelFor(
+                bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                    for (int n = 0; n < numcomp; n++) {
+                        if (targ_vof(i, j, k) > constants::LOOSE_TOL) {
+                            arr(i, j, k, dcomp + n) =
+                                targ_arr(i, j, k, orig_comp + n);
+                        }
+                    }
+                });
+        }
+    }
+}
+
+void OceanWavesBoundary::set_vof(
+    const int lev,
+    const amrex::Real /*time*/,
+    const Field& fld,
+    amrex::MultiFab& mfab) const
+{
+
+    if (!m_activate_ow_bndry) {
+        return;
+    }
+
+    BL_PROFILE("amr-wind::OceanWavesBoundary::set_vof");
+
+    const auto& geom = m_mesh.Geom(lev);
+    const auto& bctype = fld.bc_type();
+    const int nghost = 1;
+    const auto& domain = geom.growPeriodicDomain(nghost);
+
+    for (amrex::OrientationIter oit; oit != nullptr; ++oit) {
+        auto ori = oit();
+        if ((bctype[ori] != BC::mass_inflow) &&
+            (bctype[ori] != BC::mass_inflow_outflow) &&
+            (bctype[ori] != BC::wave_generation)) {
+            continue;
+        }
+
+        const int idir = ori.coordDir();
+        const auto& dbx = ori.isLow() ? amrex::adjCellLo(domain, idir, nghost)
+                                      : amrex::adjCellHi(domain, idir, nghost);
+
+        for (amrex::MFIter mfi(mfab); mfi.isValid(); ++mfi) {
+            auto gbx = amrex::grow(mfi.validbox(), nghost);
+            const auto& bx =
+                utils::face_aware_boundary_box_intersection(gbx, dbx, ori);
+            if (!bx.ok()) {
+                continue;
+            }
+
+            const auto& targ_arr = m_ow_vof(lev).const_array(mfi);
+            const auto& arr = mfab[mfi].array();
+
+            amrex::ParallelFor(
+                bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                    arr(i, j, k) = targ_arr(i, j, k);
+                });
+        }
+    }
+}
+
+void OceanWavesBoundary::set_density(
+    const int lev,
+    const amrex::Real /*time*/,
+    const Field& fld,
+    amrex::MultiFab& mfab) const
+{
+
+    if (!m_activate_ow_bndry || m_rho1 < 0.0) {
+        return;
+    }
+
+    BL_PROFILE("amr-wind::OceanWavesBoundary::set_density");
+
+    const auto& geom = m_mesh.Geom(lev);
+    const auto& bctype = fld.bc_type();
+    const int nghost = 1;
+    const amrex::Real rho1 = m_rho1;
+    const auto& domain = geom.growPeriodicDomain(nghost);
+
+    for (amrex::OrientationIter oit; oit != nullptr; ++oit) {
+        auto ori = oit();
+        if ((bctype[ori] != BC::mass_inflow) &&
+            (bctype[ori] != BC::mass_inflow_outflow) &&
+            (bctype[ori] != BC::wave_generation)) {
+            continue;
+        }
+
+        const int idir = ori.coordDir();
+        const auto& dbx = ori.isLow() ? amrex::adjCellLo(domain, idir, nghost)
+                                      : amrex::adjCellHi(domain, idir, nghost);
+
+        for (amrex::MFIter mfi(mfab); mfi.isValid(); ++mfi) {
+            auto gbx = amrex::grow(mfi.validbox(), nghost);
+            const auto& bx =
+                utils::face_aware_boundary_box_intersection(gbx, dbx, ori);
+            if (!bx.ok()) {
+                continue;
+            }
+
+            const auto& targ_vof = m_ow_vof(lev).const_array(mfi);
+            const auto& arr = mfab[mfi].array();
+
+            amrex::ParallelFor(
+                bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                    // Assume density is correct for gas phase only
+                    arr(i, j, k) = targ_vof(i, j, k) * rho1 +
+                                   (1.0 - targ_vof(i, j, k)) * arr(i, j, k);
+                });
+        }
+    }
+}
+
+} // namespace amr_wind
---
+++ amr-wind/ocean_waves/boundary_ops/OceanWavesFillInflow.cpp
@@ -0,0 +1,134 @@
+#include "amr-wind/ocean_waves/boundary_ops/OceanWavesFillInflow.H"
+#include "amr-wind/utilities/constants.H"
+
+namespace amr_wind {
+
+OceanWavesFillInflow::OceanWavesFillInflow(
+    Field& field,
+    const amrex::AmrCore& mesh,
+    const SimTime& time,
+    const OceanWavesBoundary& ow_bndry)
+    : FieldFillPatchOps<FieldBCDirichlet>(
+          field, mesh, time, FieldInterpolator::CellConsLinear)
+    , m_ow_bndry(ow_bndry)
+{}
+
+OceanWavesFillInflow::~OceanWavesFillInflow() = default;
+
+void OceanWavesFillInflow::fillpatch(
+    int lev,
+    amrex::Real time,
+    amrex::MultiFab& mfab,
+    const amrex::IntVect& nghost,
+    const FieldState fstate)
+{
+    FieldFillPatchOps<FieldBCDirichlet>::fillpatch(
+        lev, time, mfab, nghost, fstate);
+
+    if (m_field.base_name() == "velocity") {
+        m_ow_bndry.set_velocity(lev, time, m_field, mfab);
+    } else if (m_field.base_name() == "vof") {
+        m_ow_bndry.set_vof(lev, time, m_field, mfab);
+    } else if (m_field.base_name() == "density") {
+        m_ow_bndry.set_density(lev, time, m_field, mfab);
+    }
+}
+
+void OceanWavesFillInflow::fillpatch_from_coarse(
+    int lev,
+    amrex::Real time,
+    amrex::MultiFab& mfab,
+    const amrex::IntVect& nghost,
+    const FieldState fstate)
+{
+    FieldFillPatchOps<FieldBCDirichlet>::fillpatch_from_coarse(
+        lev, time, mfab, nghost, fstate);
+
+    if (m_field.base_name() == "velocity") {
+        m_ow_bndry.set_velocity(lev, time, m_field, mfab);
+    } else if (m_field.base_name() == "vof") {
+        m_ow_bndry.set_vof(lev, time, m_field, mfab);
+    } else if (m_field.base_name() == "density") {
+        m_ow_bndry.set_density(lev, time, m_field, mfab);
+    }
+}
+
+void OceanWavesFillInflow::fillphysbc(
+    int lev,
+    amrex::Real time,
+    amrex::MultiFab& mfab,
+    const amrex::IntVect& nghost,
+    const FieldState fstate)
+{
+    FieldFillPatchOps<FieldBCDirichlet>::fillphysbc(
+        lev, time, mfab, nghost, fstate);
+
+    if (m_field.base_name() == "velocity") {
+        m_ow_bndry.set_velocity(lev, time, m_field, mfab);
+    } else if (m_field.base_name() == "vof") {
+        m_ow_bndry.set_vof(lev, time, m_field, mfab);
+    } else if (m_field.base_name() == "density") {
+        m_ow_bndry.set_density(lev, time, m_field, mfab);
+    }
+}
+
+void OceanWavesFillInflow::fillpatch_sibling_fields(
+    int lev,
+    amrex::Real time,
+    amrex::Array<amrex::MultiFab*, AMREX_SPACEDIM>& mfabs,
+    amrex::Array<amrex::MultiFab*, AMREX_SPACEDIM>& ffabs,
+    amrex::Array<amrex::MultiFab*, AMREX_SPACEDIM>& cfabs,
+    const amrex::IntVect& nghost,
+    const amrex::Vector<amrex::BCRec>& bcrec,
+    const amrex::Vector<amrex::BCRec>& /* unused */,
+    const FieldState fstate)
+{
+    // Avoid trying to read after boundary has already been populated
+    const bool boundary_data_unchanged =
+        m_ow_bndry.get_boundary_data_time() - time < constants::LOOSE_TOL;
+    // First foextrap the MAC velocities
+    amrex::Vector<amrex::BCRec> fp_bcrec(m_field.num_comp());
+    amrex::Vector<amrex::BCRec> ph_bcrec(m_field.num_comp());
+    const auto& ibctype = m_field.bc_type();
+    for (amrex::OrientationIter oit; oit != nullptr; ++oit) {
+        auto ori = oit();
+        const auto side = ori.faceDir();
+        const auto bct = ibctype[ori];
+        const int dir = ori.coordDir();
+        for (int i = 0; i < m_field.num_comp(); ++i) {
+            if ((bct == BC::mass_inflow) || (bct == BC::mass_inflow_outflow) ||
+                (bct == BC::wave_generation)) {
+                if (side == amrex::Orientation::low) {
+                    ph_bcrec[i].setLo(dir, amrex::BCType::foextrap);
+                    fp_bcrec[i].setLo(
+                        dir, boundary_data_unchanged ? amrex::BCType::ext_dir
+                                                     : amrex::BCType::foextrap);
+                } else {
+                    ph_bcrec[i].setHi(dir, amrex::BCType::foextrap);
+                    fp_bcrec[i].setHi(
+                        dir, boundary_data_unchanged ? amrex::BCType::ext_dir
+                                                     : amrex::BCType::foextrap);
+                }
+            } else {
+                if (side == amrex::Orientation::low) {
+                    ph_bcrec[i].setLo(dir, bcrec[i].lo(dir));
+                    fp_bcrec[i].setLo(dir, bcrec[i].lo(dir));
+                } else {
+                    ph_bcrec[i].setHi(dir, bcrec[i].hi(dir));
+                    fp_bcrec[i].setHi(dir, bcrec[i].hi(dir));
+                }
+            }
+        }
+    }
+
+    FieldFillPatchOps<FieldBCDirichlet>::fillpatch_sibling_fields(
+        lev, time, mfabs, ffabs, cfabs, nghost, fp_bcrec, ph_bcrec, fstate);
+
+    if (!boundary_data_unchanged) {
+        for (int i = 0; i < static_cast<int>(mfabs.size()); i++) {
+            m_ow_bndry.set_velocity(lev, time, m_field, *mfabs[i], 0, i);
+        }
+    }
+}
+
+} // namespace amr_wind
