aa2b135c6eb9c4481df07810e83fbcc39e108184
moprak-nrel <120606615+moprak-nrel@users.noreply.github.com>
2025-01-30 20:50:00 +0000

    Deploying to gh-pages from @ Exawind/amr-wind@82f64d2bafcfc6882623747b82898b263fc14efb 🚀

---
+++ doxygen/html/search/variables_f.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['pitch_0',['pitch',['../structamr__wind_1_1actuator_1_1_wing_base_data.html#ad66a7da43e7862868f085455be28b6fb',1,'amr_wind::actuator::WingBaseData']]],
+  ['pitch_5ftable_1',['pitch_table',['../structamr__wind_1_1actuator_1_1_wing_base_data.html#aa4c0a7cde2b286d818fac312426d368d',1,'amr_wind::actuator::WingBaseData']]],
+  ['pitch_5ftimetable_5ffile_2',['pitch_timetable_file',['../structamr__wind_1_1actuator_1_1_wing_base_data.html#ad2697135f62219f174977bdb566b18ad',1,'amr_wind::actuator::WingBaseData']]],
+  ['pos_3',['pos',['../structamr__wind_1_1actuator_1_1_act_grid.html#a81a7c6339639c6ec5a51bb6d711c60ce',1,'amr_wind::actuator::ActGrid::pos'],['../structamr__wind_1_1actuator_1_1_component_view.html#aa2d45d8b55787d90704d231f45b1b573',1,'amr_wind::actuator::ComponentView::pos']]],
+  ['position_4',['position',['../structamr__wind_1_1actuator_1_1_actuator_cloud.html#a27d346a2c66d517e325d977be0c90e32',1,'amr_wind::actuator::ActuatorCloud']]],
+  ['postmac_5fadvection_5ftype_5',['postmac_advection_type',['../structamr__wind_1_1pde_1_1_advection_op_3_01_i_c_n_s_00_01fvm_1_1_godunov_01_4.html#a3d9d0686012d912e426205887f3bcd7c',1,'amr_wind::pde::AdvectionOp&lt; ICNS, fvm::Godunov &gt;']]],
+  ['premac_5fadvection_5ftype_6',['premac_advection_type',['../structamr__wind_1_1pde_1_1_advection_op_3_01_i_c_n_s_00_01fvm_1_1_godunov_01_4.html#acc39eed5db971d9eabca35e459cdc6dc',1,'amr_wind::pde::AdvectionOp&lt; ICNS, fvm::Godunov &gt;']]],
+  ['prescribed_5fuinf_7',['prescribed_uinf',['../structamr__wind_1_1actuator_1_1_wing_base_data.html#a7d1e9c408718e7eba97ea1238e55ea38',1,'amr_wind::actuator::WingBaseData']]],
+  ['process_5foutputs_8',['process_outputs',['../structamr__wind_1_1ib_1_1ops_1_1_use_default_op.html#a9d7d0dfcb9cde5eeee6c21b21f13d4a9',1,'amr_wind::ib::ops::UseDefaultOp::process_outputs'],['../structamr__wind_1_1actuator_1_1ops_1_1_use_default_op.html#a6826de33b0a24d4613e526297f0b7ae5',1,'amr_wind::actuator::ops::UseDefaultOp::process_outputs']]],
+  ['procs_9',['procs',['../structamr__wind_1_1actuator_1_1_act_info.html#af58195cf7e35dfcd33016b98593e2f10',1,'amr_wind::actuator::ActInfo']]],
+  ['ptr_5fbegin_10',['ptr_begin',['../structamr__wind_1_1utils_1_1_slice.html#a2fc07a34931b51eaf1a728e0a3e394e5',1,'amr_wind::utils::Slice']]],
+  ['ptr_5fend_11',['ptr_end',['../structamr__wind_1_1utils_1_1_slice.html#ae8193d06e1b3b149773c4fae6cc17e04',1,'amr_wind::utils::Slice']]]
+];
