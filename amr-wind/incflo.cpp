b3cf40a884f79c9127bb2d7fa0381995a2893f99
Mukul Dave <mhdave@lbl.gov>
2025-02-10 14:40:53 -0800

    Changes for coupling with ERF using MultiBlock (#1478)
    ---------
    
    Co-authored-by: Jean M. Sexton <jmsexton@lbl.gov>
    Co-authored-by: Marc T. Henry de Frahan <marchdf@gmail.com>

--- amr-wind/incflo.cpp
+++ amr-wind/incflo.cpp
@@ -23,6 +23,10 @@ incflo::incflo()
     // constructor. No valid BoxArray and DistributionMapping have been defined.
     // But the arrays for them have been resized.
 
+    amrex::Print() << std::endl
+                   << "Initializing AMR-Wind." << std::endl
+                   << std::endl;
+
     // Check if dry run is requested and set up if so
     CheckAndSetUpDryRun();
 
@@ -140,7 +144,7 @@ void incflo::prepare_for_time_integration()
         return;
     }
 
-    if (m_do_initial_proj || m_initial_iterations > 0) {
+    if (m_initial_iterations > 0) {
         m_sim.pde_manager().prepare_boundaries();
     }
 
