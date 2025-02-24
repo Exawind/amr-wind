14bff4ea9539cd53d7deccebb50a27d52490b653
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-02-24 11:18:50 -0700

    some physics

--- amr-wind/physics/multiphase/VortexPatch.cpp
+++ amr-wind/physics/multiphase/VortexPatch.cpp
@@ -45,63 +45,61 @@ void VortexPatch::initialize_fields(int level, const amrex::Geometry& geom)
     auto& v_mac = m_sim.repo().get_field("v_mac")(level);
     auto& w_mac = m_sim.repo().get_field("w_mac")(level);
 
-    for (amrex::MFIter mfi(velocity); mfi.isValid(); ++mfi) {
-        const auto& vbx = mfi.validbox();
-        auto uf = u_mac.array(mfi);
-        auto vf = v_mac.array(mfi);
-        auto wf = w_mac.array(mfi);
-        auto vel = velocity.array(mfi);
-        auto phi = levelset.array(mfi);
-        auto rho = density.array(mfi);
-        const amrex::Real eps = std::cbrt(2. * dx[0] * dx[1] * dx[2]);
+    const auto& uf_arrs = u_mac.arrays();
+    const auto& vf_arrs = v_mac.arrays();
+    const auto& wf_arrs = w_mac.arrays();
+    const auto& vel_arrs = velocity.arrays();
+    const auto& phi_arrs = levelset.arrays();
+    const auto& rho_arrs = density.arrays();
+    const amrex::Real eps = std::cbrt(2. * dx[0] * dx[1] * dx[2]);
 
-        amrex::ParallelFor(
-            vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
-                const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
-                const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
-                const amrex::Real xf = problo[0] + i * dx[0];
-                const amrex::Real yf = problo[1] + j * dx[1];
-                const amrex::Real zf = problo[2] + k * dx[2];
-                uf(i, j, k) = 2.0 * std::sin(M_PI * xf) * std::sin(M_PI * xf) *
-                              std::sin(2.0 * M_PI * y) *
-                              std::sin(2.0 * M_PI * z);
-                vf(i, j, k) = -std::sin(M_PI * yf) * std::sin(M_PI * yf) *
-                              std::sin(2.0 * M_PI * x) *
-                              std::sin(2.0 * M_PI * z);
-                wf(i, j, k) = -std::sin(M_PI * zf) * std::sin(M_PI * zf) *
-                              std::sin(2.0 * M_PI * x) *
-                              std::sin(2.0 * M_PI * y);
+    amrex::ParallelFor(
+        velocity, [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
+            const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+            const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
+            const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
+            const amrex::Real xf = problo[0] + i * dx[0];
+            const amrex::Real yf = problo[1] + j * dx[1];
+            const amrex::Real zf = problo[2] + k * dx[2];
+            uf_arrs[nbx](i, j, k) =
+                2.0 * std::sin(M_PI * xf) * std::sin(M_PI * xf) *
+                std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z);
+            vf_arrs[nbx](i, j, k) = -std::sin(M_PI * yf) * std::sin(M_PI * yf) *
+                                    std::sin(2.0 * M_PI * x) *
+                                    std::sin(2.0 * M_PI * z);
+            wf_arrs[nbx](i, j, k) = -std::sin(M_PI * zf) * std::sin(M_PI * zf) *
+                                    std::sin(2.0 * M_PI * x) *
+                                    std::sin(2.0 * M_PI * y);
 
-                vel(i, j, k, 0) =
-                    2.0 * std::sin(M_PI * x) * std::sin(M_PI * x) *
-                    std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z);
-                vel(i, j, k, 1) = -std::sin(M_PI * y) * std::sin(M_PI * y) *
-                                  std::sin(2.0 * M_PI * x) *
-                                  std::sin(2.0 * M_PI * z);
-                vel(i, j, k, 2) = -std::sin(M_PI * z) * std::sin(M_PI * z) *
-                                  std::sin(2.0 * M_PI * x) *
-                                  std::sin(2.0 * M_PI * y);
+            vel_arrs[nbx](i, j, k, 0) =
+                2.0 * std::sin(M_PI * x) * std::sin(M_PI * x) *
+                std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z);
+            vel_arrs[nbx](i, j, k, 1) =
+                -std::sin(M_PI * y) * std::sin(M_PI * y) *
+                std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * z);
+            vel_arrs[nbx](i, j, k, 2) =
+                -std::sin(M_PI * z) * std::sin(M_PI * z) *
+                std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * y);
 
-                phi(i, j, k) =
-                    radius - std::sqrt(
-                                 (x - xc) * (x - xc) + (y - yc) * (y - yc) +
-                                 (z - zc) * (z - zc));
-                amrex::Real smooth_heaviside;
-                if (phi(i, j, k) > eps) {
-                    smooth_heaviside = 1.0;
-                } else if (phi(i, j, k) < -eps) {
-                    smooth_heaviside = 0.;
-                } else {
-                    smooth_heaviside =
-                        0.5 *
-                        (1.0 + phi(i, j, k) / eps +
-                         1.0 / M_PI * std::sin(phi(i, j, k) * M_PI / eps));
-                }
-                rho(i, j, k) =
-                    rho1 * smooth_heaviside + rho2 * (1.0 - smooth_heaviside);
-            });
-    }
+            phi_arrs[nbx](i, j, k) =
+                radius - std::sqrt(
+                             (x - xc) * (x - xc) + (y - yc) * (y - yc) +
+                             (z - zc) * (z - zc));
+            amrex::Real smooth_heaviside;
+            if (phi_arrs[nbx](i, j, k) > eps) {
+                smooth_heaviside = 1.0;
+            } else if (phi_arrs[nbx](i, j, k) < -eps) {
+                smooth_heaviside = 0.;
+            } else {
+                smooth_heaviside =
+                    0.5 * (1.0 + phi_arrs[nbx](i, j, k) / eps +
+                           1.0 / M_PI *
+                               std::sin(phi_arrs[nbx](i, j, k) * M_PI / eps));
+            }
+            rho_arrs[nbx](i, j, k) =
+                rho1 * smooth_heaviside + rho2 * (1.0 - smooth_heaviside);
+        });
+    amrex::Gpu::synchronize();
 }
 
 void VortexPatch::pre_advance_work()
@@ -117,36 +115,35 @@ void VortexPatch::pre_advance_work()
         auto& u_mac = m_sim.repo().get_field("u_mac")(lev);
         auto& v_mac = m_sim.repo().get_field("v_mac")(lev);
         auto& w_mac = m_sim.repo().get_field("w_mac")(lev);
-        for (amrex::MFIter mfi(m_velocity(lev)); mfi.isValid(); ++mfi) {
-            const auto& vbx = mfi.growntilebox(1);
-            const auto& dx = geom[lev].CellSizeArray();
-            const auto& problo = geom[lev].ProbLoArray();
-            const amrex::Real TT = m_TT;
-            auto uf = u_mac.array(mfi);
-            auto vf = v_mac.array(mfi);
-            auto wf = w_mac.array(mfi);
-            amrex::ParallelFor(
-                vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                    const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
-                    const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
-                    const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
-                    const amrex::Real xf = problo[0] + i * dx[0];
-                    const amrex::Real yf = problo[1] + j * dx[1];
-                    const amrex::Real zf = problo[2] + k * dx[2];
-                    uf(i, j, k) =
-                        2.0 * std::sin(M_PI * xf) * std::sin(M_PI * xf) *
-                        std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z) *
-                        std::cos(M_PI * time / TT);
-                    vf(i, j, k) = -std::sin(M_PI * yf) * std::sin(M_PI * yf) *
-                                  std::sin(2.0 * M_PI * x) *
-                                  std::sin(2.0 * M_PI * z) *
-                                  std::cos(M_PI * time / TT);
-                    wf(i, j, k) = -std::sin(M_PI * zf) * std::sin(M_PI * zf) *
-                                  std::sin(2.0 * M_PI * x) *
-                                  std::sin(2.0 * M_PI * y) *
-                                  std::cos(M_PI * time / TT);
-                });
-        }
+        const auto& dx = geom[lev].CellSizeArray();
+        const auto& problo = geom[lev].ProbLoArray();
+        const amrex::Real TT = m_TT;
+        const auto& uf_arrs = u_mac.arrays();
+        const auto& vf_arrs = v_mac.arrays();
+        const auto& wf_arrs = w_mac.arrays();
+        amrex::ParallelFor(
+            m_velocity(lev), amrex::IntVect(1),
+            [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
+                const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+                const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
+                const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
+                const amrex::Real xf = problo[0] + i * dx[0];
+                const amrex::Real yf = problo[1] + j * dx[1];
+                const amrex::Real zf = problo[2] + k * dx[2];
+                uf_arrs[nbx](i, j, k) =
+                    2.0 * std::sin(M_PI * xf) * std::sin(M_PI * xf) *
+                    std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z) *
+                    std::cos(M_PI * time / TT);
+                vf_arrs[nbx](i, j, k) =
+                    -std::sin(M_PI * yf) * std::sin(M_PI * yf) *
+                    std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * z) *
+                    std::cos(M_PI * time / TT);
+                wf_arrs[nbx](i, j, k) =
+                    -std::sin(M_PI * zf) * std::sin(M_PI * zf) *
+                    std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * y) *
+                    std::cos(M_PI * time / TT);
+            });
+        amrex::Gpu::synchronize();
         u_mac.FillBoundary(geom[lev].periodicity());
         v_mac.FillBoundary(geom[lev].periodicity());
         w_mac.FillBoundary(geom[lev].periodicity());
@@ -162,32 +159,31 @@ void VortexPatch::post_advance_work()
 
     // Overriding the velocity field
     for (int lev = 0; lev < nlevels; ++lev) {
-        for (amrex::MFIter mfi(m_velocity(lev)); mfi.isValid(); ++mfi) {
-            const auto& vbx = mfi.growntilebox();
-            const auto& dx = geom[lev].CellSizeArray();
-            const auto& problo = geom[lev].ProbLoArray();
-            const amrex::Real TT = m_TT;
-            auto vel = m_velocity(lev).array(mfi);
-            amrex::ParallelFor(
-                vbx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-                    const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
-                    const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
-                    const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
-                    vel(i, j, k, 0) =
-                        2.0 * std::sin(M_PI * x) * std::sin(M_PI * x) *
-                        std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z) *
-                        std::cos(M_PI * time / TT);
-                    vel(i, j, k, 1) = -std::sin(M_PI * y) * std::sin(M_PI * y) *
-                                      std::sin(2.0 * M_PI * x) *
-                                      std::sin(2.0 * M_PI * z) *
-                                      std::cos(M_PI * time / TT);
-                    vel(i, j, k, 2) = -std::sin(M_PI * z) * std::sin(M_PI * z) *
-                                      std::sin(2.0 * M_PI * x) *
-                                      std::sin(2.0 * M_PI * y) *
-                                      std::cos(M_PI * time / TT);
-                });
-        }
+        const auto& dx = geom[lev].CellSizeArray();
+        const auto& problo = geom[lev].ProbLoArray();
+        const amrex::Real TT = m_TT;
+        const auto& vel_arrs = m_velocity(lev).arrays();
+        amrex::ParallelFor(
+            m_velocity(lev), amrex::IntVect(1),
+            [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
+                const amrex::Real x = problo[0] + (i + 0.5) * dx[0];
+                const amrex::Real y = problo[1] + (j + 0.5) * dx[1];
+                const amrex::Real z = problo[2] + (k + 0.5) * dx[2];
+                vel_arrs[nbx](i, j, k, 0) =
+                    2.0 * std::sin(M_PI * x) * std::sin(M_PI * x) *
+                    std::sin(2.0 * M_PI * y) * std::sin(2.0 * M_PI * z) *
+                    std::cos(M_PI * time / TT);
+                vel_arrs[nbx](i, j, k, 1) =
+                    -std::sin(M_PI * y) * std::sin(M_PI * y) *
+                    std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * z) *
+                    std::cos(M_PI * time / TT);
+                vel_arrs[nbx](i, j, k, 2) =
+                    -std::sin(M_PI * z) * std::sin(M_PI * z) *
+                    std::sin(2.0 * M_PI * x) * std::sin(2.0 * M_PI * y) *
+                    std::cos(M_PI * time / TT);
+            });
     }
+    amrex::Gpu::synchronize();
 
     m_velocity.fillpatch(time);
 }
