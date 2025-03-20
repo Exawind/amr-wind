05eb0fd0f426caad91ff911bd77367f4ab20938b
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-03-20 15:32:33 -0600

    Remove more deadweight and add implementation

--- amr-wind/physics/SuperEllipseBody.cpp
+++ amr-wind/physics/SuperEllipseBody.cpp
@@ -11,24 +11,35 @@
 
 namespace amr_wind::superellipsebody {
 
-namespace {} // namespace
+namespace {
+
+//! Utility function to parse inputs and return a vector instance
+inline vs::Vector parse_vector(amrex::ParmParse& pp, const std::string& key)
+{
+    amrex::Vector<amrex::Real> tmp;
+    pp.getarr(key.data(), tmp);
+    AMREX_ALWAYS_ASSERT(tmp.size() == 3U);
+
+    return vs::Vector{tmp[0], tmp[1], tmp[2]};
+}
+
+
+} // namespace
 
 SuperEllipseBody::SuperEllipseBody(CFDSim& sim)
     : m_sim(sim)
     , m_repo(sim.repo())
     , m_mesh(sim.mesh())
     , m_body_blank(sim.repo().declare_int_field("body_blank", 1, 1, 1))
-    , m_body_drag(sim.repo().declare_int_field("body_drag", 1, 1, 1))
 {
 
         amrex::ParmParse pp(identifier());
         pp.query("body_file", m_body_file);
+        m_dim = parse_vector(pp, "dimensions");
 
     m_sim.io_manager().register_output_int_var("body_drag");
-    m_sim.io_manager().register_output_int_var("body_blank");
 
     m_body_blank.setVal(0.0);
-    m_body_drag.setVal(0.0);
 }
 
 void SuperEllipseBody::initialize_fields(int level, const amrex::Geometry& geom)
@@ -39,16 +50,23 @@ void SuperEllipseBody::initialize_fields(int level, const amrex::Geometry& geom)
     std::ifstream file(m_body_file, std::ios::in);
     if (file.good()) {
         // Read coordinates and orientation from body file
+        // Just read 12 numbers one after the other. First 3 represent location, next 9 represent orientation
+        file >> m_loc[0] >> m_loc[1] >> m_loc[2];
+        file >> m_orient.xx() >> m_orient.xy() >> m_orient.xz() >> m_orient.yx()
+             >> m_orient.yy() >> m_orient.yz() >> m_orient.zx() >> m_orient.zy()
+             >> m_orient.zz();
     }
     file.close();
 
     const auto& dx = geom.CellSizeArray();
     const auto& prob_lo = geom.ProbLoArray();
     auto& blanking = m_body_blank(level);
-    auto& drag = m_body_drag(level);
     auto levelBlanking = blanking.arrays();
-    auto levelDrag = drag.arrays();
-    auto levelheight = body_height.arrays();
+
+    vs::Vector dim = m_dim;
+    vs::Tensor orient = m_orient;
+    vs::Vector loc = m_loc;
+
     amrex::ParallelFor(
         blanking, m_body_blank.num_grow(),
         [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
@@ -56,18 +74,11 @@ void SuperEllipseBody::initialize_fields(int level, const amrex::Geometry& geom)
             const amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];
             const amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
 
+            // Transform to orientation inside 
+            vs::Vector xtransform = orient & (vs::Vector(x, y, z) - loc);
+
             // Determine whether point in body
-            levelBlanking[nbx](i, j, k, 0) = 1;
-        });
-    amrex::Gpu::streamSynchronize();
-    amrex::ParallelFor(
-        blanking, [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
-            if ((levelBlanking[nbx](i, j, k, 0) == 0) && (k > 0) &&
-                (levelBlanking[nbx](i, j, k - 1, 0) == 1)) {
-                levelDrag[nbx](i, j, k, 0) = 1;
-            } else {
-                levelDrag[nbx](i, j, k, 0) = 0;
-            }
+            levelBlanking[nbx](i, j, k, 0) = static_cast<int>( (xtransform.x/dim.x)^6 + (xtransform.y/dim.y)^6 + (xtransform.z/dim.z)^6 - 1.0 );
         });
     amrex::Gpu::streamSynchronize();
 }
@@ -75,66 +86,19 @@ void SuperEllipseBody::initialize_fields(int level, const amrex::Geometry& geom)
 void SuperEllipseBody::post_init_actions()
 {
     BL_PROFILE("amr-wind::" + this->identifier() + "::post_init_actions");
-    compute_body_fields();
 }
 
 void SuperEllipseBody::pre_advance_work()
 {
     BL_PROFILE("amr-wind::" + this->identifier() + "::pre_advance_work");
-    compute_body_fields();
 }
 
 void SuperEllipseBody::post_regrid_actions()
 {
+
     const int nlevels = m_sim.repo().num_active_levels();
     for (int lev = 0; lev < nlevels; ++lev) {
         initialize_fields(lev, m_sim.repo().mesh().Geom(lev));
-    }
-}
-
-void SuperEllipseBody::compute_body_fields()
-{
-    const int nlevels = m_sim.repo().num_active_levels();
-    // Uniform, low roughness for waves
-    m_bodyz0.setVal(1e-4);
-    for (int level = 0; level < nlevels; ++level) {
-        const auto geom = m_sim.repo().mesh().Geom(level);
-        const auto& dx = geom.CellSizeArray();
-        const auto& prob_lo = geom.ProbLoArray();
-        auto& blanking = m_body_blank(level);
-        auto& body_height = m_body_height(level);
-        auto& drag = m_body_drag(level);
-
-        auto levelBlanking = blanking.arrays();
-        auto levelDrag = drag.arrays();
-        auto levelHeight = body_height.arrays();
-
-        const auto negative_wave_elevation =
-            (*m_wave_negative_elevation)(level).const_arrays();
-        const auto wave_vol_frac =
-            (*m_wave_volume_fraction)(level).const_arrays();
-
-        // Get body blanking from ocean waves fields
-        amrex::ParallelFor(
-            blanking, m_body_blank.num_grow(),
-            [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
-                const amrex::Real x = prob_lo[0] + (i + 0.5) * dx[0];
-                const amrex::Real y = prob_lo[1] + (j + 0.5) * dx[1];                
-                const amrex::Real z = prob_lo[2] + (k + 0.5) * dx[2];
-                //Determine whether a point inside the superellipsebody
-                levelBlanking[nbx](i, j, k, 0) = 1;
-
-            });
-        amrex::ParallelFor(
-            blanking,
-            [=] AMREX_GPU_DEVICE(int nbx, int i, int j, int k) noexcept {
-                if ((levelBlanking[nbx](i, j, k, 0) == 0) && (k > 0) &&
-                    (levelBlanking[nbx](i, j, k - 1, 0) == 1)) {
-                    levelDrag[nbx](i, j, k, 0) = 1;
-                } else {
-                    levelDrag[nbx](i, j, k, 0) = 0;
-                }
-            });
     }
 }
 
