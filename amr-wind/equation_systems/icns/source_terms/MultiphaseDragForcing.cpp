5fa996458015727783d97af8ee9c35b5c9009a74
Michael Kuhn <mkuhn@nrel.gov>
2025-04-15 11:30:01 -0600

    add parameters for drag model, equal and opposite in gas vs liquid

--- amr-wind/equation_systems/icns/source_terms/MultiphaseDragForcing.cpp
+++ amr-wind/equation_systems/icns/source_terms/MultiphaseDragForcing.cpp
@@ -39,18 +39,12 @@ MultiphaseDragForcing::MultiphaseDragForcing(const CFDSim& sim)
     , m_sim(sim)
     , m_mesh(sim.mesh())
     , m_velocity(sim.repo().get_field("velocity"))
+    , m_vof(sim.repo().get_field("vof"))
+    , m_density(sim.repo().get_field("density"))
 {
-    const auto terrain_phys =
-        m_sim.physics_manager().get<amr_wind::terraindrag::TerrainDrag>();
-    const auto target_vel_name = terrain_phys.wave_velocity_field_name();
-    m_target_vel = &sim.repo().get_field(target_vel_name);
-    const auto target_levelset_name =
-        terrain_phys.wave_negative_elevation_name();
-    m_target_levelset = &sim.repo().get_field(target_levelset_name);
-    const bool has_vof = sim.repo().field_exists("vof");
-    if (!has_vof) {
-        amrex::Abort("Works only when vof field is available");
-    }
+    amrex::ParmParse pp(identifier());
+    pp.query("wave_roughness", m_wave_roughness);
+    pp.query("density_ratio_limit", m_density_ratio_limit);
 }
 
 MultiphaseDragForcing::~MultiphaseDragForcing() = default;
@@ -64,55 +58,69 @@ void MultiphaseDragForcing::operator()(
 {
     const auto& vel =
         m_velocity.state(field_impl::dof_state(fstate))(lev).const_array(mfi);
-    auto* const m_vof = &this->m_sim.repo().get_field("vof");
-    const auto& vof = (*m_vof)(lev).const_array(mfi);
-    auto* const m_terrainz0 = &this->m_sim.repo().get_field("terrainz0");
-    const auto& terrainz0 = (*m_terrainz0)(lev).const_array(mfi);
-    const auto& target_vel_arr = (*m_target_vel)(lev).const_array(mfi);
-    const auto& target_lvs_arr = (*m_target_levelset)(lev).const_array(mfi);
+    const auto& vof = m_vof(lev).const_array(mfi);
+    const auto& rho = m_density(lev).const_array(mfi);
     const auto& geom = m_mesh.Geom(lev);
     const auto& dx = geom.CellSizeArray();
     const auto& dt = m_time.delta_t();
     const amrex::Real kappa = 0.41;
-    const amrex::Real z0_min = 1e-4;
+    const amrex::Real z0 = m_wave_roughness;
+    const amrex::Real f_rho = m_density_ratio_limit;
     const auto tiny = std::numeric_limits<amrex::Real>::epsilon();
     amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
-        const amrex::Real ux1 = vel(i, j, k, 0);
-        const amrex::Real uy1 = vel(i, j, k, 1);
+        const amrex::IntVect iv{i, j, k};
+        amrex::IntVect iv_gas{iv};
+        amrex::IntVect iv_liquid{iv};
+        int add_drag{0};
+        if (vof(i, j, k) < 0.5 && rho(i, j, k) < f_rho * rho(i, j, k - 1)) {
+            // Current cell is mostly gas, density ratio indicates drag should
+            // be added
+            add_drag = 1;
+            iv_liquid[2] -= 1;
+        }
+        if (vof(i, j, k + 1) < 0.5 && rho(i, j, k + 1) < f_rho * rho(i, j, k)) {
+            // Above cell is mostly gas, density ratio indicates drag should be
+            // subtracted
+            add_drag = -1;
+            iv_gas[2] += 1;
+        }
+        const amrex::IntVect iv_above_gas{iv_gas[0], iv_gas[1], iv_gas[2] + 1};
+
+        // Strain rate uses gas for flow velocity, liquid for wall velocity
+        const amrex::Real ux1 = vel(iv_gas, 0);
+        const amrex::Real uy1 = vel(iv_gas, 1);
         amrex::Real Dxz = 0.0;
         amrex::Real Dyz = 0.0;
         amrex::Real bc_forcing_x = 0;
         amrex::Real bc_forcing_y = 0;
-        int k_off = -1;
-        if (vof(i, j, k) == 0 && vof(i, j, k - 1) > 0) {
-            const amrex::Real cell_length_2D =
-                std::sqrt(dx[0] * dx[0] + dx[2] * dx[2]);
-            if (target_lvs_arr(i, j, k) + cell_length_2D >= 0) {
-                // Current cell will be used for wave velocity
-                k_off = 0;
-            }
-            const amrex::Real wall_u = target_vel_arr(i, j, k + k_off, 0);
-            const amrex::Real wall_v = target_vel_arr(i, j, k + k_off, 1);
-            // Relative velocities for calculating shear
-            const amrex::Real ux1r = ux1 - wall_u;
-            const amrex::Real uy1r = uy1 - wall_v;
-            const amrex::Real ux2r = vel(i, j, k + 1, 0) - wall_u;
-            const amrex::Real uy2r = vel(i, j, k + 1, 1) - wall_v;
-            const amrex::Real z0 = std::max(terrainz0(i, j, k), z0_min);
-            const amrex::Real ustar = viscous_drag_calculations(
-                Dxz, Dyz, ux1r, uy1r, ux2r, uy2r, z0, dx[2], kappa, tiny);
-            const amrex::Real uTarget =
-                ustar / kappa * std::log(0.5 * dx[2] / z0);
-            const amrex::Real uxTarget =
-                uTarget * ux2r / (tiny + std::sqrt(ux2r * ux2r + uy2r * uy2r));
-            const amrex::Real uyTarget =
-                uTarget * uy2r / (tiny + std::sqrt(ux2r * ux2r + uy2r * uy2r));
-            // BC forcing pushes nonrelative velocity toward target velocity
-            bc_forcing_x = -(uxTarget - ux1) / dt;
-            bc_forcing_y = -(uyTarget - uy1) / dt;
-        }
-        src_term(i, j, k, 0) -= Dxz + bc_forcing_x;
-        src_term(i, j, k, 1) -= Dyz + bc_forcing_y;
+
+        const amrex::Real wall_u = vel(iv_liquid, 0);
+        const amrex::Real wall_v = vel(iv_liquid, 1);
+        // Relative velocities for calculating shear
+        const amrex::Real ux1r = ux1 - wall_u;
+        const amrex::Real uy1r = uy1 - wall_v;
+        const amrex::Real ux2r = vel(iv_above_gas, 0) - wall_u;
+        const amrex::Real uy2r = vel(iv_above_gas, 1) - wall_v;
+        const amrex::Real ustar = viscous_drag_calculations(
+            Dxz, Dyz, ux1r, uy1r, ux2r, uy2r, z0, dx[2], kappa, tiny);
+        const amrex::Real uTarget = ustar / kappa * std::log(0.5 * dx[2] / z0);
+        const amrex::Real uxTarget =
+            uTarget * ux2r / (tiny + std::sqrt(ux2r * ux2r + uy2r * uy2r));
+        const amrex::Real uyTarget =
+            uTarget * uy2r / (tiny + std::sqrt(ux2r * ux2r + uy2r * uy2r));
+        // BC forcing pushes nonrelative velocity toward target velocity
+        bc_forcing_x = -(uxTarget - ux1) / dt;
+        bc_forcing_y = -(uyTarget - uy1) / dt;
+
+        // Drag force is calculated in gas phase and is equal and opposite in
+        // neighboring cell. Because source term is kinematic and local density
+        // is multiplied later, need to apply density ratio here
+        const amrex::Real r_rho = rho(iv_gas) / rho(iv);
+
+        src_term(i, j, k, 0) -=
+            static_cast<amrex::Real>(add_drag) * r_rho * (Dxz + bc_forcing_x);
+        src_term(i, j, k, 1) -=
+            static_cast<amrex::Real>(add_drag) * r_rho * (Dyz + bc_forcing_y);
     });
 }
 
