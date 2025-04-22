f9f1eeb019c50f39e0dd9aeaab9fb1f8ddb07a28
Harish <harish.gopalan@gmail.com>
2025-04-22 12:56:56 -0400

    Specified Monin-Obukhov Length  (#1418)
    * First commit. RANS no support yet.
    
    * RANS support and GPU change
    
    * Static.
    
    * clang
    
    * RANS model broke
    
    * Clang
    
    * Temperature support for non-neutral on terrain
    
    * clean
    
    * Fix
    
    * Update
    
    * consistency
    
    * Restore
    
    * Consistenct
    
    * Consistency
    
    * Consistency
    
    * Update
    
    * Clang
    
    * Warning
    
    * Works
    
    * tiny
    
    * Reg Test
    
    * Docs
    
    * TKE aware
    
    * gpu
    
    * Clean
    
    * bug
    
    * Replacing with soil temperature
    
    * Fix
    
    * Non-neutral diverged. Fixing it.
    
    * Sigh
    
    * GPU
    
    * bracket
    
    * clang
    
    * Clang
    
    * add regtests to cmakelists
    
    * Move MO parameters to use the mo object
    
    * remove commented code
    
    * Replace 1e-30 with constants::LOW_NUM
    
    * Extend MOData calc_psi_m to be static and use it instead of duplicate stability function
    
    * And replace duplicate thermal_stability with MOData::calc_psi_h
    
    * rename mol_length to monin_obukhov_length
    
    * Better init for monin_obukhov_length, set to large num to represent neutral
    
    * Apply suggestions from code review - formatting
    
    ---------
    
    Co-authored-by: prakash <120606615+moprak-nrel@users.noreply.github.com>
    Co-authored-by: prakash <prakash.mohan@nrel.gov>
    Co-authored-by: Michael B Kuhn <31661049+mbkuhn@users.noreply.github.com>

--- amr-wind/equation_systems/temperature/source_terms/DragTempForcing.cpp
+++ amr-wind/equation_systems/temperature/source_terms/DragTempForcing.cpp
@@ -1,6 +1,7 @@
 
 #include "amr-wind/equation_systems/temperature/source_terms/DragTempForcing.H"
 #include "amr-wind/utilities/IOManager.H"
+#include "amr-wind/wind_energy/MOData.H"
 
 #include "AMReX_ParmParse.H"
 #include "AMReX_Gpu.H"
@@ -9,18 +10,26 @@
 namespace amr_wind::pde::temperature {
 
 DragTempForcing::DragTempForcing(const CFDSim& sim)
-    : m_sim(sim)
+    : m_time(sim.time())
+    , m_sim(sim)
     , m_mesh(sim.mesh())
     , m_velocity(sim.repo().get_field("velocity"))
     , m_temperature(sim.repo().get_field("temperature"))
-    , m_transport(sim.transport_model())
 {
     amrex::ParmParse pp("DragTempForcing");
     pp.query("drag_coefficient", m_drag_coefficient);
-    if (pp.contains("reference_temperature")) {
-        amrex::Abort(
-            "DragTempForcing.reference_temperature has been deprecated. Please "
-            "replace with transport.reference_temperature.");
+    pp.query("soil_temperature", m_soil_temperature);
+    amrex::ParmParse pp_abl("ABL");
+    pp_abl.query("wall_het_model", m_wall_het_model);
+    pp_abl.query("monin_obukhov_length", m_monin_obukhov_length);
+    pp_abl.query("kappa", m_kappa);
+    pp_abl.query("mo_gamma_m", m_gamma_m);
+    pp_abl.query("mo_beta_m", m_beta_m);
+    pp_abl.query("mo_gamma_m", m_gamma_h);
+    pp_abl.query("mo_beta_m", m_beta_h);
+    {
+        amrex::ParmParse pp_incflow("incflo");
+        pp_incflow.queryarr("gravity", m_gravity);
     }
 }
 
@@ -46,24 +55,64 @@ void DragTempForcing::operator()(
     auto* const m_terrain_blank =
         &this->m_sim.repo().get_int_field("terrain_blank");
     const auto& blank = (*m_terrain_blank)(lev).const_array(mfi);
+    auto* const m_terrain_drag =
+        &this->m_sim.repo().get_int_field("terrain_drag");
+    const auto& drag = (*m_terrain_drag)(lev).const_array(mfi);
+    auto* const m_terrainz0 = &this->m_sim.repo().get_field("terrainz0");
+    const auto& terrainz0 = (*m_terrainz0)(lev).const_array(mfi);
     const auto& geom = m_mesh.Geom(lev);
     const auto& dx = geom.CellSizeArray();
     const amrex::Real drag_coefficient = m_drag_coefficient / dx[2];
-    amrex::FArrayBox ref_theta_fab(bx, 1, amrex::The_Async_Arena());
-    amrex::Array4<amrex::Real> const& ref_theta_arr = ref_theta_fab.array();
-    m_transport.ref_theta_impl(lev, mfi, bx, ref_theta_arr);
+    const amrex::Real gravity_mod = std::abs(m_gravity[2]);
+    const amrex::Real kappa = m_kappa;
+    const amrex::Real z0_min = 1e-4;
+    const amrex::Real monin_obukhov_length = m_monin_obukhov_length;
+    const auto& dt = m_time.delta_t();
+    const amrex::Real psi_m =
+        (m_wall_het_model == "mol")
+            ? MOData::calc_psi_m(
+                  1.5 * dx[2] / m_monin_obukhov_length, m_beta_m, m_gamma_m)
+            : 0.0;
+    const amrex::Real psi_h_neighbour =
+        (m_wall_het_model == "mol")
+            ? MOData::calc_psi_h(
+                  1.5 * dx[2] / m_monin_obukhov_length, m_beta_h, m_gamma_h)
+            : 0.0;
+    const amrex::Real psi_h_cell =
+        (m_wall_het_model == "mol")
+            ? MOData::calc_psi_h(
+                  0.5 * dx[2] / m_monin_obukhov_length, m_beta_h, m_gamma_h)
+            : 0.0;
     const auto tiny = std::numeric_limits<amrex::Real>::epsilon();
     const amrex::Real cd_max = 10.0;
+    const amrex::Real T0 = m_soil_temperature;
     amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+        const amrex::Real z0 = std::max(terrainz0(i, j, k), z0_min);
         const amrex::Real ux1 = vel(i, j, k, 0);
         const amrex::Real uy1 = vel(i, j, k, 1);
         const amrex::Real uz1 = vel(i, j, k, 2);
+        const amrex::Real theta = temperature(i, j, k, 0);
+        const amrex::Real theta2 = temperature(i, j, k + 1, 0);
+        const amrex::Real wspd = std::sqrt(ux1 * ux1 + uy1 * uy1);
+        const amrex::Real ustar =
+            wspd * kappa / (std::log(1.5 * dx[2] / z0) - psi_m);
+        //! We do not know the actual temperature so use cell above
+        const amrex::Real thetastar =
+            theta * ustar * ustar /
+            (kappa * gravity_mod * monin_obukhov_length);
+        const amrex::Real surf_temp =
+            theta2 -
+            thetastar / kappa * (std::log(1.5 * dx[2] / z0) - psi_h_neighbour);
+        const amrex::Real tTarget =
+            surf_temp +
+            thetastar / kappa * (std::log(0.5 * dx[2] / z0) - psi_h_cell);
+        const amrex::Real bc_forcing_t = -(tTarget - theta) / dt;
         const amrex::Real m = std::sqrt(ux1 * ux1 + uy1 * uy1 + uz1 * uz1);
         const amrex::Real Cd =
             std::min(drag_coefficient / (m + tiny), cd_max / dx[2]);
-        const amrex::Real T0 = ref_theta_arr(i, j, k);
         src_term(i, j, k, 0) -=
-            (Cd * (temperature(i, j, k, 0) - T0) * blank(i, j, k, 0));
+            (Cd * (theta - T0) * blank(i, j, k, 0) +
+             bc_forcing_t * drag(i, j, k));
     });
 }
 
