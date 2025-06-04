caca833ddb1031f69f9ae7d6e7c022019a2185b5
Michael Kuhn <mkuhn@nrel.gov>
2025-06-04 15:22:09 -0600

    unit test cleanup

--- unit_tests/utilities/test_time_averaging.cpp
+++ unit_tests/utilities/test_time_averaging.cpp
@@ -15,11 +15,6 @@ protected:
         {
             amrex::ParmParse pp("time");
             pp.add("fixed_dt", m_dt);
-            pp.add("cfl", 0.45);
-            pp.add("verbose", -1);
-            pp.add("regrid_interval", -1);
-            pp.add("checkpoint_interval", -1);
-            pp.add("plot_interval", -1);
         }
         {
             amrex::ParmParse pp("incflo");
@@ -43,6 +38,7 @@ protected:
     const std::string m_name = "temperature_mean_tavg";
     const amrex::Real m_fwidth = 2.0;
     const amrex::Real m_dt = 0.1;
+    const amrex::Real m_tol = 1e-8;
 };
 
 TEST_F(TimeAveragingTest, every_step)
@@ -63,27 +59,25 @@ TEST_F(TimeAveragingTest, every_step)
     post_manager.post_init_actions();
 
     const auto& f_avg = sim().repo().get_field(m_name);
-    constexpr amrex::Real tol = 1e-8;
 
     amrex::Real avg_val{0.};
     while (time.new_timestep()) {
         // Give field a linear profile with time
         const amrex::Real fval = 10. * time.current_time();
         temp.setVal(fval);
-        time.set_current_cfl(0.45 / 0.3, 0.0, 0.0);
         time.advance_time();
         post_manager.post_advance_work();
         // Get field values
         const auto max_f = utils::field_max(f_avg);
         const auto min_f = utils::field_min(f_avg);
         // Confirm mean fields are uniform
-        EXPECT_NEAR(max_f, min_f, tol);
+        EXPECT_NEAR(max_f, min_f, m_tol);
         // Confirm value is as expected
         const amrex::Real avg_time =
             amrex::max(amrex::min(time.new_time(), m_fwidth), m_dt);
         const amrex::Real old_avg_time = amrex::max(avg_time - m_dt, 0.);
         avg_val = (avg_val * (old_avg_time) + m_dt * fval) / avg_time;
-        EXPECT_NEAR(max_f, avg_val, tol);
+        EXPECT_NEAR(max_f, avg_val, m_tol);
     }
 }
 
@@ -101,7 +95,6 @@ TEST_F(TimeAveragingTest, phase_linear)
     post_manager.post_init_actions();
 
     const auto& f_avg = sim().repo().get_field(m_name);
-    constexpr amrex::Real tol = 1e-8;
 
     amrex::Real avg_val{0.};
     int step_count{0};
@@ -110,14 +103,13 @@ TEST_F(TimeAveragingTest, phase_linear)
         // Give field a linear profile with time
         const amrex::Real fval = 10. * time.current_time();
         temp.setVal(fval);
-        time.set_current_cfl(0.45 / 0.3, 0.0, 0.0);
         time.advance_time();
         post_manager.post_advance_work();
         // Get field values
         const auto max_f = utils::field_max(f_avg);
         const auto min_f = utils::field_min(f_avg);
         // Confirm mean fields are uniform
-        EXPECT_NEAR(max_f, min_f, tol);
+        EXPECT_NEAR(max_f, min_f, m_tol);
         // Confirm value is as expected
         if (step_count % 2 == 0) {
             const amrex::Real avg_time =
@@ -125,7 +117,7 @@ TEST_F(TimeAveragingTest, phase_linear)
             const amrex::Real old_avg_time =
                 amrex::max(avg_time - 2. * m_dt, 0.);
             avg_val = (avg_val * (old_avg_time) + 2. * m_dt * fval) / avg_time;
-            EXPECT_NEAR(max_f, avg_val, tol);
+            EXPECT_NEAR(max_f, avg_val, m_tol);
         }
     }
 }
@@ -148,7 +140,6 @@ TEST_F(TimeAveragingTest, phase_stairstep_offset)
     post_manager.post_init_actions();
 
     const auto& f_avg = sim().repo().get_field(m_name);
-    constexpr amrex::Real tol = 1e-8;
 
     int step_count{0};
     while (time.new_timestep()) {
@@ -156,17 +147,16 @@ TEST_F(TimeAveragingTest, phase_stairstep_offset)
         // Give field a linear profile with time
         const amrex::Real fval = (step_count % 2) * 5.;
         temp.setVal(fval);
-        time.set_current_cfl(0.45 / 0.3, 0.0, 0.0);
         time.advance_time();
         post_manager.post_advance_work();
         // Get field values
         const auto max_f = utils::field_max(f_avg);
         const auto min_f = utils::field_min(f_avg);
         // Confirm mean fields are uniform
-        EXPECT_NEAR(max_f, min_f, tol);
+        EXPECT_NEAR(max_f, min_f, m_tol);
         // Confirm value is as expected
         if (step_count % 2 == 1) {
-            EXPECT_NEAR(max_f, 5.0, tol);
+            EXPECT_NEAR(max_f, 5.0, m_tol);
         }
     }
 }
@@ -189,12 +179,10 @@ TEST_F(TimeAveragingTest, mismatch_time_interval)
     post_manager.post_init_actions();
 
     const auto& f_avg = sim().repo().get_field(m_name);
-    constexpr amrex::Real tol = 1e-8;
 
     int step_count{0};
     while (time.new_timestep()) {
         ++step_count;
-        time.set_current_cfl(0.45 / 0.3, 0.0, 0.0);
         time.advance_time();
         // Give field a linear profile with time
         const amrex::Real fval = 10. * time.new_time();
@@ -206,7 +194,7 @@ TEST_F(TimeAveragingTest, mismatch_time_interval)
     const auto max_f = utils::field_max(f_avg);
     const auto min_f = utils::field_min(f_avg);
     // Confirm mean fields are uniform
-    EXPECT_NEAR(max_f, min_f, tol);
+    EXPECT_NEAR(max_f, min_f, m_tol);
     // Times when averaging should occur:
     // 0.2, 0.3, 0.5, 0.6, 0.8, 0.9
     amrex::Real avg_val = 0.2 * 2.0 / 0.2;
@@ -215,13 +203,13 @@ TEST_F(TimeAveragingTest, mismatch_time_interval)
     avg_val = (avg_val * (0.6 - 0.1) + 6.0 * 0.1) / 0.6;
     avg_val = (avg_val * (0.8 - 0.2) + 8.0 * 0.2) / 0.8;
     avg_val = (avg_val * (0.9 - 0.1) + 9.0 * 0.1) / 0.9;
-    EXPECT_NEAR(max_f, avg_val, tol);
+    EXPECT_NEAR(max_f, avg_val, m_tol);
 
     const amrex::Real avg_val_ideal = (0.2 * 2.0 + 0.1 * 3.0 + 0.2 * 5.0 +
                                        0.1 * 6.0 + 0.2 * 8.0 + 0.1 * 9.0) /
                                       0.9;
 
-    EXPECT_NEAR(max_f, avg_val_ideal, tol);
+    EXPECT_NEAR(max_f, avg_val_ideal, m_tol);
 }
 
 } // namespace amr_wind_tests
