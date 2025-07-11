118966d04049193e2a45f61fe494cce4783eb595
mbkuhn <31661049+mbkuhn@users.noreply.github.com>
2025-07-11 21:17:22 +0000

    Deploying to gh-pages from @ Exawind/amr-wind@ec37004957332935f9e6f85fc06d8fbe26f668a6 🚀

---
+++ doxygen/html/search/typedefs_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ofinputtype_0',['OfInputType',['../namespaceexw__fast.html#ab5149debc8923e8d332bb980a566277c',1,'exw_fast']]],
+  ['ofoutputtype_1',['OfOutputType',['../namespaceexw__fast.html#a06c7c1043ffdf1223fa56a0b91f92bc0',1,'exw_fast']]],
+  ['ownertype_2',['OwnerType',['../classamr__wind_1_1actuator_1_1ops_1_1_spreading_function.html#a61468c81f41b7910fa7af4aeb4b49389',1,'amr_wind::actuator::ops::SpreadingFunction']]]
+];
