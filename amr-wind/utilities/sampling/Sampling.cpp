af21693074ac1ac01aff20a2904a6698284f1407
Matt Churchfield <matt.churchfield@nrel.gov>
2026-01-08 17:14:10 -0500

    One more fix of formatting.

--- amr-wind/utilities/sampling/Sampling.cpp
+++ amr-wind/utilities/sampling/Sampling.cpp
@@ -428,7 +428,7 @@ void Sampling::write_info_file(const std::string& fname)
     }
 
     // YAML formatting
-    fh << "time: " << std::setprecision(6) << m_sim.time().new_time() 
+    fh << "time: " << std::setprecision(6) << m_sim.time().new_time()
        << std::endl;
     fh << "samplers:" << std::endl;
     for (int i = 0; i < m_samplers.size(); ++i) {
