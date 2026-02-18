9df0fff1e203569cec7d74168479a333b80cbde9
Jon Rood <jon.rood@nrel.gov>
2026-02-18 16:11:46 -0700

    Fix compilation error.

--- unit_tests/wind_energy/actuator/test_actuator_joukowsky_disk.cpp
+++ unit_tests/wind_energy/actuator/test_actuator_joukowsky_disk.cpp
@@ -182,9 +182,7 @@ struct ComputeForceOp<::amr_wind_tests::Joukowsky, ActSrcDisk>
 template <>
 struct ProcessOutputsOp<::amr_wind_tests::Joukowsky, ActSrcDisk>
 {
-    ProcessOutputsOp<::amr_wind_tests::Joukowsky, ActSrcDisk>(
-        ::amr_wind_tests::Joukowsky::DataType& /**/)
-    {}
+    ProcessOutputsOp(::amr_wind_tests::Joukowsky::DataType& /**/) {}
     void operator()(::amr_wind_tests::Joukowsky::DataType& /*data*/) {}
     void read_io_options(const utils::ActParser& /**/) {}
     void prepare_outputs(const std::string& /**/) {}
