f97c9d8d2a5fbcd03bff39aa6ca7c90272d95bfa
jrood-nrel <jon.rood@nrel.gov>
2026-02-21 00:31:34 +0000

    Deploying to gh-pages from @ Exawind/amr-wind@09f25dbebc254f6450e67457ccedb5bcff2b7ed7 🚀

---
+++ _sources/references.rst.txt
@@ -0,0 +1,30 @@
+How to Cite AMR-Wind
+====================
+
+The following journal articles (`AMR-Wind reference <https://doi.org/10.1002/we.70010>`_, `ExaWind reference <https://doi.org/10.1002/we.2886>`_) should be cited when referencing AMR-Wind::
+
+    @article{amrwind2025,
+        author = {Kuhn, Michael B. and {Henry de Frahan}, Marc T. and Mohan, Prakash and Deskos, Georgios and Churchfield, Matthew and Cheung, Lawrence and Sharma, Ashesh and Almgren, Ann and Ananthan, Shreyas and Brazell, Michael J. and {Martinez-Tossas} Luis A. and Thedin, Regis and Rood, Jon and Sakievich, Philip and Vijayakumar, Ganesh and Zhang, Weiqun and Sprague, Michael A.},
+        title = {AMR-Wind: A performance-portable, high-fidelity flow solver for wind farm simulations},
+        journal = {Wind Energy},
+        volume = {28},
+        number = {5},
+        pages = {-},
+        doi = {10.1002/we.70010},
+        url = {https://onlinelibrary.wiley.com/doi/full/10.1002/we.70010},
+        eprint = {},
+        year = {2025}
+    }
+
+    @article{exawind2024,
+        author = {Sharma, Ashesh and Brazell, Michael J. and Vijayakumar, Ganesh and Ananthan, Shreyas and Cheung, Lawrence and deVelder, Nathaniel and {Henry de Frahan}, Marc T. and Matula, Neil and Mullowney, Paul and Rood, Jon and Sakievich, Philip and Almgren, Ann and Crozier, Paul S. and Sprague, Michael},
+        title = {ExaWind: Open-source CFD for hybrid-RANS/LES geometry-resolved wind turbine simulations in atmospheric flows},
+        journal = {Wind Energy},
+        volume = {27},
+        number = {3},
+        pages = {225-257},
+        doi = {10.1002/we.2886},
+        url = {https://onlinelibrary.wiley.com/doi/abs/10.1002/we.2886},
+        eprint = {https://onlinelibrary.wiley.com/doi/pdf/10.1002/we.2886},
+        year = {2024}
+    }
---
+++ doxygen/html/search/variables_13.js
@@ -0,0 +1,28 @@
+var searchData=
+[
+  ['t_0',['T',['../structamr__wind_1_1_factory_1_1_register.html#a7750bbbf17dbc8cd175864e96cd3808b',1,'amr_wind::Factory::Register::T'],['../classamr__wind_1_1_view_field.html#a5cb2f1b2f7df0f7d38b634337472f2dc',1,'amr_wind::ViewField::T']]],
+  ['t_1',['t',['../structamr__wind_1_1ocean__waves_1_1_w2_a_waves_data.html#afe71b72c9a0fc118e22203f07bb64a49',1,'amr_wind::ocean_waves::W2AWavesData']]],
+  ['t_5flast_2',['t_last',['../structamr__wind_1_1ocean__waves_1_1_w2_a_waves_data.html#a6f99f862c01e49aaafee8a65ea27fc92',1,'amr_wind::ocean_waves::W2AWavesData']]],
+  ['t_5fwinit_3',['t_winit',['../structamr__wind_1_1ocean__waves_1_1_w2_a_waves_data.html#a9508facfe449e88e6cc44f5cf2fc0328',1,'amr_wind::ocean_waves::W2AWavesData']]],
+  ['table_5fvelocity_4',['table_velocity',['../structamr__wind_1_1actuator_1_1_disk_base_data.html#acf58a5898a66fdf8e99abd1657141fbf',1,'amr_wind::actuator::DiskBaseData']]],
+  ['tcomm_5',['tcomm',['../structamr__wind_1_1actuator_1_1_turbine_external_data.html#a5ff00442f098adb0cc7d1f3055dda418',1,'amr_wind::actuator::TurbineExternalData']]],
+  ['temp_5fflux_5fsurface_6',['temp_flux_surface',['../structamr__wind_1_1_shear_stress_donelan.html#abfbd10f69394f56956b8d04eddac56cc',1,'amr_wind::ShearStressDonelan']]],
+  ['term1_7',['term1',['../structamr__wind_1_1_shear_stress_constant.html#af2dbd606a4bd5621be135d484b64bb90',1,'amr_wind::ShearStressConstant::term1'],['../structamr__wind_1_1_shear_stress_local.html#a5b427f3f0208a317797d09c58dbed34f',1,'amr_wind::ShearStressLocal::term1'],['../structamr__wind_1_1_shear_stress_schumann.html#ac5cf6ba250419918dbeaef3fee31925d',1,'amr_wind::ShearStressSchumann::term1'],['../structamr__wind_1_1_shear_stress_moeng.html#af5bb4c4739c6eb01cd63b1a7e9d9788c',1,'amr_wind::ShearStressMoeng::term1']]],
+  ['theta0_8',['theta0',['../structamr__wind_1_1sampling_1_1_prism_parameters.html#ad388b3a12f142fb06f1dfed1d1bda515',1,'amr_wind::sampling::PrismParameters']]],
+  ['theta_5fmean_9',['theta_mean',['../structamr__wind_1_1_m_o_data.html#ab62750deeee2907fa18d571d1bb09ca4',1,'amr_wind::MOData::theta_mean'],['../structamr__wind_1_1_shear_stress_constant.html#ad8bedc51d906044c0d016cc149c6b03f',1,'amr_wind::ShearStressConstant::theta_mean'],['../structamr__wind_1_1_shear_stress_moeng.html#a523b32160311be51c3c09e642a59e6c5',1,'amr_wind::ShearStressMoeng::theta_mean'],['../structamr__wind_1_1_shear_stress_donelan.html#afe730679af36958c16729a1219290e2b',1,'amr_wind::ShearStressDonelan::theta_mean']]],
+  ['theta_5fsurface_10',['theta_surface',['../structamr__wind_1_1_shear_stress_constant.html#a21b2e548d7b78afd86da0ef17f4108a0',1,'amr_wind::ShearStressConstant::theta_surface'],['../structamr__wind_1_1_shear_stress_local.html#ac0747794b0bc1c995ba2621df4e3c1e9',1,'amr_wind::ShearStressLocal::theta_surface'],['../structamr__wind_1_1_shear_stress_schumann.html#aa1cfafdd787aea6bd5b83ad38e52474a',1,'amr_wind::ShearStressSchumann::theta_surface'],['../structamr__wind_1_1_shear_stress_moeng.html#a2356842aec852b8bf101a3958c8719e2',1,'amr_wind::ShearStressMoeng::theta_surface'],['../structamr__wind_1_1_shear_stress_donelan.html#a8f79e17b9fea42368104ccd2f7eb8359',1,'amr_wind::ShearStressDonelan::theta_surface']]],
+  ['thrust_5fcoeff_11',['thrust_coeff',['../structamr__wind_1_1actuator_1_1_disk_base_data.html#a24b72f9706d75f74ae0fe520af65ab68',1,'amr_wind::actuator::DiskBaseData']]],
+  ['tid_5fglobal_12',['tid_global',['../structext__turb_1_1_external_turbine.html#a3d0170535e43709ea333380c9be8210c',1,'ext_turb::ExternalTurbine']]],
+  ['tid_5flocal_13',['tid_local',['../structext__turb_1_1_external_turbine.html#ab69769f9f8da58a297666a8342cce2a6',1,'ext_turb::ExternalTurbine']]],
+  ['tight_5ftol_14',['TIGHT_TOL',['../namespaceamr__wind_1_1constants.html#a0aee8a942fdb663d4cb5d169e94d9817',1,'amr_wind::constants']]],
+  ['time_15',['time',['../structamr__wind_1_1_m_o_s_d.html#a946ce9806c11fd86fdc7e813412699d6',1,'amr_wind::MOSD']]],
+  ['time_5findex_16',['time_index',['../structext__turb_1_1_external_turbine.html#a5fc4b3807afa14d498a6d9ad0c71f055',1,'ext_turb::ExternalTurbine']]],
+  ['time_5ftable_17',['time_table',['../structamr__wind_1_1actuator_1_1_wing_base_data.html#aa2926aae70a1cf7184dce62f8a47d626',1,'amr_wind::actuator::WingBaseData']]],
+  ['tip_5fcorrection_18',['tip_correction',['../structamr__wind_1_1actuator_1_1_joukowsky_data.html#ab29f28e58ac7c70f8df1a86f27129f9b',1,'amr_wind::actuator::JoukowskyData']]],
+  ['tlabel_19',['tlabel',['../structext__turb_1_1_external_turbine.html#af0b89881e0a20055e353073164c37bcd',1,'ext_turb::ExternalTurbine']]],
+  ['to_5fcfd_20',['to_cfd',['../structext__turb_1_1_fast_turbine.html#a8fe264d1f2a75701a9a6d542061647b9',1,'ext_turb::FastTurbine']]],
+  ['to_5fsc_21',['to_sc',['../structext__turb_1_1_fast_turbine.html#ad24caa4e4ea7064e3cf25b9f5f4b39be',1,'ext_turb::FastTurbine']]],
+  ['top_5fvel_22',['top_vel',['../structamr__wind_1_1udf_1_1_two_layer_1_1_device_op.html#a51b5d2794877fe0c447df8d925d2bbbe',1,'amr_wind::udf::TwoLayer::DeviceOp']]],
+  ['tower_23',['tower',['../structamr__wind_1_1actuator_1_1_turbine_base_data.html#a57a694de8ab9f67ec4f67a67aff0aea5',1,'amr_wind::actuator::TurbineBaseData']]],
+  ['tr_5fmat_24',['tr_mat',['../structamr__wind_1_1_synth_turb_data.html#a220e525c1e38d1b02e32b16de0438972',1,'amr_wind::SynthTurbData::tr_mat'],['../structamr__wind_1_1_synth_turb_device_data.html#acc2859bbd7137a488eaf914f86d7a963',1,'amr_wind::SynthTurbDeviceData::tr_mat']]]
+];
